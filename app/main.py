62a47675c94c7064a3af304a5843bd6cb9c7eb46
Dmitry Ivanov <karward94@gmail.com>
2022-02-02 20:19:31 +0200

    Fix path for files.

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import csv
 def create_report(data_file_name: str, report_file_name: str):
     buy = 0
     supply = 0
-    with open(f"../{data_file_name}", 'r') as csv_file:
+    with open(data_file_name, 'r') as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
         for row in reader:
             if row[0] == "buy":
@@ -13,7 +13,7 @@ def create_report(data_file_name: str, report_file_name: str):
                 supply += int(row[1])
     result = supply - buy
 
-    with open(f"{report_file_name}", "w") as csv_file_report:
+    with open(report_file_name, "w") as csv_file_report:
         writer = csv.writer(csv_file_report, delimiter=",")
         writer.writerow(["supply", supply])
         writer.writerow(["buy", buy])
