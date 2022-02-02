d621abfc3cae9419ec43937965a1e6f3c8896fc4
Alex Zhidkov <sashazudkov.@gmail.com>
2022-02-02 15:23:00 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    supply = 0
+    buy = 0
+    with open(data_file_name[3:], 'r') as filein:
+        for line in filein:
+            items = line.split(",")
+            if len(items) > 0 and items[0] == 'supply':
+                supply += int(items[1])
+            elif len(items) > 0 and items[0] == 'buy':
+                buy += int(items[1])
+    result = supply - buy
+    dict_result = {"supply": supply, "buy": buy, "result": result}
+    with open(report_file_name, 'w') as fileout:
+        for name, value in dict_result.items():
+            fileout.writelines(f'{str(name)},' + str(value) + '\n')
