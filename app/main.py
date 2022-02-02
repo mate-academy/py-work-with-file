cde71218fb625d563b4f2dc1c65e4bc29640ad0c
TJgkOsVP <sasha06052001@gmail.com>
2022-02-02 21:14:18 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 def create_report(data_file_name: str, report_file_name):
-    operation_amount = {"buy": 0, "supply": 0}
-    with open(data_file_name) as file1:
-        for line in file1:
+    report_dict = {"buy": 0, "supply": 0}
+    with open(data_file_name) as source:
+        for line in source:
             info = line.strip('\n').split(',')
-            operation_amount[info[0]] += int(info[1])
-        with open(report_file_name, "w") as file2:
-            supply = operation_amount["supply"]
-            buy = operation_amount["buy"]
-            result = supply - buy
-            file2.write(f"supply,{supply}\nbuy,{buy}\nresult,{result}\n")
+            report_dict[info[0]] += int(info[1])
+        report_dict["rest"] = report_dict["supply"] - report_dict["buy"]
+        with open(report_file_name, "w") as report:
+            report.write("supply,{supply}\nbuy,{buy}\nresult,{rest}\n".format(**report_dict))
