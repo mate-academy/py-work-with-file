c616b8729a36b85a0d66ba76351b7f97d0d83ca7
Alex Zhidkov <sashazudkov.@gmail.com>
2022-02-02 18:01:30 +0200

    remove dict

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def create_report(data_file_name: str, report_file_name: str):
             elif len(items) > 0 and items[0] == 'buy':
                 buy += int(items[1])
     result = supply - buy
-    dict_result = {"supply": supply, "buy": buy, "result": result}
     with open(report_file_name, 'w') as fileout:
-        for name, value in dict_result.items():
-            fileout.writelines(f'{str(name)},' + str(value) + '\n')
+        fileout.writelines(f'supply,' + str(supply) + '\n')
+        fileout.writelines(f'buy,' + str(buy) + '\n')
+        fileout.writelines(f'result,' + str(result) + '\n')
