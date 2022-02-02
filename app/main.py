e8a420520573d178af7602a90293a492ae658d3d
TJgkOsVP <sasha06052001@gmail.com>
2022-02-02 21:17:39 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -6,4 +6,6 @@ def create_report(data_file_name: str, report_file_name):
             report_dict[info[0]] += int(info[1])
         report_dict["rest"] = report_dict["supply"] - report_dict["buy"]
         with open(report_file_name, "w") as report:
-            report.write("supply,{supply}\nbuy,{buy}\nresult,{rest}\n".format(**report_dict))
+            report.write("supply,"
+                         "{supply}\nbuy,{buy}\nresult,"
+                         "{rest}\n".format(**report_dict))
