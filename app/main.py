006ef5d58a5b815383542e89ef02fe4e333479e3
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-02-02 16:30:49 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    with open(data_file_name, "r") as f:
+        lines = f.readlines()
+        with open(report_file_name, "w") as f2:
+            supply = 0
+            buy = 0
+            for line in lines:
+                content = line.split(',')
+                if content[0] == "supply":
+                    supply += int(content[1])
+                elif content[0] == "buy":
+                    buy += int(content[1])
+            f2.write(f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n")
+            print("success")
+
+
+if __name__ == "__main__":
+    create_report()
