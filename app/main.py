9142b1bfaabd9e1dfad13d5184a783e8f3c3da9d
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-02-02 17:19:38 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def create_report(data_file_name: str, report_file_name: str):
-    with open(data_file_name, "r") as f:
-        lines = f.readlines()
-        with open(report_file_name, "w") as f2:
+    with open(data_file_name, "r") as data:
+        lines = data.readlines()
+        with open(report_file_name, "w") as report:
             supply = 0
             buy = 0
             for line in lines:
@@ -10,7 +10,7 @@ def create_report(data_file_name: str, report_file_name: str):
                     supply += int(content[1])
                 elif content[0] == "buy":
                     buy += int(content[1])
-            f2.write(f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n")
+            report.write(f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n")
             print("success")
 
 
