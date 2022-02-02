cd4d389d9f512cd52fee8fbf763d4cf3220cd136
Alex Zhidkov <sashazudkov.@gmail.com>
2022-02-02 16:29:35 +0200

    fix tests

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def create_report(data_file_name: str, report_file_name: str):
     supply = 0
     buy = 0
-    with open(data_file_name[3:], 'r') as filein:
+    with open(data_file_name, 'r') as filein:
         for line in filein:
             items = line.split(",")
             if len(items) > 0 and items[0] == 'supply':
