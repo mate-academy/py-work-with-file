2968d41cb1cc99a5f8be4f1e27771fc91e7bde38
alix makarova <alix.makarova@gmail.com>
2022-02-03 17:21:00 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -10,8 +10,9 @@ def create_report(data_file_name: str, report_file_name: str):
                 split_line = line.strip("\n").split(",")
                 result[split_line[0]] += int(split_line[1])
 
-        result["result"] = result["supply"] - result["buy"]
+    result["result"] = result["supply"] - result["buy"]
 
-        with open(report_file_name, "w") as report:
-            for key, value in result.items():
-                report.write(f"{key},{value}\n")
+    with open(report_file_name, "w") as report:
+        for key, value in result.items():
+            report.write(f"{key},{value}\n")
+
