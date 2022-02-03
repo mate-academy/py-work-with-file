0f236789873d6f6feef40167a099b31d1669965e
nikon4uk <nikonchukmax4@gmail.com>
2022-02-03 20:46:00 +0200

    was name refactored

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ def create_report(data_file_name: str, report_file_name: str):
     supply = 0
     buy = 0
 
-    with open(data_file_name, 'r') as f:
-        for line in f:
+    with open(data_file_name, 'r') as data_file:
+        for line in data_file:
             line = line.split(",")
             if line[0] == "supply":
                 supply += int(line[1])
@@ -12,8 +12,8 @@ def create_report(data_file_name: str, report_file_name: str):
 
     result = supply - buy
 
-    with open(report_file_name, 'w') as f2:
-        f2.write(
+    with open(report_file_name, 'w') as report_file:
+        report_file.write(
             f"supply,{supply}\n"
             f"buy,{buy}\n"
             f"result,{result}\n"
