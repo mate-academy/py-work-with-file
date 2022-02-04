ea395139a395a7ca53074d2fb4bca60099172e06
Zikkolion <nmadaraosnova835@gmail.com>
2022-02-04 08:34:14 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    with open(data_file_name, 'r') as f:
+        with open(report_file_name, 'w') as wr_file:
+            buy = 0
+            supply = 0
+            for line in f.readlines():
+                if line != '':
+                    ls = line.split(',')
+                    if ls[0] == 'buy':
+                        buy += int(ls[1])
+                        continue
+                    supply += int(ls[1])
+            report = (f'supply,{supply}\n'
+                      f'buy,{buy}\n'
+                      f'result,{supply - buy}\n')
+            wr_file.write(report)
+
