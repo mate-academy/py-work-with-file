88284f539ac3d59787d915ff79f2972ae51fee95
Kalyta Valerii <deptovka@gmail.com>
2022-02-06 22:05:28 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,12 +2,14 @@ def create_report(data_file_name: str, report_file_name: str):
 
     with open(data_file_name, "r") as data:
         trade = data.readlines()
+        supply = 0
+        buy = 0
 
-        supply = sum(int(line.split(",")[1]) for line in trade
-                     if line.split(",")[0] == "supply")
-
-        buy = sum(int(line.split(",")[1]) for line in trade
-                  if line.split(",")[0] == "buy")
+        for line in trade:
+            if line.split(",")[0] == "supply":
+                supply += int(line.split(",")[1])
+            else:
+                buy += int(line.split(",")[1])
 
         with open(report_file_name, "w") as report:
             report.write(f"supply,{supply}\n"
