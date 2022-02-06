72a94024ddac25840cf6e01f25d61c5ce7289264
Vlad Tsvigun <vl.tsv@outlook.com>
2022-02-06 18:21:45 +0200

    fix for nested functions

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ def create_report(data_file_name: str, report_file_name: str):
     buy = 0
 
     with open(data_file_name, "r") as data_file:
+        lines = data_file.readlines()
         with open(report_file_name, "w") as report_file:
-            for line in data_file.readlines():
+            for line in lines:
                 data = line.split(',')
                 if data[0] == "supply":
                     supply += int(data[1])
