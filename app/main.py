0025f8e6a582483f6de2ba1d0042e95a08bce4d0
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-05-17 18:23:01 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    with open(data_file_name, "r") as file:
+        file_list = file.readlines()
+
+    sum_supply, sum_buy = 0, 0
+    for line in file_list:
+        if 'supply' in line:
+            sum_supply += int(line[line.index(",") + 1:])
+        elif "buy" in line:
+            sum_buy += int(line[line.index(",") + 1:])
+
+    result_data = [
+        f"supply,{sum_supply}\n",
+        f"buy,{sum_buy}\n"
+        f"result,{sum_supply - sum_buy}\n"
+    ]
+
+    with open(report_file_name, "w") as wfile:
+        wfile.writelines(result_data)
