dab6ce28c781a388b2f1b7d35772ab4cee70b287
Flashmobber <ufinaid@gmail.com>
2022-05-28 23:42:59 +0300

    All done.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    supply, buy = 0, 0
+
+    with open(data_file_name, "r") as data_file:
+        with open(report_file_name, "w") as report_file:
+            data = data_file.read().split("\n")
+            for line in data:
+                if line != "":
+                    operation, volume = line.split(",")
+                    if operation == "supply":
+                        supply += int(volume)
+                    if operation == "buy":
+                        buy += int(volume)
+
+            report = f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n"
+            report_file.write(report)
+
+            report_file.close()
+            data_file.close()
