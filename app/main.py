3d98b1e79c1d5416bf4ccc11c43bf2c63182ddb2
Artem Toporchan <gydratvar@gmail.com>
2022-05-31 21:59:58 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 def create_report(data_file_name: str, report_file_name: str):
     buy_result = 0
     supply_result = 0
+
     with open(data_file_name, "r") as file_read:
         for line in file_read:
             line = list(line.split(","))
@@ -11,7 +12,6 @@ def create_report(data_file_name: str, report_file_name: str):
                 buy_result += int(line[1])
 
     with open(report_file_name, "w") as file_write:
-
         file_write.write(f"supply,{supply_result}\n"
                          f"buy,{buy_result}\n"
                          f"result,{supply_result - buy_result}\n")
