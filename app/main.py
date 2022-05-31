e4194670578d1131e0051b17571ba6672d953c78
Taras Nester <tarasnester414@gmail.com>
2022-05-31 12:24:12 +0300

    add "result" key to dict

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def create_report(data_file_name: str, report_file_name: str):
     with open(data_file_name) as file:
-        report = {"supply": 0, "buy": 0}
+        report = {"supply": 0, "buy": 0, "result": 0}
         for line in file:
             key, value = line.split(",")
             report[key] += int(value)
