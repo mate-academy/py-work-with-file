a0bf761a6c2d5f502557d1d78b04f32d7ce3ffb4
Flashmobber <ufinaid@gmail.com>
2022-05-31 20:56:05 +0300

    For tests

--- app/main.py
+++ app/main.py
@@ -9,7 +9,6 @@ def create_report(data_file_name: str, report_file_name: str):
                     supply += int(volume)
                 if operation == "buy":
                     buy += int(volume)
-
             with open(report_file_name, "w") as report_file:
                 report_file.write(f"supply,{supply}\nbuy,"
                                   f"{buy}\nresult,{supply - buy}\n")
