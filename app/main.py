973d9c5b7cb273dd7aace1869aee17a944fdf37f
Denis <sairustv228@gmail.com>
2022-06-03 16:45:02 +0300

    v1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name):
+    buy = 0
+    supply = 0
+
+    with open(data_file_name, "r") as input_file, open(report_file_name, "w") as report_file:
+        for line in input_file:
+            line = line.split(",")
+            if line[0] == "buy":
+                buy += int(line[1].rstrip())
+            elif line[0] == "supply":
+                supply += int(line[1].rstrip())
+
+        result = supply - buy
+        report_file.write(
+            f"supply,{supply}\n"
+            f"buy,{buy}\n"
+            f"result,{result}\n"
+        )
