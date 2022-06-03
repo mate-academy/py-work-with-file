ddab0727b82e09a54abea888867e1c6878016229
Denis <sairustv228@gmail.com>
2022-06-03 16:46:07 +0300

    v1.1

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ def create_report(data_file_name: str, report_file_name):
     buy = 0
     supply = 0
 
-    with open(data_file_name, "r") as input_file, open(report_file_name, "w") as report_file:
+    with open(data_file_name, "r") as input_file,\
+            open(report_file_name, "w") as report_file:
         for line in input_file:
             line = line.split(",")
             if line[0] == "buy":
