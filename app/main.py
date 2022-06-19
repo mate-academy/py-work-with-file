0c869829f086348d2bcdb15cedb50d1f65255939
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-06-19 22:09:22 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    with open(f"../{data_file_name}", 'r') as f,\
+            open(report_file_name, "w") as rep_file:
+        buy = []
+        supply = []
+        for item in sorted(f.readlines()):
+            key = item.strip("\n").split(",")[0]
+            value = int(item.strip("\n").split(",")[1])
+            if key == "buy":
+                buy.append(value)
+            else:
+                supply.append(value)
+        rep_file.write(f"supply,{sum(supply)}\n")
+        rep_file.write(f"buy,{sum(buy)}\n")
+        rep_file.write(f"result,{sum(supply) - sum(buy)}\n")
