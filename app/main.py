c34744b75cba43991ca7337c0cced46fbdd9bad3
Misha Hrynko <misha@mate.academy>
2022-07-18 14:34:09 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    data_file_name = f'C:/Users/Bodnarchuk/Desktop/Project/study-project/py-work-with-file/{data_file_name}'
+    with open(data_file_name, "r") as data_r:
+        lines = data_r.readlines()
+        s, b = 0, 0
+        for line in lines:
+            line = line.split(",")
+            if "supply" in line:
+                s += int(line[1])
+            if "buy" in line:
+                b += int(line[1])
+        sup = f"supply,{s}"
+        buy = f"buy,{b}"
+        result = f"result,{s - b}"
+        res = [sup, buy, result]
+        res = "\n".join(res) + "\n"
+        with open(report_file_name, "w") as rep_w:
+            rep_w.write(res)
