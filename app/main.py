beb73b930f4d9d003765276e47dfd3e6eb406288
stoveprofi <stoveprofi@gmail.com>
2022-08-01 15:42:13 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    sup = 0
+    buy_l = 0
+    with open(data_file_name, 'r') as f, open(report_file_name, 'w') as f2:
+        rep = f.readlines()
+        for row in rep:
+            line = row.split(",")
+            if line[0] == 'supply':
+                sup += int(line[1])
+            else:
+                buy_l += int(line[1])
+        f2.write("supply," + str(sup) + "\n" + "buy," + str(
+            buy_l) + "\nresult," + str(sup - buy_l) + "\n")
