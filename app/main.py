1a9993a0c48fda0d5708e805bd2fa29117cee5b9
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2022-09-23 13:12:51 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    buy = 0
+    supply = 0
+    with open(data_file_name, "r") as file_in, \
+            open(report_file_name, "w") as file_out:
+        for line in file_in:
+            key, value = line.split(",")
+            if key == "buy":
+                buy += int(value)
+            if key == "supply":
+                supply += int(value)
+        file_out.write(f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n")
