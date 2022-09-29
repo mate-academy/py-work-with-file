4d717ad403dc721ccfb3df464177f19d5a8b1266
AndriyKy <akrechovetskiy@gmail.com>
2022-09-29 11:50:44 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    with open(data_file_name, "r") as source, \
+            open(report_file_name, "a") as target:
+        data = {}
+        for line in source.readlines():
+            line = [line.split(",")[0], int(line.split(",")[1])]
+            if line[0] in data:
+                data[line[0]] += line[1]
+            else:
+                data[line[0]] = line[1]
+
+        data["result"] = data["supply"] - data["buy"]
+
+        target.write(f"supply,{data['supply']}\n")
+        target.write(f"buy,{data['buy']}\n")
+        target.write(f"result,{data['result']}\n")
