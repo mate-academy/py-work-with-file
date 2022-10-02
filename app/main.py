812fd4c493c3e373c94a305b94d8d6180fcf859d
Vasyl Poremchuk <nleveryone@gmail.com>
2022-10-02 13:28:47 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as data_file:
+        content = {}
+
+        for line in data_file:
+            line = line.split(",")
+            if line[0] == "supply":
+                content["supply"] = content.get("supply", 0) + int(line[1])
+            else:
+                content["buy"] = content.get("buy", 0) + int(line[1])
+
+    content["result"] = content["supply"] - content["buy"]
+
+    with open(report_file_name, "w") as report_file:
+        report_file.write(
+            f"supply,{content['supply']}\n"
+            f"buy,{content['buy']}\n"
+            f"result,{content['result']}\n"
+        )
