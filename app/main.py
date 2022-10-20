9433895e1ad0e9d2f7befd81f6e54b90a1aa4654
George <georgepavlej@gmail.com>
2022-10-20 16:15:22 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    buy = 0
+    supply = 0
+    with open(data_file_name, "r") as data, \
+            open(report_file_name, "w") as report:
+        for row in data:
+            if row.startswith("buy"):
+                buy += int(row[4:])
+            else:
+                supply += int(row[7:])
+        report.write(f"supply,{supply}\n"
+                     f"buy,{buy}\n"
+                     f"result,{supply - buy}\n")
