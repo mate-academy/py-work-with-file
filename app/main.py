d7ced2615488e6f559a64d1e71882092be2fae63
Denys Tereshchuk <terrya@ukr.net>
2022-10-30 17:41:54 +0400

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_report(data_file_name: str,
+                  report_file_name: str) -> None:
+    temp = {"supply": 0, "buy": 0, "result": 0}
+    with (open(f"/Users/terrya/projects/py-work-with-file"
+               f"/{data_file_name}", "r") as data_file,
+          open(report_file_name, "w") as report_file):
+        for line in data_file.read().splitlines():
+            if line.split(",")[0] == "supply":
+                temp["supply"] += int(line.split(",")[1])
+            if line.split(",")[0] == "buy":
+                temp["buy"] += int(line.split(",")[1])
+        temp["result"] = temp["supply"] - temp["buy"]
+        for key, value in temp.items():
+            report_file.write(f"{key},{str(value)}\n")
+        report_file.write("")
