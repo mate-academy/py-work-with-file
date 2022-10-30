701db707b13aca61576b5d60514ea9f687d25a89
Taras Martyniuk <bythewaters800@gmail.com>
2022-10-30 18:12:43 +0100

    Sol

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str,
+                  report_file_name: str) -> None:
+
+    with open(data_file_name, "r") as data, \
+            open(report_file_name, "w") as report:
+        data_dict = {}
+        for i in data:
+            new = [i.rstrip("\n").split(",")]
+            for name in new:
+                if name[0] not in data_dict:
+                    data_dict[name[0]] = int(name[1])
+                else:
+                    data_dict[name[0]] += int(name[1])
+        data_dict["result"] = data_dict["supply"] - data_dict["buy"]
+
+        report.write(f"supply,{data_dict['supply']}\n"
+                     f"buy,{data_dict['buy']}\n"
+                     f"result,{data_dict['result']}\n")
