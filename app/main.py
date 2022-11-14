b8c7b8af32586dceeb7971e79a1142479cbbe0dd
Ihor Ivashov <dm-i@i.ua>
2022-11-14 14:17:28 +0200

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,12 +1,7 @@
-import os
-
-
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    cur_path = os.getcwd()
-    new_path = cur_path[:cur_path.rfind("\\")]
     supply = 0
     buy = 0
-    with open(new_path + "/" + data_file_name, "r") as file_in, \
+    with open(data_file_name, "r") as file_in, \
          open(report_file_name, "w") as file_report:
         for line in file_in:
             line_ls = line.split(",")
