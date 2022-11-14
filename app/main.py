80383025864ad9d961206208977ccb271708c6f8
Ihor Ivashov <dm-i@i.ua>
2022-11-14 14:12:11 +0200

    Fixed path

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     new_path = cur_path[:cur_path.rfind("\\")]
     supply = 0
     buy = 0
-    with open(new_path + "\\" + data_file_name, "r") as file_in, \
+    with open(new_path + r"\\" + data_file_name, "r") as file_in, \
          open(report_file_name, "w") as file_report:
         for line in file_in:
             line_ls = line.split(",")
