22a9141cf80c7b4378fd29dd370aab49fff5644f
soobig666 <darkvader.ukr.net@gmail.com>
2022-11-22 22:07:13 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+def create_report(
+        data_file_name: str,
+        report_file_name: str) -> None:
+    with (
+        open(data_file_name, "r") as file_in,
+        open(report_file_name, "w") as file_out
+    ):
+        file_in_copy = file_in.read().replace("\n", ",").split(",")
+        file_in.close()
+        action = None
+        info = {"supply": 0, "buy": 0, "result": 0}
+
+        for string in file_in_copy:
+            if action is None:
+                action = string
+                continue
+            info[action] += int(string)
+            action = None
+
+        info["result"] += info["supply"] - info["buy"]
+        info = f"supply,{info['supply']}\nbuy,{info['buy']}\nresult,{info['result']}"
+
+        file_out.write(info)
+        file_out.close()
