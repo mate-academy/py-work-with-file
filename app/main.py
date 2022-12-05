7f0ab39e4039d6e156f3172ba48e247c11a8ca2d
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-12-05 16:24:26 +0200

    flake 8 5

--- app/main.py
+++ app/main.py
@@ -2,19 +2,18 @@ import csv
 
 
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    total_supply = 0
-    total_buy = 0
-
-    with open(data_file_name, "r", encoding="UTF-8") as csv_report_file:
-        reader = csv.reader(csv_report_file)
-        for row in reader:
-            if row[0] == "supply":
-                total_supply = total_supply + int(row[1])
-            if row[0] == "buy":
-                total_buy = total_buy + int(row[1])
-        rezult = total_supply - total_buy
-
-    report_file = csv.writer(csv_report_file, delimiter=",")
-    report_file.writerow(["supply", f"{total_supply}"])
-    report_file.writerow(["buy", f"{total_buy}"])
-    report_file.writerow(["result", f"{rezult}"])
+    with open(data_file_name, "r") as csv_data_file, \
+            open(report_file_name, "a", newline="") as csv_report_file:
+        data_file = csv.reader(csv_data_file, delimiter=",")
+        supply_total = 0
+        buy_total = 0
+        for operation in data_file:
+            if operation[0] == "supply":
+                supply_total += int(operation[1])
+            if operation[0] == "buy":
+                buy_total += int(operation[1])
+        result = supply_total - buy_total
+        report_file = csv.writer(csv_report_file, delimiter=",")
+        report_file.writerow(["supply", f"{supply_total}"])
+        report_file.writerow(["buy", f"{buy_total}"])
+        report_file.writerow(["result", f"{result}"])
