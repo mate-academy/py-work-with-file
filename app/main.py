9ddfa8f68497a9e7370fa1ae30b55b494a057a7a
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-12-05 16:22:32 +0200

    flake 8 5

--- app/main.py
+++ app/main.py
@@ -5,15 +5,14 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     total_supply = 0
     total_buy = 0
 
-    with open(data_file_name, "r", encoding="UTF-8") as csvfile_read:
-        reader = csv.reader(csvfile_read)
+    with open(data_file_name, "r", encoding="UTF-8") as csv_report_file:
+        reader = csv.reader(csv_report_file)
         for row in reader:
             if row[0] == "supply":
                 total_supply = total_supply + int(row[1])
             if row[0] == "buy":
                 total_buy = total_buy + int(row[1])
         rezult = total_supply - total_buy
-
 
     report_file = csv.writer(csv_report_file, delimiter=",")
     report_file.writerow(["supply", f"{total_supply}"])
