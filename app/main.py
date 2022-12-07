78c8f52759bc9190c16032019e8280cf9f636192
Ivan Berezyanskyi <gamaew.res@gmail.com>
2022-12-07 16:08:11 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open(data_file_name, "r") as f, open(report_file_name, "w") as q:
+    with open(data_file_name, "r") as file, \
+         open(report_file_name, "w") as reports:
         supply_sum, buy_sum = 0, 0
         while True:
-            a = f.readline().split(",")
-            if a[0] == "supply":
-                supply_sum += int(a[1])
-            elif a[0] == "buy":
-                buy_sum += int(a[1])
+            line = file.readline().split(",")
+            if line[0] == "supply":
+                supply_sum += int(line[1])
+            elif line[0] == "buy":
+                buy_sum += int(line[1])
             else:
                 break
-        q.write(f"supply,{supply_sum}\n"
-                f"buy,{buy_sum}\n"
-                f"result,{supply_sum - buy_sum}\n"
-                )
+        reports.write(f"supply,{supply_sum}\n"
+                      f"buy,{buy_sum}\n"
+                      f"result,{supply_sum - buy_sum}\n"
+                      )
