53ab81e8efd9726d75222d4e58c8951c87176a30
Ivan Berezyanskyi <gamaew.res@gmail.com>
2022-12-07 16:05:40 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as f, open(report_file_name, "w") as q:
+        supply_sum, buy_sum = 0, 0
+        while True:
+            a = f.readline().split(",")
+            if a[0] == "supply":
+                supply_sum += int(a[1])
+            elif a[0] == "buy":
+                buy_sum += int(a[1])
+            else:
+                break
+        q.write(f"supply,{supply_sum}\n"
+                f"buy,{buy_sum}\n"
+                f"result,{supply_sum - buy_sum}\n"
+                )
