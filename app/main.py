ff7fa5144ec7a461de01b7329c53b78c84eba070
Tetiana Kozakova <kleotan901@gmail.com>
2022-12-15 16:10:55 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+
+    with open(f"/Users/admin/projects/"
+              f"py-work-with-file/{data_file_name}", "r") as file_in, \
+         open(f"/Users/admin/projects/"
+              f"py-work-with-file/{report_file_name}", "w+") as file_out:
+        lst = file_in.readlines()
+        total_supply = 0
+        total_buy = 0
+        for el in lst:
+            if el.startswith("supply"):
+                total_supply += int(el.strip("supply,"))
+            if el.startswith("buy"):
+                total_buy += int(el.strip("buy,"))
+
+        result = total_supply - total_buy
+        file_out.writelines(f"supply,{total_supply}\n"
+                            f"buy,{total_buy}\nresult,{result}\n")
