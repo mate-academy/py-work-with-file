229b8a5f61f1077e4aed127acc82c45e62e57efa
Yevhen Martyniuk <evgenijmartynuk07@gmail.com>
2022-12-22 21:05:34 +0300

    'solution2'

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     with open(data_file_name, "r") as infile, \
             open(report_file_name, "a") as outfile:
         count = {}
-        for x in infile.read().split():
-            key, value = x.split(",")[0], int(x.split(",")[1])
+        for line in infile.read().split():
+            key, value = line.split(",")[0], int(line.split(",")[1])
             if key in count:
                 count[key] += value
             else:
