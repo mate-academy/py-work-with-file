74b9eb057aaa733014c37e412605d11e2d0acc3a
Vitalii Panochko <vpapplic@gmail.com>
2022-12-25 02:35:12 +0200

    CSV_file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import csv
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open("d:/project/py-work-with-file/" + data_file_name, "r") as data, \
+            open(report_file_name, "w") as report:
+        supply = 0
+        buy = 0
+        reader = csv.reader(data)
+        for line in reader:
+            # data_mas = line.split(",")
+            if line[0] == "supply":
+                supply += int(line[1])
+            if line[0] == "buy":
+                buy += int(line[1])
+        content = f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n"
+        report.write(content)
