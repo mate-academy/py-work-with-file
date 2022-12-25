6543cad0371b7b634ef5d096c3623a64b1625f2a
Vitalii Panochko <vpapplic@gmail.com>
2022-12-25 03:05:33 +0200

    create_report

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import csv
 
 
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open("d:/project/py-work-with-file/" + data_file_name, "r") as data, \
+    with open("./" + data_file_name, "r") as data, \
             open(report_file_name, "w") as report:
         supply = 0
         buy = 0
