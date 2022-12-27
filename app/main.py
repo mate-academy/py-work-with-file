3a555f96641d541a4aa5481dae15a942d6170e0b
Viacheslav Yasynetskyi <slawa1594@gmail.com>
2022-12-27 23:00:48 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     supply = 0
     buy = 0
     with open(data_file_name, "r") as file_in, \
-            open(f"{report_file_name}.csv", "w") as file_out:
+            open(report_file_name, "w") as file_out:
         for line in file_in:
             operation, amount = line.split(",")
             if operation == "supply":
@@ -11,6 +11,3 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
                 buy += int(amount)
         result = supply - buy
         file_out.write(f"supply,{supply}\nbuy,{buy}\nresult,{result}")
-
-
-create_report("apples.csv", "new")
