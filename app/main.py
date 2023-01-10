90be707589964135135486801cc7160b02eb4a71
Artur Leleiko <artur_98@ukr.net>
2023-01-10 23:32:09 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open(data_file_name, "r") as data_file, open(report_file_name, "a") as report_file:
+    with open(data_file_name, "r") as data_file, \
+            open(report_file_name, "a") as report_file:
         supply, buy = 0, 0
         for line in data_file:
             if len(line) == 0:
