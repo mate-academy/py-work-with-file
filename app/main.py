acffb9cb952ea88027f5bada1fe7893e64eedfd3
Danylo Tiutiushkin <48014331+danylott@users.noreply.github.com>
2023-01-16 09:09:00 +0200

    Update README.md

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> str:
+    i_supply = 0
+    i_buy = 0
+    with open(data_file_name, "r") as file:
+        for i in open(data_file_name).read().rsplit("\n"):
+            if i.rsplit(";")[0] == "supply":
+                i_supply += int(i.rsplit(";")[1])
+            elif i.rsplit(";")[0] == "buy":
+                i_buy += int(i.rsplit(";")[1])
+            with open(report_file_name, "w") as file:
+                file.write(
+                    str(f"supply,{i_supply}\nbuy,"
+                        f"{i_buy}\nresult,{i_supply - i_buy}\n")
+                )
+            file.close()
+    return f"supply,{i_supply}\nbuy,{i_buy}\nresult,{i_supply - i_buy}\n"
