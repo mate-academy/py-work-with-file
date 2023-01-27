3610852c74a6d9f82a2694b9be64171d6b955277
Olena Liubynetska <olena.liuby@gmail.com>
2023-01-27 13:52:01 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with (
+        open(data_file_name, "r") as data_file,
+        open(report_file_name, "w") as report_file
+    ):
+        supply = 0
+        buy = 0
+        for line in data_file:
+            operation_type, amount = line.split(",")
+            print(line, operation_type, amount)
+            if operation_type == "supply":
+                supply += int(amount)
+            if operation_type == "buy":
+                buy += int(amount)
+        report_file.write(f"result,{supply - buy}")
