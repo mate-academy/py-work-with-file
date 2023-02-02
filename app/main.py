f8ce177ba7c219d481579a74470d276c33050e8d
Vitalii <fahivchik1@gmail.com>
2023-02-02 09:46:55 +0200

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
 
-    with open(data_file_name, "r") as file_in, open(report_file_name, "w") as file_out:
+    with open(data_file_name, "r") as file_in, \
+            open(report_file_name, "w") as file_out:
+
         data = file_in.readlines()
         supply = 0
         buy = 0
