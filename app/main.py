51f5213dd75c9254a0cb32d80b17d29ced4a8a3e
Andriy <andriykrav@gmail.com>
2023-02-06 15:03:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def create_report(data_file_name: str,
+                  report_file_name: str
+                  ) -> None:
+    with open(data_file_name, "r") as data_f, \
+            open(report_file_name, "w") as report_f:
+        supply = 0
+        buy = 0
+        for row in data_f:
+            if "supply" in row.split(","):
+                supply += int(row.split(",")[1])
+            if "buy" in row.split(","):
+                buy += int(row.split(",")[1])
+        result = supply - buy
+        report_f.write(f"supply,{supply}\nbuy,{buy}\nresult,{result}\n")
