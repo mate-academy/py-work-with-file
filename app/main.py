ec228a75a110f23927e1a10d7c42286b6c887983
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-13 15:04:11 +0200

    1 commit

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    data_to_writing = {"supply": 0, "buy": 0}
+
+    with open(data_file_name, "r") as file_read:
+        data = file_read.read()
+        for row in data.split("\n"):
+            if row:
+                name, value = row.split(",")
+                data_to_writing[name] += int(value)
+
+        data_to_writing["result"] = data_to_writing["supply"] - data_to_writing["buy"]
+
+    with open(report_file_name, "w") as file_write:
+        for key, value in data_to_writing.items():
+            file_write.write(f"{key},{value}\n")
+
+
+create_report("oranges.csv", "data.csv")
