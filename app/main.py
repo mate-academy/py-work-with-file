60a8e364b24072f0ef3b09988fa31e2c9c727663
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-13 15:05:14 +0200

    Fixed flake8

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def create_report(data_file_name: str, report_file_name: str):
+def create_report(data_file_name: str, report_file_name: str) -> None:
     data_to_writing = {"supply": 0, "buy": 0}
 
     with open(data_file_name, "r") as file_read:
@@ -8,7 +8,8 @@ def create_report(data_file_name: str, report_file_name: str):
                 name, value = row.split(",")
                 data_to_writing[name] += int(value)
 
-        data_to_writing["result"] = data_to_writing["supply"] - data_to_writing["buy"]
+        data_to_writing["result"] = (data_to_writing["supply"]
+                                     - data_to_writing["buy"])
 
     with open(report_file_name, "w") as file_write:
         for key, value in data_to_writing.items():
