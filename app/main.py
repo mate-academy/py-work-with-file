d706fa7b6b310ec6eec2c4e5232a17d9c357d1ad
Yulia <Mala>
2023-02-19 20:10:50 +0200

    path changed - 3

--- app/main.py
+++ app/main.py
@@ -1,16 +1,11 @@
-from pathlib import Path
-
-
 def create_report(data_file_name: str, report_file_name: str) -> None:
     oper_dict = {}
-    data_path = Path(f"../py-work-with-file_solution/{data_file_name}")
-    with open(data_path, "r") as file:
+    with open(data_file_name, "r") as file:
         for pair in file.readlines():
             oper, num = pair.split(",")
             num = int(num.strip("\n"))
             oper_dict[oper] = oper_dict.setdefault(oper, 0) + num
-    report_path = Path(f"../py-work-with-file_solution/{report_file_name}")
-    with open(report_path, "w") as file:
+    with open(report_file_name, "w") as file:
         for oper, num in sorted(oper_dict.items(), reverse=True):
             file.write(f"{oper},{num}\n")
         file.write(f"result,{oper_dict['supply'] - oper_dict['buy']}\n")
