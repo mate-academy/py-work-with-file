9f7f5540371f0846140866c1356d3bf52612de34
Yulia <Mala>
2023-02-19 19:21:33 +0200

    path changed

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     oper_dict = {}
-    with open("/Users/yuliamala/PycharmProjects/py-work-with-file_solution/"
-              + data_file_name, "r") as file:
+    with open("../" + data_file_name, "r") as file:
         for pair in file.readlines():
             oper, num = pair.split(",")
             num = int(num.strip("\n"))
