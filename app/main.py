aacb6e7f0501ade5a98b419b44849c2579c9cd30
Ruslan <noezyboy1@gmail.com>
2023-02-19 13:43:43 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+import csv
+import os
+
+
+def process_data(data_file_name: str, report_file_name: str) -> None:
+    supply_count = 0
+    buy_count = 0
+
+    input_file_path = os.path.abspath(data_file_name)
+
+    with open(input_file_path, newline="") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if len(row) == 2:
+                if row[0] == "supply":
+                    supply_count += int(row[1])
+                elif row[0] == "buy":
+                    buy_count += int(row[1])
+
+    result = supply_count - buy_count
+
+    report_file_path = os.path.abspath(report_file_name)
+
+    with open(report_file_path, "w") as f:
+        f.write(f"supply,{supply_count}\nbuy,{buy_count}\nresult,{result}")
+
+
+process_data("input.csv", "report.txt")
