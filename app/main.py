efb47c42e970c3ad71a7e2557d06014053ab6e63
sdobzhynetskyi <sergii.dobzhynetskyi@gmail.com>
2023-02-22 15:47:06 +0200

    Revert "solved task"
    This reverts commit 59533b781e7ab6dfc252fc9bd960ba59733311e2.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,1 @@
 # write your code here
-def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open(f"../{data_file_name}", "r") as input_data, \
-            open(report_file_name, "w") as report_data:
-        reporting_counter = dict(supply = 0, buy = 0, result = 0)
-        for line in input_data:
-            action, value = line.split(",")
-            reporting_counter[action] += int(value)
-        reporting_counter["result"] = \
-            reporting_counter["supply"] - reporting_counter["buy"]
-        for key, value in reporting_counter.items():
-            report_data.write(f"{key},{value}\n")
