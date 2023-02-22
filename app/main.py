c694da3c1ded38d9e27d8dad990f119b26d4c9ca
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-02-21 22:09:05 -0600

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_dict = {}
+    with open(data_file_name, "r") as data_file:
+        for line in data_file.read().splitlines():
+            temp_data = line.split(",")
+            if temp_data[0] not in data_dict:
+                data_dict[temp_data[0]] = 0
+            data_dict[temp_data[0]] += int(temp_data[1])
+
+    with open(report_file_name, "a") as report_file:
+        report_file.write(f"{'supply'},{data_dict['supply']}\n")
+        report_file.write(f"{'buy'},{data_dict['buy']}\n")
+        report_file.write(
+            f"result,{data_dict['supply'] - data_dict['buy']}\n"
+        )
