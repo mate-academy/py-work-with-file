9e153a49d785b9449ed51c1d2d4cbb428a3d80ed
Mkrtych Khachatrian <mukuchbrat@gmail.com>
2023-03-06 13:06:44 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as file_in, open(
+            report_file_name, "w") as file_out:
+        data_dict = {
+            "supply": 0,
+            "buy": 0,
+        }
+
+        for line in file_in:
+            data = line.split(",")
+            data_dict[data[0]] += int(data[1])
+
+        data_dict["result"] = data_dict["supply"] - data_dict["buy"]
+
+        for key, value in data_dict.items():
+            file_out.write(f"{key},{value}\n")
