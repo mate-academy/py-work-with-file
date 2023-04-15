5fb9f6f77458ca0ebab763bd89cd43ba629c9d9d
Volodymyr Borovets <vborovets0@gmail.com>
2023-04-15 22:08:49 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with (open(data_file_name, "r") as data_file,
+          open(report_file_name, "w") as report_file):
+        dic = {}
+        for line in data_file.readlines():
+            key, value = line.split(",")
+            dic[key] = dic.get(key, 0) + int(value)
+
+        report_file.write(f"supply,{dic['supply']}\n")
+        report_file.write(f"buy,{dic['buy']}\n")
+        report_file.write(f"result,{dic['supply'] - dic['buy']}\n")
