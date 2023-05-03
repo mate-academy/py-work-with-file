6050a1ad4f52131dba169ce8281b88d3715acac6
Alim Selemetov <alimselemetow@gmail.com>
2023-05-03 21:18:08 +0300

    fix flake8

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open(data_file_name, "r") as file_with_information, open(report_file_name, "a") as report_file:
+    with (
+        open(data_file_name, "r") as file_with_information,
+        open(report_file_name, "a") as report_file
+    ):
         count_supply = 0
         count_buy = 0
         for data in file_with_information:
