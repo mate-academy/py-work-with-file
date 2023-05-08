f8c3d30af06f65b040dc4c44a717b9dfec26b5c9
Maks Blazhko <makssimm1@gmail.com>
2023-05-08 18:23:50 +0200

    solution v2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open(data_file_name, "r") as data_file, open(report_file_name, "a") as report_file:
+    with open(data_file_name, "r") as data_file, open(
+        report_file_name, "a"
+    ) as report_file:
         supply = 0
         buy = 0
         for line in data_file.readlines():
