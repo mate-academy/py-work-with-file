c79928cfdf1ee89a6b4edc86b33913d83dfe646c
Denys Belei <denysbelei96>
2023-05-08 11:44:06 +0100

    "solution"

--- app/main.py
+++ app/main.py
@@ -1,32 +1,15 @@
-
-import csv
-
-
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    # Initialize variables to store supply and buy amounts
     supply = 0
     buy = 0
 
-    # Open the input file and read data
-    with open(data_file_name, 'r') as data_file:
-        reader = csv.reader(data_file)
-        # Loop through each row of data and update supply and buy variables
-        for row in reader:
-            if row:  # Skip empty rows
-                operation, amount = row
-                if operation == 'supply':
-                    supply += int(amount)
-                elif operation == 'buy':
-                    buy += int(amount)
-
-    # Calculate the result
-    result = supply - buy
-
-    # Write the report to the output file
-    with open(report_file_name, 'w') as report_file:
-        writer = csv.writer(report_file)
-        writer.writerow(['supply', supply])
-        writer.writerow(['buy', buy])
-        writer.writerow(['result', result])
-
-
+    with open(data_file_name, "r") as data_file:
+        for line in data_file:
+            if "supply" in line:
+                supply += int(line.split(",")[1])
+            if "buy" in line:
+                buy += int(line.split(",")[1])
+    with open(report_file_name, "w") as report_file:
+        report_file.write(
+            f"supply,{supply}\nbuy,"
+            f"{buy}\nresult,{supply - buy}\n"
+        )
