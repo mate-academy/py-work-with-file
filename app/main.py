f4f0b121ffb65277ff373af0ddaad821e8ed5c28
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-05-12 16:43:11 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    summary = {"supply": 0,
+               "buy": 0,
+               "result": 0}
+
+    with (open(f"{data_file_name}", "r") as source_file,
+          open(f"{report_file_name}", "w") as report_file):
+        for line in source_file:
+            try:
+                line = line.split(",")
+                summary[line[0]] += int(line[1].replace("\n", ""))
+            except ValueError as e:
+                print(f"Source file contains empty lines "
+                      f"or invalid values:\n{e}")
+
+        report_file.write(f"supply,{summary['supply']}\n"
+                          f"buy,{summary['buy']}\n"
+                          f"result,{summary['supply'] - summary['buy']}")
