9d8144ed218f8af6d44ccde314fa4e2a379a9416
S-SMTN <cven28@gmail.com>
2023-05-30 15:50:34 +0300

    implemented create_report

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     supply = 0
     buy = 0
     with (
-        open(f"../{data_file_name}", "r") as csvfile,
+        open(data_file_name, "r") as csvfile,
         open(report_file_name, "w") as report_file
     ):
         csv_reader = csv.reader(csvfile, delimiter=",")
