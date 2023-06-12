014f07b72e139c601675cbd9de83af9d3e820a64
Denys Seliverstov <catsperr322@gmail.com>
2023-06-12 15:26:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+def create_report(
+        data_file_name: str,
+        report_file_name: str) -> None:
+    with (open(data_file_name, "r") as file_in,
+          open(report_file_name, "w") as file_out):
+        operations = {
+            "supply": 0,
+            "buy": 0
+        }
+
+        for row in file_in.read().split("\n"):
+            if row:
+                operation, value = row.split(",")
+                operations[operation] += int(value)
+
+        result = operations["supply"] - operations["buy"]
+
+        supply = operations["supply"]
+        buy = operations["buy"]
+        file_out.writelines([
+            f"supply,{supply}\n",
+            f"buy,{buy}\n",
+            f"result,{result}\n"
+        ])
