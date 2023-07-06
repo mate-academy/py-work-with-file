c6ac5a4cbf6dee0f0c0bafc5c0de87de4326da04
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-07-06 15:10:48 +0300

    report

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    dict_of_data = {
+        "supply": 0,
+        "buy": 0
+                }
+
+    with open(data_file_name, "r") as dt_file, open(f"{report_file_name}", "w") as file_out:
+        for line in dt_file.readlines():
+            line = line.split(",")
+            dict_of_data[line[0]] += int(line[1].replace("\n", ''))
+        dict_of_data["result"] = dict_of_data["supply"] - dict_of_data["buy"]
+
+
+        for key, value in dict_of_data.items():
+            file_out.write(f"{key},{value}\n")
