7b144137155653b6072b4f19c3117091727267e7
Olexandr <maxbvling23432@gmail.com>
2023-07-07 14:46:53 +0300

    Merge remote-tracking branch 'origin/master'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import os.path
+
+
 def create_report(data_file_name: str, report_file_name: str) -> str:
     i_supply = 0
     i_buy = 0
@@ -7,10 +10,14 @@ def create_report(data_file_name: str, report_file_name: str) -> str:
                 i_supply += int(i.rsplit(";")[1])
             elif i.rsplit(";")[0] == "buy":
                 i_buy += int(i.rsplit(";")[1])
-            with open(report_file_name, "w") as file1:
-                file1.write(
+            with open(report_file_name, "w") as file:
+                file.write(
                     str(f"supply,{i_supply}\nbuy,"
                         f"{i_buy}\nresult,{i_supply - i_buy}\n")
                 )
+            if os.path.exists(report_file_name):
+                print(f"Файл {report_file_name} було успішно створено.")
+            else:
+                print(f"Не вдалося створити файл {report_file_name}.")
             file.close()
-    return f"supply,{i_supply}\nbuy,{i_buy}\nresult,{i_supply - i_buy}\n"
+ #   return f"supply,{i_supply}\nbuy,{i_buy}\nresult,{i_supply - i_buy}\n"
