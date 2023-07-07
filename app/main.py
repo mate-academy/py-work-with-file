d849f498ab7d5034f4004e80964070045c1b2685
Olexandr <maxbovling23432@gmail.com>
2023-07-07 21:07:05 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/main.py

--- app/main.py
+++ app/main.py
@@ -7,8 +7,10 @@ def create_report(data_file_name: str, report_file_name: str) -> str:
                 i_supply += int(i.rsplit(";")[1])
             elif i.rsplit(";")[0] == "buy":
                 i_buy += int(i.rsplit(";")[1])
-        with open(report_file_name, "w") as file1:
+        with open(f"{report_file_name}", "w") as file1:
             file1.write(
                 str(f"supply,{i_supply}\nbuy,"
                     f"{i_buy}\nresult,{i_supply - i_buy}\n")
             )
+        with open(report_file_name, "r") as file2:
+            return file2.read()
