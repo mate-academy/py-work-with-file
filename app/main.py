fe4e67a5b1d81e6ddb55683de395e4f8e9301263
Olexandr <maxbvling23432@gmail.com>
2023-07-07 14:54:12 +0300

    Merge remote-tracking branch 'origin/master'

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def create_report(data_file_name: str, report_file_name: str) -> str:
     i_supply = 0
     i_buy = 0
     with open(data_file_name, "r") as file:
-        for i in open(data_file_name).read().rsplit("\n"):
+        for i in file.read().rsplit("\n"):
             if i.rsplit(";")[0] == "supply":
                 i_supply += int(i.rsplit(";")[1])
             elif i.rsplit(";")[0] == "buy":
