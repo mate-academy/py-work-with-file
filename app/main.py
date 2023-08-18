9b1f3ce16a71eed5b2da05f04541dccdc53b2952
Roman Podolets <romanpyu@gmail.com>
2023-08-18 21:16:46 +0300

    Implemented solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as source:
+        stats = {}
+        for line in source:
+            action, number = line.split(",")
+            stats[action] = stats.get(action, 0) + int(number)
+
+        stats["result"] = stats.get("supply", 0) - stats.get("buy", 0)
+
+        with open(report_file_name, "w") as destination:
+            destination.write(f"supply,{stats.get('supply', 0)}\n")
+            destination.write(f"buy,{stats.get('buy', 0)}\n")
+            destination.write(f"result,{stats.get('result')}\n")
