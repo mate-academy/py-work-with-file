36450364b2c6a4e88d9d5585d6a74f8dc018676d
Mikhail <tiunovmixs@gmail.com>
2023-08-20 23:12:06 +0300

    Implement work with file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    new_supply = 0
+    new_buy = 0
+    with open(data_file_name, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            value = line.split(",")
+            if value[0] == "supply":
+                new_supply += int(value[1])
+            if value[0] == "buy":
+                new_buy += int(value[1])
+        result = new_supply - new_buy
+    with open(report_file_name, "w") as f:
+        f.write(f"supply,{new_supply}\n")
+        f.write(f"supply,{new_buy}\n")
+        f.write(f"supply,{result}\n")
