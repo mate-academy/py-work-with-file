94163e7d91f895881546fdecb087b9bb3c6c8bc1
Kristian Yendrek <kristianendreck4@gmail.com>
2023-08-21 21:15:23 +0300

    Implemented solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as fr:
+        with open(report_file_name, "w") as fw:
+            text = fr.read()
+            text = text.replace("\n", ",")
+            items = text.split(",")
+            report = {
+                "supply": 0,
+                "buy": 0
+            }
+            for index, operation in enumerate(items):
+                if operation == "supply":
+                    report["supply"] += int(items[index + 1])
+                if operation == "buy":
+                    report["buy"] += int(items[index + 1])
+            report["result"] = report["supply"] - report["buy"]
+            fw.write(
+                f"supply,{report['supply']}\n"
+                f"buy,{report['buy']}\n"
+                f"result,{report['result']}\n"
+            )
