edf94fbdf067435995003c737bc7508161d676b8
Ostap <ostapbojko04@gmail.com>
2023-08-22 16:03:58 +0300

    py-work

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def create_report(data_file_name: str, report_file_name: str) -> str:
+def create_report(data_file_name: str, report_file_name: str) -> int:
     try:
         with open(data_file_name, "r") as data_file:
             lines = data_file.readlines()
