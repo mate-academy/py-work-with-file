7643599791f781398fabf734a428d396edc24fbd
Denys <deibukdenyscloud@gmail.com>
2023-08-23 17:07:29 +0300

    Set default values for the dictionary

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    data: dict[str, int] = {}
+    data: dict[str, int] = {"supply": 0, "buy": 0}
     with open(data_file_name, "r") as source, open(
         report_file_name, "w"
     ) as target:
