78606d500a5e32fbba33324786fd9ea9fb5a1158
Даня Пономаренко <you@example.com>
2023-09-06 12:56:13 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,8 +4,7 @@ from csv import reader
 def create_report(data_file_name: str, report_file_name: str) -> None:
     supply = 0
     buy = 0
-    with open(rf"D:\python\mate\py-work-with-file\{data_file_name}",
-              "r") as file:
+    with open(data_file_name, "r") as file:
         print(file)
         lines = reader(file)
         supply = 0
@@ -15,8 +14,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
                 supply += int(line[1])
             elif line[0] == "buy":
                 buy += int(line[1])
-    new_file = open(rf"D:\python\mate\py-work-with-file\{report_file_name}",
-                    "w")
+    new_file = open(report_file_name, "w")
     amount = supply - buy
     new_file.write(f"supply,{str(supply)}\n")
     new_file.write(f"buy,{str(buy)}\n")
