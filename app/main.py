26c7ddc51e19aeb22abafbc445a08c15b1d21aaa
Serhii Berdianskyi <zeferot94@gmail.com>
2023-10-02 14:05:07 +0300

    implemented Create report function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply = 0
+    buy = 0
+    with open(data_file_name, "r") as file:
+        data = file.readlines()
+        for line in data:
+            value = line.split(",")
+            if value[0] == "supply":
+                supply += int(value[1])
+            if value[0] == "buy":
+                buy += int(value[1])
+    result = supply - buy
+    with open(report_file_name, "w") as f:
+        f.write(f"supply,{supply}\n")
+        f.write(f"buy,{buy}\n")
+        f.write(f"result,{result}\n")
+
