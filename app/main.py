ed31a06244506399464a7aa5403cfd4a9e319dfe
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-10-16 13:50:58 +0300

    csv

--- app/main.py
+++ app/main.py
@@ -1,15 +1,21 @@
-import pandas as pd
+import csv
 
 
 def create_report(data_file_name: str, report_file_name: str) -> None:
 
-    df = pd.read_csv(data_file_name)
-    grouped = df.groupby("operation")["amount"].sum()
-    result = grouped["supply"] - grouped["buy"]
+    supply = 0
+    buy = 0
 
-    with open(report_file_name, "w") as rf:
-        rf.write(
-            f"supply,{grouped.supply}\n"
-            f"buy,{grouped.buy}\n"
-            f"result,{result}\n"
-        )
+    with open(data_file_name, "r") as file_read:
+        csv_reader = csv.reader(file_read)
+
+        for data in csv_reader:
+            if data[0] == "supply":
+                supply += int(data[1])
+            elif data[0] == "buy":
+                buy += int(data[1])
+
+        result = supply - buy
+
+    with open(report_file_name, "w") as report:
+        report.write(f"supply,{supply}\nbuy,{buy}\nresult,{result}\n")
