71d0f48edc8e9fd135419f00284ef319018c2b1c
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-10-16 13:33:29 +0300

    sol

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    with open(data_file_name, "r") as file:
+        data = file.readlines()
+
+    supply = 0
+    buy = 0
+
+    for row in data:
+        operation, amount_str = row.strip().split(',')
+        amount = int(amount_str)
+
+        if operation == "supply":
+            supply += amount
+        elif operation == "buy":
+            buy += amount
+
+    result = supply - buy
+
+    with open(report_file_name, "w") as report:
+        report.write(f"supply,{supply}\nbuy,{buy}\nresult,{result}")
