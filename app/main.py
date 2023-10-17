e9d992c48a14131673dc450987d989fdf7c012c0
Denys <den.prislipskyi@gmail.com>
2023-10-17 16:55:31 +0200

    Solution_4

--- app/main.py
+++ app/main.py
@@ -2,19 +2,16 @@ import csv
 
 
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    buy = 0
     supply = 0
-    with open(data_file_name, "r") as csv_file:
-        csv_data = csv.reader(csv_file)
-        for row in csv_data:
-            if row:
-                operation, amount = row
-                if operation == "supply":
-                    supply += int(amount)
-                elif operation == "buy":
-                    buy += int(amount)
+    buy = 0
+    with open(data_file_name, "r") as file:
+        for line in file:
+            if "supply" in line:
+                supply += int(line.split(",")[-1])
+            else:
+                buy += int(line.split(",")[-1])
 
-    with open(report_file_name, "w") as report_file:
+    with open(report_file_name, "w", newline="") as report_file:
         writer = csv.writer(report_file)
         writer.writerows([["supply", supply], ["buy", buy],
                           ["result", supply - buy]])
