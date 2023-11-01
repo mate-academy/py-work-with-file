3f0b49719b9fa305af7818bc024c7e1d72120971
Sergii Pryima <spryima@gmail.com>
2023-11-01 17:25:13 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as source:
+        to_sort = source.read()
+
+    report = {}
+    for line in to_sort.rstrip().split("\n"):
+        name, amount = line.split(",")
+        if report.get(name):
+            report[name] += int(amount)
+        else:
+            report[name] = int(amount)
+    with open(report_file_name, "w") as destination:
+
+        destination.write(f"supply,{report['supply']}\n")
+        destination.write(f"buy,{report['buy']}\n")
+        destination.write(f"result,{report['supply'] - report['buy']}\n")
