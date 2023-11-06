80176f82ea22a795d6651684fa7a91d2d5529cee
Denis Dymerlii <dum.denya15@gmail.com>
2023-11-06 22:54:02 +0200

    Solution1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     csv_data = {"supply": 0, "buy": 0}
 
-    with open(data_file_name, "r") as data_file, open(report_file_name, "w") as report_file:
+    with (open(data_file_name, "r") as data_file,
+          open(report_file_name, "w") as report_file):
         for line in data_file:
             data = line.strip().split(",")
 
