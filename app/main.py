919f38a15e52de3ef8a7ce4ba545e10d7683b298
Andrii Malovanyi <malovaniy.an@gmail.com>
2023-11-06 16:00:31 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(
+        data_file_name: str,
+        report_file_name: str
+) -> None:
+    with (
+        open(data_file_name, "r") as read_file,
+        open(report_file_name, "a") as report
+    ):
+        temp = {"supply": 0, "buy": 0}
+        for line in read_file.readlines():
+            content = line.split(",")
+            temp[content[0]] = temp[content[0]] + int(content[1])
+        result = temp["supply"] - temp["buy"]
+        for key, value in temp.items():
+            report.write(key + "," + str(value) + "\n")
+        report.write("result" + "," + str(result) + "\n")
