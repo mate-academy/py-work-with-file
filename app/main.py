a8ffb5aba1a7dd51d6016be8cbede436c9e7a1bb
Alex <oleksa.shcherbakov@gmail.com>
2023-11-11 16:34:50 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open(data_file_name, "r") as source:
+    with (open(data_file_name, "r") as source, open(report_file_name, "w") as result_file):
         new_dict = {"supply": 0, "buy": 0}
         for line in source:
-            new_dict[line.split(",")[0]] += line.split(",")[1]
+            new_dict[line.split(",")[0]] += int(line.split(",")[1])
         new_dict["result"] = new_dict.get("supply") - new_dict.get("buy")
-        with open(report_file_name, "w") as result_file:
-            for key, value in new_dict.items():
-                result_file.write(f"{key},{value}\n")
+        for key, value in new_dict.items():
+            result_file.write(f"{key},{value}\n")
