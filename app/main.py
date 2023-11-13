d38897ec692d9853197365a05757505af06e5228
Yuliia Valko <yuliia.valko1@gmail.com>
2023-11-13 20:46:42 +0200

    Solution_2

--- app/main.py
+++ app/main.py
@@ -1,18 +1,17 @@
-
 def create_report(data_file_name: str, report_file_name: str) -> None:
     with (open(data_file_name, "r") as file_in,
           open(report_file_name, "w") as file_out):
-        lines_dict = {}
+        lines_dict = {"buy": 0, "supply": 0}
         for line in file_in:
             if line:
                 line_list = line.split(",")
-                if line_list[0] not in lines_dict.keys():
-                    lines_dict[line_list[0]] = int(line_list[1])
-                else:
-                    lines_dict[line_list[0]] += int(line_list[1])
-
-        for key, value in lines_dict.items():
-            file_out.write(f"{key},{value}\n")
+                if line_list[0] == "buy":
+                    lines_dict["buy"] += int(line_list[1])
+                elif line_list[0] == "supply":
+                    lines_dict["supply"] += int(line_list[1])
 
         result = int(lines_dict["supply"]) - int(lines_dict["buy"])
-        file_out.write(f"result,{result}")
+
+        file_out.writelines([f"supply,{lines_dict['supply']}\n",
+                             f"buy,{lines_dict['buy']}\n",
+                             f"result,{result}\n"])
