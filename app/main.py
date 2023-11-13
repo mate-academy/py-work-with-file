8a6fe4b6b9b101992d69417e0bd40fd6081cee26
Yuliia Valko <yuliia.valko1@gmail.com>
2023-11-13 17:31:23 +0200

    Solution_1

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open(data_file_name, "r") as file_in, open(report_file_name, "w") as file_out:
+    with (open(data_file_name, "r") as file_in,
+          open(report_file_name, "w") as file_out):
         lines_dict = {}
         for line in file_in:
             if line:
