582b3304d01ab5ec61c05579fb228e1ea11980e8
Mykhailo Slavsky <slavskij2017@gmail.com>
2023-12-07 01:35:49 +0200

    Fix Solution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     with (open(data_file_name, "r") as file,
           open(report_file_name, "w") as result_file):
-        suply, buy, = [], []
+        supply, buy, = [], []
         for string in file:
             arg = string.split(",")
-            if arg[0] == "suply":
-                suply.append(arg[1])
+            if arg[0] == "supply":
+                supply.append(arg[1].strip())
             elif arg[0] == "buy":
-                buy.append(arg[1])
-        sum_suply = sum([int(i) for i in suply])
+                buy.append(arg[1].strip())
+        sum_suply = sum([int(i) for i in supply])
         sum_buy = sum([int(i) for i in buy])
-        result_file.write(f"supply = {' + '.join(suply)} = {sum_suply}")
-        result_file.write(f"supply = {' + '.join(buy)} = {sum_buy}")
-        result_file.write(f"result = supply - buy = {sum_suply} "
-                          f"- {sum_buy} = {sum_suply - sum_buy}")
+        result_file.write(f"supply,{sum_suply}\n")
+        result_file.write(f"buy,{sum_buy}\n")
+        result_file.write(f"result,{sum_suply - sum_buy}\n")
