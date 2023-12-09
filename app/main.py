8f5eb3cd87d4527f1dc3be094a9d65c7379ab7bb
Serhii Hrynyshyn <hrynyshyn.serhii@gmail.com>
2023-12-09 22:54:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -16,7 +16,6 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             else:
                 buy += int(el[1])
         result = supply - buy
-
         second.write(f"supply,{supply}\n")
         second.write(f"buy,{buy}\n")
         second.write(f"result,{result}\n")
