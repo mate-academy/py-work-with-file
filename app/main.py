bc9ad585cc0beb2273a2eaeb30397d9bd3a5fab3
Serhii Hrynyshyn <hrynyshyn.serhii@gmail.com>
2023-12-09 22:59:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open("..//" + data_file_name, "r") as first, open(report_file_name,
+    with open(data_file_name, "r") as first, open(report_file_name,
                                                            "a") as second:
         data1 = []
         for line in first:
