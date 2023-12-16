021531563c0a5322441d000ab2a76b8028f851ad
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2023-12-16 23:22:33 +0200

    fix: order of fields

--- app/main.py
+++ app/main.py
@@ -1,14 +1,26 @@
+class TransactionTypes:
+    SUPPLY = "supply"
+    BUY = "buy"
+    RESULT = "result"
+
+
 def create_report(data_file_name: str,
                   report_file_name: str) -> None:
     with (open(data_file_name, "r") as data_file,
           open(report_file_name, "a") as report_file):
+
         results = {}
         for line in data_file.readlines():
             columns = line.rstrip("\n").split(",")
             [operation, count] = columns
             results[operation] = results.get(operation, 0) + int(count)
 
-        results["result"] = results["supply"] - results["buy"]
+        results[TransactionTypes.RESULT] = results[TransactionTypes.SUPPLY] - \
+            results[TransactionTypes.BUY]
 
-        for key, value in results.items():
-            report_file.write(f"{key},{value}\n")
+        report_file.write(
+            f"{TransactionTypes.SUPPLY},{results[TransactionTypes.SUPPLY]}\n")
+        report_file.write(
+            f"{TransactionTypes.BUY},{results[TransactionTypes.BUY]}\n")
+        report_file.write(
+            f"{TransactionTypes.RESULT},{results[TransactionTypes.RESULT]}\n")
