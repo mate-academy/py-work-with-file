d3396a76c4132d1c71069ad04f1a751b2d12c261
Vitalii <ananievvitalii10@gmail.com>
2023-12-17 00:00:48 +0200

    Work with files

--- app/main.py
+++ app/main.py
@@ -1,1 +1,38 @@
-# write your code here
+from collections import OrderedDict
+
+
+def create_report(
+        data_file_name: str,
+        report_file_name: str) -> None:
+    with open(f"../{data_file_name}", "r") as file:
+        lines = file.read().splitlines()
+
+    result = OrderedDict()
+
+    for line in lines:
+        if not line.strip():
+            continue
+        parts = line.split(",")
+        operation_type, amount = parts[0], int(parts[1])
+
+        if operation_type in result:
+            result[operation_type] += amount
+        else:
+            result[operation_type] = amount
+
+    sorted_result = OrderedDict(
+        sorted(
+            result.items(),
+            key=lambda x: x[1],
+            reverse=True))
+
+    with open(report_file_name, "w") as w:
+        for i in sorted_result:
+            w.write(f"{i},{sorted_result[i]}\n")
+
+    result_line = (
+        f"result,"
+        f"{sorted_result.get('supply', 0) - sorted_result.get('buy', 0)}"
+    )
+    with open(report_file_name, "a") as report_file:
+        report_file.write(f"{result_line}\n")
