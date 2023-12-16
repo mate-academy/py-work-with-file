f9e0f539f1ad5819992ab822f3081197b8e8b04e
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2023-12-16 17:00:13 +0200

    feat: almost done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str = "out.txt") -> None:
+    with open(data_file_name, "r") as data_file, open(report_file_name, 'w') as report_file:
+        results = {}
+        for line in data_file.readlines():
+            columns = line.rstrip("\n").split(",")
+            [operation, count] = columns
+            results[operation] = results.get(operation, 0) + int(count)
+
+        results["result"] = results["supply"] - results["buy"]
+        print(results)
+        # for item in results:
+        #     print(item)
+
+
+create_report("bananas.csv")
+
+# with open("bananas.csv", "r") as f:
+#     print(f.readlines())
