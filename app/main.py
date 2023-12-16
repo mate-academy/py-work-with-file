6b98a4dc7444f13dfc71526da4982e45ac3dc545
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2023-12-16 23:09:17 +0200

    feat: appending result

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-def create_report(data_file_name: str, report_file_name: str = "out.txt") -> None:
-    with open(data_file_name, "r") as data_file, open(report_file_name, 'w') as report_file:
+def create_report(data_file_name: str,
+                  report_file_name: str) -> None:
+    with (open(data_file_name, "r") as data_file,
+          open(report_file_name, "a") as report_file):
         results = {}
         for line in data_file.readlines():
             columns = line.rstrip("\n").split(",")
@@ -7,12 +9,6 @@ def create_report(data_file_name: str, report_file_name: str = "out.txt") -> Non
             results[operation] = results.get(operation, 0) + int(count)
 
         results["result"] = results["supply"] - results["buy"]
-        print(results)
-        # for item in results:
-        #     print(item)
 
-
-create_report("bananas.csv")
-
-# with open("bananas.csv", "r") as f:
-#     print(f.readlines())
+        for key, value in results.items():
+            report_file.write(f"{key},{value}\n")
