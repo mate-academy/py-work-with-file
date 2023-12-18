7e2beb3fb59e61a379a769c399bc6e67368028dc
Vitalij Chajka <chaika.vitalii@gmail.com>
2023-12-18 19:02:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as infile:
+        res = {"supply": 0, "buy": 0}
+        while True:
+            data = infile.readline()
+            if data == "":
+                break
+            reso = data.split(",")
+            res[reso[0]] += int(reso[1])
+    with open(report_file_name, "w") as outfile:
+        outfile.write(f"supply,{res['supply']}\n")
+        outfile.write(f"buy,{res['buy']}\n")
+        outfile.write(f"result,{res['supply'] - res['buy']}\n")
