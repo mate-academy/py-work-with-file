4665f19e78e57c7eb46d5d7e6659e6d7dc7d874d
Veronika <veronikaboeva935@gmail.com>
2024-01-05 20:39:16 +0100

    Sulution 05.01.2024

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    sum_supply = 0
+    sum_buy = 0
+
+    with open(data_file_name, "r") as f:
+        for row in f:
+            name, amount = row.split(",")
+            if name == "supply":
+                sum_supply += int(amount)
+            elif name == "buy":
+                sum_buy += int(amount)
+
+    with open(report_file_name, "w") as r:
+        r.write(
+            f"supply,{sum_supply}\n"
+            f"buy,{sum_buy}\n"
+            f"result,{sum_supply - sum_buy}\n"
+        )
