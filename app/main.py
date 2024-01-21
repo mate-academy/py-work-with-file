4de3142ab4d226c18344d32e7f24845d36d18f13
barabarinov <barabarinov@gmail.com>
2024-01-21 14:34:21 +0200

    fixed statement

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open(data_file_name, "r") as data_file:
+    with open(data_file_name) as data_file:
         supply = 0
         buy = 0
 
         for line in data_file:
             action, amount = line.split(",")
-            if action.lower() in "supply":
+            if action.lower() == "supply":
                 supply += int(amount)
-            else:
+            elif action.lower() == "buy":
                 buy += int(amount)
+            else:
+                raise ValueError(f"Invalid value {action}")
 
     with open(report_file_name, "w") as report_file:
         report_file.write(f"supply,{supply}\n")
