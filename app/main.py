3ad7740a773d04fd7c517baf39e42c47770296af
Tymofii Skopenko <youmustdeth@gmail.com>
2024-01-24 15:53:03 +0200

    Try to fix it

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     data = {"supply": 0, "buy": 0}
 
-    with (open(data_file_name, "r") as input_csv_data,
-          open(report_file_name, "w") as report_csv_data):
-        for line in input_csv_data:
+    with (open(data_file_name, "r") as inputer,
+          open(report_file_name, "w") as report):
+        for line in inputer:
             if line:
                 transaction_type, quantity = map(str.strip, line.split(","))
                 data[transaction_type] += int(quantity)
 
-        report_csv_data.write(f"supply,{data["supply"]}\n")
-        report_csv_data.write(f"buy,{data["buy"]}\n")
-        report_csv_data.write(f"result,{data["supply"] - data["buy"]}\n")
+        report.write(f'supply,{data["supply"]}\n')
+        report.write(f'buy,{data["buy"]}\n')
+        report.write(f'result,{data["supply"] - data["buy"]}\n')
