1ca77b284884193095b920f54eb22a54e4acf0ad
Nick Illich <nickillich96@gmail.com>
2024-01-24 15:33:48 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import csv
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    total = {"supply": 0, "buy": 0}
+    source_path = f"C:\MA\py-work-with-file\{data_file_name}"
+
+    with open(source_path, "r") as source_file:
+        reader = csv.DictReader(source_file, fieldnames=("key", "value"))
+        for line in reader:
+            total[line["key"]] += int(line["value"])
+        total["result"] = total["supply"] - total["buy"]
+
+    with open(report_file_name, "w", newline="") as report:
+        writer = csv.writer(report)
+        writer.writerows(total.items())
