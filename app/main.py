bc1f238dc845e532b9c28496aebc2bead4640d01
Nick Illich <nickillich96@gmail.com>
2024-01-24 15:39:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,9 +3,8 @@ import csv
 
 def create_report(data_file_name: str, report_file_name: str) -> None:
     total = {"supply": 0, "buy": 0}
-    source_path = f"C:\MA\py-work-with-file\{data_file_name}"
 
-    with open(source_path, "r") as source_file:
+    with open(data_file_name, "r") as source_file:
         reader = csv.DictReader(source_file, fieldnames=("key", "value"))
         for line in reader:
             total[line["key"]] += int(line["value"])
