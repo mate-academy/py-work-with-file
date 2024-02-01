4e4a77669e35365de4c54fbd1560cba55b8d6610
sergey <38cerg12021979@jmal.com>
2024-02-01 07:22:12 +0200

    01_02_24_03

--- app/main.py
+++ app/main.py
@@ -3,16 +3,14 @@ import csv
 
 def create_report(data_file_name: str, report_file_name: str) -> None:
     text_dict = {"supply": 0, "buy": 0, "result": 0}
-    with open(f"/Users/sergey/meta_project"
-              f"/py-work-with-file/{data_file_name}", "r", newline="") as file:
+    with open(f"{data_file_name}", "r", newline="") as file:
         reader = csv.reader(file)
         data = list(reader)
     for row in data:
         if row[0] in text_dict:
             number = text_dict.get(row[0])
             text_dict[row[0]] = number + int(row[1])
-    numeral = text_dict["supply"] - text_dict["buy"]
-    text_dict["result"] = numeral
+    text_dict["result"] = text_dict["supply"] - text_dict["buy"]
     text_list = [[f"{key},{vel}\n"] for key, vel in text_dict.items()]
     line_text = ["".join(i) for i in text_list]
     with open(f"{report_file_name}", "w") as file:
