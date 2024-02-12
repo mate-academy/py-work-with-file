22be2ae3ec608690bad8289cdf6de8d264f08c04
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-02-12 16:29:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with (open(data_file_name, "r") as input_file,
+          open(report_file_name, "w") as output_file):
+        final_data = {}
+        lines = input_file.readlines()
+
+        for line in lines:
+            if line:
+                name, value = line.split(",")
+
+                if name in final_data:
+                    final_data[name] += int(value)
+                else:
+                    final_data[name] = int(value)
+
+        final_data["result"] = final_data["supply"] - final_data["buy"]
+
+        output_file.write("\n".join([f"{key},{value}" for key, value in final_data.items()]))
+
+
