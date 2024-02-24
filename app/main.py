e10184f3b525b29cd878de9c711e46c77d06b6f5
vburkalo <vitaliyburkalo81@gmail.com>
2024-02-24 20:32:44 +0200

    123

--- app/main.py
+++ app/main.py
@@ -2,21 +2,17 @@ import csv
 
 
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    total_supply = 0
-    total_buy = 0
-    with open(data_file_name, "r") as csvfile:
-        csvreader = csv.reader(csvfile)
-        for row in csvreader:
-            if not row:
-                continue
-            operation_type, amount = row[0], int(row[1])
-            if operation_type == "supply":
-                total_supply += amount
-            elif operation_type == "buy":
-                total_buy += amount
-    result = total_supply - total_buy
-    with open(report_file_name, "w", newline="") as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(["supply", total_supply])
-        csvwriter.writerow(["buy", total_buy])
-        csvwriter.writerow(["result", result])
+    data = {"supply": 0, "buy": 0}
+    with open(data_file_name, "r") as file:
+        for line in file.readlines():
+            if line is not None:
+                if line[0] == "s":
+                    data["supply"] += int(line[7:])
+                else:
+                    data["buy"] += int(line[4:])
+    with open(report_file_name, "w") as f:
+        f.write(
+            f"supply,{data['supply']}\n"
+            f"buy,{data['buy']}\n"
+            f"result,{data['supply'] - data['buy']}\n"
+        )
