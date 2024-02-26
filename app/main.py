4ac6cda34292d39af3b1a8b0c91b0dc0b8e3c076
SysoevDmitro <113758312+SysoevDmitro@users.noreply.github.com>
2024-02-26 19:08:43 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
         for line in data_file.readlines():
             data[line.split(",")[0]] += line.split(",")[1]
 
-    with open(report_file_name, "w", newline='') as report_file:
+    with open(report_file_name, "w", newline="") as report_file:
         writer = csv.writer(report_file)
         for key, value in data.items():
             writer.writerow([key, value])
