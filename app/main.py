219ef98189010f03ce3647e0f3baf0b00310e1a3
Dmitro Sysoev <dimasysoev20000@gmail.com>
2024-02-26 19:06:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import csv
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data = {}
+
+    with open(data_file_name, "r") as data_file:
+        reader = csv.reader(data_file)
+        for row in reader:
+            key, value = row
+            data[key] += value
+        for line in data_file.readlines():
+            data[line.split(",")[0]] += line.split(",")[1]
+
+    with open(report_file_name, "w", newline='') as report_file:
+        writer = csv.writer(report_file)
+        for key, value in data.items():
+            writer.writerow([key, value])
