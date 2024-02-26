20076df0431ac72fcda6fb4a3f47b6fb81314a9c
SysoevDmitro <113758312+SysoevDmitro@users.noreply.github.com>
2024-02-26 19:10:44 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
 import csv
+from collections import defaultdict
 
 
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    data = {}
+    data = defaultdict(str)
 
     with open(data_file_name, "r") as data_file:
         reader = csv.reader(data_file)
         for row in reader:
             key, value = row
             data[key] += value
-        for line in data_file.readlines():
-            data[line.split(",")[0]] += line.split(",")[1]
 
     with open(report_file_name, "w", newline="") as report_file:
         writer = csv.writer(report_file)
