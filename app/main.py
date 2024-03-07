afb3b85aa94d666c7de20a542b48256b36e8ec11
Mykyta <nikita.ua1311@gmail.com>
2024-03-08 00:16:31 +0200

    Solution1

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 import csv
 
 
-def create_report(data_file_name: str, report_file_name: str):
-    operation_totals = {'supply': 0, 'buy': 0}
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    operation_totals = {"supply": 0, "buy": 0}
 
-    with open(data_file_name, 'r') as csv_file:
+    with open(data_file_name, "r") as csv_file:
         csv_reader = csv.reader(csv_file)
         for row in csv_reader:
             if row:
@@ -12,11 +12,11 @@ def create_report(data_file_name: str, report_file_name: str):
                 if operation_type in operation_totals:
                     operation_totals[operation_type] += int(amount)
 
-    supply_total = operation_totals['supply']
-    buy_total = operation_totals['buy']
+    supply_total = operation_totals["supply"]
+    buy_total = operation_totals["buy"]
     result = supply_total - buy_total
 
-    with open(report_file_name, 'w') as report_file:
-        report_file.write(f'supply,{supply_total}\n')
-        report_file.write(f'buy,{buy_total}\n')
-        report_file.write(f'result,{result}\n')
+    with open(report_file_name, "w") as report_file:
+        report_file.write(f"supply,{supply_total}\n")
+        report_file.write(f"buy,{buy_total}\n")
+        report_file.write(f"result,{result}\n")
