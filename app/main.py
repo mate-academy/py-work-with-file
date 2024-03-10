8d51e9de339052dcf55ba1ee675dc54b59f79941
skeletronys <mykytcey@gmail.com>
2024-03-10 20:33:55 +0100

    fixed

--- app/main.py
+++ app/main.py
@@ -6,17 +6,11 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     with (open(data_file_name, "r") as cvs_file,
           open(report_file_name, "w") as report_file):
         reader = csv.reader(cvs_file)
-        #writer = csv.writer(report_file)
         for row in reader:
             if row[0] == "supply":
                 supply_buy["supply"] += int(row[1])
             if row[0] == "buy":
                 supply_buy["buy"] += int(row[1])
-        # report_result = [["supply", supply_buy["supply"]],
-        #                  ["buy", supply_buy["buy"]],
-        #                  ["result", supply_buy["supply"] - supply_buy["buy"]]]
-        # for row in report_result:
-        #     writer.writerow(row)
         report_file.write(f"supply,{supply_buy["supply"]}\n")
         report_file.write(f"buy,{supply_buy["buy"]}\n")
         report_file.write(f"result,"
