218beb16159ecffc5acae11582cddf427a45f13d
Yaryna Hryniv <ygrinsv@gmail.com>
2024-03-16 12:59:57 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,23 +1,17 @@
-import csv
-
-
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    supply_total = 0
-    buy_total = 0
-
+    supply = 0
+    buy = 0
     with open(data_file_name, "r") as f:
         for line in f:
             row = line.split(",")
             if row:
-                operation_type, amount = row
-                if operation_type == "supply":
-                    supply_total += int(amount)
-                elif operation_type == "buy":
-                    buy_total += int(amount)
+                if row[0] == "supply":
+                    supply += int(row[1])
+                elif row[0] == "buy":
+                    buy += int(row[1])
+    result = supply - buy
 
-    result = supply_total - buy_total
-
-    with open(report_file_name, mode="w") as report_file:
-        report_file.write(f"supply,{supply_total}\n")
-        report_file.write(f"buy,{buy_total}\n")
-        report_file.write(f"result,{result}\n")
+    with open(report_file_name, "w") as f:
+        f.write(f"supply,{supply}\n")
+        f.write(f"buy,{buy}\n")
+        f.write(f"result,{result}\n")
