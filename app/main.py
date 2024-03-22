2bbad6e789cb6c400dbb220f738fd3f1523726b8
vova-stoliar <vova.stoliar@overdose.digital>
2024-03-22 20:52:53 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,33 @@
-# write your code here
+import csv
+
+
+def create_report(data_file_name: str, report_file_name: str):
+    with (
+        open(data_file_name, "r") as data_file_csvfile,
+        open(report_file_name, "w") as report_csvfile,
+    ):
+        csvreader = csv.reader(data_file_csvfile)
+
+        total_operations_type_amount = {}
+
+        for [operations_type, amount] in csvreader:
+            operations_type_amount = (
+                total_operations_type_amount.get(operations_type) or 0
+            )
+
+            total_operations_type_amount[operations_type] = (
+                operations_type_amount + int(amount)
+            )
+
+        writer = csv.writer(report_csvfile)
+
+        writer.writerow(["supply", total_operations_type_amount["supply"]])
+        writer.writerow(["buy", total_operations_type_amount["buy"]])
+
+        writer.writerow(
+            [
+                "result",
+                total_operations_type_amount["supply"]
+                - total_operations_type_amount["buy"],
+            ]
+        )
