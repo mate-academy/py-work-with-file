c6224190de396f8152469e3fd0b51ce12d350443
Tymur Shmorhun <shmorguntimur@gmail.com>
2024-03-25 18:40:34 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     supply = 0
     buy = 0
-    with open(data_file_name, "r") as csv_file, open(report_file_name, "w") as file:
+
+    with open(data_file_name, "r") as csv_file:
         for line in csv_file:
             line = line.split(",")
             if line[0] == "supply":
@@ -9,7 +10,5 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             if line[0] == "buy":
                 buy += int(line[1])
 
+    with open(report_file_name, "w") as file:
         file.write(f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}")
-
-    with open(report_file_name, "r") as file:
-        print(file.read())
