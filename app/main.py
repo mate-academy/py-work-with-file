15f7aef6f95399694a596e945deabdff3ce7cfc9
Valentin Pozniak <waliker448@gmail.com>
2024-03-27 23:30:34 +0200

    'fix_2'

--- app/main.py
+++ app/main.py
@@ -10,6 +10,10 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             else:
                 result[parameter] += int(data)
 
-        report_file.write("supply," + f"{result["supply"]}\n")
-        report_file.write("buy," + f"{result["buy"]}\n")
-        report_file.write("result," + f"{result["supply"] - result["buy"]}\n")
+        report_file.write("supply," + str(result["supply"]) + "\n")
+        report_file.write("buy," + str(result["buy"]) + "\n")
+        report_file.write(
+            "result,"
+            + str(result["supply"] - result["buy"])
+            + "\n"
+        )
