84a6d306b762bcb5bcb0e02f1e24b54cd438de02
Valentin Pozniak <waliker448@gmail.com>
2024-03-27 23:18:15 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+
+    with (open(data_file_name, "r") as data_file,
+          open(report_file_name, "w") as report_file):
+        result = {}
+        for line in data_file:
+            parameter, data = line.split(",")
+            if parameter not in result:
+                result[parameter] = int(data)
+            else:
+                result[parameter] += int(data)
+
+        report_file.write(f"{"supply"},{result["supply"]}\n")
+        report_file.write(f"{"buy"},{result["buy"]}\n")
+        report_file.write(f"result,{result["supply"] - result["buy"]}\n")
