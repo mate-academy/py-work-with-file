d7225d1b624d6e54429aecf8d7e6daabbcd3e127
Maksym Hekalo <maximgekalo@gmail.com>
2024-03-30 17:11:07 +0200

    fix concatenate

--- app/main.py
+++ app/main.py
@@ -17,6 +17,9 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             report_dict[log_key] = int(log_value)
         report_dict["result"] = report_dict["supply"] - report_dict["buy"]
         with open(report_file_name, "w") as report_file:
-            report_file.write("supply," + str(report_dict.get("supply")) + "\n")
-            report_file.write("buy," + str(report_dict.get("buy")) + "\n")
-            report_file.write("result," + str(report_dict.get("result")) + "\n")
+            report_file.write(
+                "supply," + str(report_dict.get("supply")) + "\n")
+            report_file.write(
+                "buy," + str(report_dict.get("buy")) + "\n")
+            report_file.write(
+                "result," + str(report_dict.get("result")) + "\n")
