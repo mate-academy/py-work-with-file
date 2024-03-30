e38cf1ccdd0689b8be150555fb82a01ca42ad58c
Mikhail Yemets <mikhailyemets.pl@gmail.com>
2024-03-30 19:01:10 +0100

    idk how it works

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    with (open(data_file_name, "r")
+          as file_in, open(report_file_name, "a") as file_out):
+
+        data = file_in.read()
+        new_str = data.replace("\n", " ").replace(",", " ")
+        lst = new_str.split(" ")
+
+        supply = 0
+        buy = 0
+        for i in range(len(lst) - 1):
+            if lst[i] == "supply":
+                supply += int(lst[i + 1])
+            if lst[i] == "buy":
+                buy += int(lst[i + 1])
+        report = f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n"
+        file_out.write(report)
