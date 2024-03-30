afea905df384316a40bf454f18cac759d7eb4a0a
Maksym Hekalo <maximgekalo@gmail.com>
2024-03-30 17:21:11 +0200

    fix file path

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 
 def create_report(data_file_name: str, report_file_name: str) -> None:
     report_dict = dict()
-    with open(f"../{data_file_name}", "r") as file:
+    with open(data_file_name, "r") as file:
         data = list(
             map(
                 lambda value: {value.split(",")[0]: value.split(",")[1]},
