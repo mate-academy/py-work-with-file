f343b3e5a1322ee2d6915ebdb4c62dc40c0af736
Illia <ilaruslanovich7@gmail.com>
2024-03-31 13:56:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as first:
+        read = first.read()
+        list_first = read.split()
+        supply_count = 0
+        buy_count = 0
+        resulto = 0
+
+        for i in list_first:
+            operatcie, quantity = i.split(",")
+            if operatcie == "supply":
+                supply_count += int(quantity)
+            elif operatcie == "buy":
+                buy_count += int(quantity)
+
+        resulto = supply_count - buy_count
+        text_to_file = "".join(("supply,", str(supply_count), "\n",
+                                "buy,", str(buy_count), "\n",
+                                "result,", str(resulto), "\n"))
+
+        with open(report_file_name, "w") as second:
+            second.write(text_to_file)
