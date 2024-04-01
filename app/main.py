f0ba54ce44fef76c085d2df4dddc2e9e1be3bfb3
Lev <dkotkod@gmail.com>
2024-04-01 12:03:06 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import csv
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply_total = 0
+    buy_total = 0
+    with open(data_file_name, newline="") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if row:
+                operation_type, amount = row
+                if operation_type == "supply":
+                    supply_total += int(amount)
+                elif operation_type == "buy":
+                    buy_total += int(amount)
+
+    result = supply_total - buy_total
+
+    with open(report_file_name, "w") as reportfile:
+        reportfile.write(f'supply,{supply_total}\n')
+        reportfile.write(f'buy,{buy_total}\n')
+        reportfile.write(f'result,{result}\n')
