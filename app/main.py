ea78f95b382566a21c7fbb3845c301ccc4f1f8dc
Vladyslav Prymas <prymas.vladyslav@gmail.com>
2024-04-02 00:04:53 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    report_dict = {}
+    with open(data_file_name, "r") as file_in:
+        for line in file_in.readlines():
+            if line == "":
+                continue
+            line_list = line.split(",")
+            if line_list[0] not in report_dict:
+                report_dict[line_list[0]] = int(line_list[1])
+            else:
+                report_dict[line_list[0]] += int(line_list[1])
+    with open(report_file_name, "w") as file_out:
+        for ind, key in report_dict.items():
+            file_out.write(f"{ind},{key}")
