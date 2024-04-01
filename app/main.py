4650d33563e22cd42aa926ca8c219394f1d53c5b
Vladyslav Prymas <prymas.vladyslav@gmail.com>
2024-04-02 00:12:22 +0300

    Solution3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+from collections import OrderedDict
+
+
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    report_dict = {}
+    report_dict = OrderedDict()
     with open(data_file_name, "r") as file_in:
         for line in file_in.readlines():
             if line == "":
@@ -9,6 +12,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
                 report_dict[line_list[0]] = int(line_list[1])
             else:
                 report_dict[line_list[0]] += int(line_list[1])
+
     with open(report_file_name, "w") as file_out:
         for ind, key in report_dict.items():
             file_out.write(f"{ind},{key}\n")
