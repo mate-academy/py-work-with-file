7ce652fcb4bdf4a7f8e9e6ac4f9d50319e1bbb3a
Vladyslav Prymas <prymas.vladyslav@gmail.com>
2024-04-02 14:22:06 +0300

    Solution final

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
-from collections import OrderedDict
-
-
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    report_dict = OrderedDict()
+    report_dict = {"supply": 0, "buy": 0}
     with open(data_file_name, "r") as file_in:
-        for line in file_in.readlines():
+        for line in file_in:
             if line == "":
                 continue
             line_list = line.split(",")
@@ -12,7 +9,8 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
                 report_dict[line_list[0]] = int(line_list[1])
             else:
                 report_dict[line_list[0]] += int(line_list[1])
-
+    result = report_dict["supply"] - report_dict["buy"]
     with open(report_file_name, "w") as file_out:
         for ind, key in report_dict.items():
             file_out.write(f"{ind},{key}\n")
+        file_out.write(f"result,{result}\n")
