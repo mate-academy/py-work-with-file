b3b43bd82b33dd18f60251ef12dc8628467a3a03
siaserb <assiya.serb@gmail.com>
2024-04-02 03:19:30 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    operations = [0, 0]
+    with open(data_file_name, "r") as input_file, open(report_file_name, "w") as output_file:
+        data = input_file.read().split("\n")
+        for line in data:
+            if line:
+                operation_type, amount = line.split(",")
+                if operation_type == "supply":
+                    operations[0] += int(amount)
+                elif operation_type == "buy":
+                    operations[1] -= int(amount)
+        output_file.write(f"supply,{operations[0]}\n"
+                          f"buy,{abs(operations[1])}\n"
+                          f"result,{sum(operations)}\n")
