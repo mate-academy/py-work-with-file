3999a4df8921e4796dd3388de86964ded07f7c3d
Volkov <volkovyegorandriiovich@gmail.com>
2024-04-11 05:25:31 +0300

    merch_clctor_v1.0

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+def create_report(data_file_name: str,
+                  report_file_name: str) -> None:
+    file_1 = f"{data_file_name}"
+    file_2 = f"{report_file_name}"
+    with open(file_1, "r") as data, open(file_2, "w") as report:
+        buy = 0
+        supply = 0
+        for line in data.readlines():
+            action = line.rstrip("\n").split(",")
+            if action[0] == "supply":
+                supply += int(action[1])
+            if action[0] == "buy":
+                buy += int(action[1])
+        result = supply - buy
+        report.write(f"supply,{supply}\n"
+                     f"buy,{buy}\n"
+                     f"result,{result}\n")
+
+
+# all tests pass, but on practice we need to use data file names in
+# following format:
+# "../apples.csv"
