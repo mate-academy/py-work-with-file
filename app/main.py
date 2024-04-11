6cce139e5b171d38d7e7d30ca4b7520798e2b637
Ruslan CHavus <chruslan4@gmail.com>
2024-04-11 17:05:00 +0300

    Fix result

--- app/main.py
+++ app/main.py
@@ -1,15 +1,19 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    operations = dict()
+    operations = {"supply": 0, "buy": 0}
     with open(data_file_name, "r") as input_file:
         for line in input_file.readlines():
             line_mapped = line.split(",")
             if len(line_mapped) == 2:
                 line_mapped[1] = line_mapped[1][:-1]
-                if line_mapped[0] not in operations:
-                    operations[line_mapped[0]] = int(line_mapped[1])
-                else:
-                    operations[line_mapped[0]] += int(line_mapped[1])
+                if line_mapped[0] == "supply":
+                    operations["supply"] += int(line_mapped[1])
+                elif line_mapped[0] == "buy":
+                    operations["buy"] += int(line_mapped[1])
+    operations["result"] = operations["supply"] - operations["buy"]
 
     with open(report_file_name, "w") as out_file:
         for operation, amount in operations.items():
             out_file.write(f"{operation},{amount}\n")
+
+
+# create_report("grapes.csv", "report.csv")
