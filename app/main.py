0dd39bd7f65e501ccf4cb7e6794e4d01c929805d
Klishch Nazar <klishch65@gmail.com>
2024-04-13 17:14:02 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    buy = 0
-    supply = 0
+    buy, supply = 0, 0
     with open(data_file_name, "r") as source_file, open(report_file_name, "w") as report_file:
         for data in source_file:
             op_data, value = data.split(",")
