e2174d6ab7b1229256b86468e060966150e508b0
dvkim28 <d.villarionovich@gmail.com>
2024-04-15 00:11:30 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -3,10 +3,10 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     buy = 0
     with open(data_file_name, "r") as file:
         for data in file:
-            action, value = data.split(",")
-            if action == "buy":
+            line, value = data.split(",")
+            if line == "buy":
                 buy += int(value)
-            else:
+            if line == "supply":
                 supply += int(value)
 
     with open(report_file_name, "w") as file:
