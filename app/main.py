0266ea98b42cef2fe75ba8909930e83f9747d018
Olena Poplias <poplyasitasony@gmail.com>
2024-05-01 12:25:34 -0400

    "Solution"

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+def create_report(
+        data_file_name: str,
+        report_file_name: str
+) -> None:
+
+    with (
+        open(data_file_name, "r") as data_file,
+        open(report_file_name, "w") as report_file
+    ):
+
+        data = data_file.read().splitlines()
+        supply = 0
+        buy = 0
+
+        for un in data:
+            unit = un.split(",")
+            if unit[0] == "supply":
+                supply += int(unit[1])
+            if unit[0] == "buy":
+                buy += int(unit[1])
+
+        result = supply - buy
+
+        report_file.write(f"supply, {supply}\nbuy, {buy}\nresult, {result}\n")
