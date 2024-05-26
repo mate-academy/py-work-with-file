fdcef8fbcc993d802982d4a99b77459efb896d45
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-05-26 13:10:06 +0300

    fix 2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open("../" + data_file_name, "r") as data_file:
+    with open(data_file_name, "r") as data_file:
         data_dict = {"supply": 0, "buy": 0}
         while True:
             cur_row = data_file.readline()
