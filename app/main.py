184c8986cb7b6408ad18c8136eab130a8acd5410
Yurii Yurchenko <fuceqwertyt@gmail.com>
2024-06-03 16:34:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with (open(data_file_name, "r") as file_in,
+          open(report_file_name, "w") as file_out):
+        data = {}
+        for line in file_in:
+            if line == "":
+                break
+            name, amount = line.split(",")
+            if name not in data:
+                data[name] = int(amount)
+            else:
+                data[name] += int(amount)
+        data["result"] = data["supply"] - data["buy"]
+
+        file_out.write(f"supply,{data["supply"]}\n")
+        file_out.write(f"buy,{data["buy"]}\n")
+        file_out.write(f"result,{data["result"]}\n")
