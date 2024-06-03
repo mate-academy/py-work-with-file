6df183748248dd960a57b9296a0ea9a319f06ee1
Іван Білейчук <ivan.bileichuk@gmail.com>
2024-06-03 17:49:03 +0300

    Solution_V2

--- app/main.py
+++ app/main.py
@@ -15,13 +15,13 @@ def create_report(data_file_name: str,
             operation_type, amount = row
             amount = int(amount)
 
-            if operation_type == "suply":
+            if operation_type == "supply":
                 total_supply += amount
             elif operation_type == "buy":
                 total_buy += amount
 
     result = total_supply - total_buy
     with open(report_file_name, "w") as report_file:
-        report_file.write(f"suply,{total_supply}\n")
+        report_file.write(f"supply,{total_supply}\n")
         report_file.write(f"buy,{total_buy}\n")
         report_file.write(f"result,{result}\n")
