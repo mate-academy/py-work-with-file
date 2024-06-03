0f7b5f79811b399f147caa48b6804daa23a44ee2
Іван Білейчук <ivan.bileichuk@gmail.com>
2024-06-03 17:45:14 +0300

    Solution_V1

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def create_report(data_file_name: str,
     total_supply = 0
     total_buy = 0
 
-    with open(data_file_name, 'r') as csv_file:
+    with open(data_file_name, "r") as csv_file:
         csv_reader = csv.reader(csv_file)
         for row in csv_reader:
             if not row:
@@ -15,13 +15,13 @@ def create_report(data_file_name: str,
             operation_type, amount = row
             amount = int(amount)
 
-            if operation_type == 'supply':
+            if operation_type == "suply":
                 total_supply += amount
-            elif operation_type == 'buy':
+            elif operation_type == "buy":
                 total_buy += amount
 
     result = total_supply - total_buy
-    with open(report_file_name, 'w') as report_file:
-        report_file.write(f'supply,{total_supply}\n')
-        report_file.write(f'buy,{total_buy}\n')
-        report_file.write(f'result,{result}\n')
+    with open(report_file_name, "w") as report_file:
+        report_file.write(f"suply,{total_supply}\n")
+        report_file.write(f"buy,{total_buy}\n")
+        report_file.write(f"result,{result}\n")
