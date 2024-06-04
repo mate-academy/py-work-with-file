60b99952fca9a7b7bf23348fca037723f5933333
Oleh Deshko <kochaminny@gmail.com>
2024-06-04 17:02:46 +0300

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -1,18 +1,11 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    total_supply = 0
-    total_buy = 0
-
+    data_dict = {"supply": 0, "buy": 0}
     with open(data_file_name, "r") as file:
-        for line in file:
-            line_split = line.split(",")
-            operation, value = line_split[0], int(line_split[1])
-            if operation == "supply":
-                total_supply += value
-            else:
-                total_buy += value
-    result = total_supply - total_buy
-
-    with open(report_file_name, "w") as report:
-        report.write(f"supply,{total_supply}\n")
-        report.write(f"buy,{total_buy}\n")
-        report.write(f"result,{result}\n")
+        for line in file.readlines():
+            key, value = line.split(",")
+            value = int(value)
+            data_dict[key] += value
+    with open(report_file_name, "w") as file:
+        file.write(f"supply,{data_dict['supply']}\n")
+        file.write(f"buy,{data_dict['buy']}\n")
+        file.write(f"result,{data_dict['supply'] - data_dict['buy']}\n")
