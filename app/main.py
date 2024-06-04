a52d627b0112c46776d449223625ecbebf7c0192
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-06-04 17:37:22 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    file = open("data_file_name", "r")
+    file.read()
+    file.close()
+
+    with open("report_file_name", "w") as report_file:
+        for line in file:
+            report_file.write(line + "\n")
+
+    if __name__ == "__main__":
+        main()
