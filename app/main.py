d652643cd5b991a149746c8867d6dccba43ec052
unlie9 <unlie.devops9@gmail.com>
2024-06-04 14:25:55 +0300

    sol

--- app/main.py
+++ app/main.py
@@ -1,24 +1,17 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open(data_file_name, "r") as file:
-        info = {
-            "supply": 0,
-            "buy": 0,
-            "result": 0
-        }
+    info = {
+        "supply": int(0),
+        "buy": int(0),
+        "result": int(0)
+    }
 
-        lines = []
-        for line in file.readline():
-            lines.append(line.split(","))
-
-        for key, value in lines:
-            if key == "supply":
-                info["supply"] += value
-            if key == "buy":
-                info["buy"] += value
-
-        info["result"] = info["supply"] - info["buy"]
+    with open(data_file_name) as file:
+        for line in file.readlines():
+            key, value = line.split(",")
+            value = int(value)
+            info[key] += value
 
     with open(report_file_name, "w") as file:
         file.write(f'supply, {info["supply"]}\n')
         file.write(f'buy, {info["buy"]}\n')
-        file.write(f'result, {info["result"]}\n')
+        file.write(f'result, {info["supply"] - info["buy"]}\n')
