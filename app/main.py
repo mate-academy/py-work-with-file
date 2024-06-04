fe8ee58ddd8f29875c37d13216ffce0be9937b1b
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-06-04 21:51:29 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open("data_file_name", "r") as file:
-        content = file.read()
+    my_dict = {"supply": 0,
+               "buy": 0}
 
-    with open("report_file_name", "w") as report_file:
+    with open(data_file_name, "r") as file:
+        content = file.readlines()
+
+    with open(report_file_name, "w") as report_file:
         for line in content:
-            report_file.write(line + "\n")
+            operation, quantity = line.split(",")
+            if operation in my_dict:
+                my_dict[operation] += int(quantity)
 
-    # if __name__ == "__main__":
-    #     main()
+        my_dict["result"] = my_dict["supply"] - my_dict["buy"]
+
+        for key, value in my_dict.items():
+            report_file.write(",".join([key, str(value)]) + "\n")
