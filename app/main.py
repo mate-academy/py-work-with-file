bdac93f44b54d086412c2b0d056eea796c797997
timur <stukantimur811@gmail.com>
2024-06-09 19:41:03 +0300

    some work with files

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> str:
+    fl = open(data_file_name)
+
+    supply = 0
+    buy = 0
+    for line in fl:
+        item, count = line.split(",")
+        if item == "supply":
+            supply += int(count[:-1])
+        else:
+            buy += int(count[:-1])
+    fl.close()
+
+    fl = open(report_file_name, "w")
+    fl.write(f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n")
+    fl.close()
