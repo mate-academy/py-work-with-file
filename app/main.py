5275358ede488594eab8b613c65db8c337367243
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-06-13 14:14:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def create_report(data_file_name: str, report_file_name: str):
+def create_report(data_file_name: str, report_file_name: str) -> None:
     with (open(data_file_name, "r") as file_data,
           open(report_file_name, "w") as file_report):
         supply = 0
@@ -9,4 +9,4 @@ def create_report(data_file_name: str, report_file_name: str):
             elif line.split(",")[0] == "buy":
                 supply += line.split(",")[1]
         result = supply - buy
-        file_report.write(f"supply,{supply}\nbuy,{buy}\nresult,{result}")
+        file_report.write(f"supply, {supply}\nbuy, {buy}\nresult, {result}")
