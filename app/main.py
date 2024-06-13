e00fcf7bda0ee63fa7ce03701c6ac751ed001b68
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-06-13 15:36:45 +0300

    Solution p2

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with (open(data_file_name, "r") as file_data,
-          open(report_file_name, "w") as file_report):
-        supply = 0
-        buy = 0
-        for line in file_data:
-            if line.split(",")[0] == "supply":
-                supply += line.split(",")[1]
-            elif line.split(",")[0] == "buy":
-                supply += line.split(",")[1]
-        result = supply - buy
-        file_report.write(f"supply, {supply}\nbuy, {buy}\nresult, {result}")
+    data = {
+        "supply": 0,
+        "buy": 0,
+        "result": 0
+    }
+    with open(data_file_name, "r") as file:
+        line = file.readline()
+        while line:
+            operation, amount = line.split(",")
+            data[operation] += amount
+            line = file.readline().strip()
+        data["result"] = data["supply"] - data["buy"]
+
+    with open(report_file_name, "w") as file:
+        for operation, amount in data.items:
+            file.write(",".join((operation, str(amount))) + "\n")
