b08b0368531e32a4c183c96c50aa5793717fb3fe
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-06-13 15:39:14 +0300

    Solution p3

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
-def create_report(data_file_name: str, report_file_name: str) -> None:
+def create_report(data_file_name: str,
+                  report_file_name: str) -> None:
     data = {
         "supply": 0,
         "buy": 0,
         "result": 0
     }
+
     with open(data_file_name, "r") as file:
-        line = file.readline()
+        line = file.readline().strip()
         while line:
             operation, amount = line.split(",")
-            data[operation] += amount
+            data[operation] += int(amount)
             line = file.readline().strip()
         data["result"] = data["supply"] - data["buy"]
 
     with open(report_file_name, "w") as file:
-        for operation, amount in data.items:
+        for operation, amount in data.items():
             file.write(",".join((operation, str(amount))) + "\n")
