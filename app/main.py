0286912ea7164d90a184d29db2ff19417c247e04
Artem <mikhailetc.artem@gmail.com>
2024-06-13 15:20:29 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,21 +1,20 @@
-import csv
-
-
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    operation_totals = {"supply": 0, "buy": 0}
+    with open(data_file_name, "r") as data_file:
+        result = {}
+        for line in data_file:
+            temp = line.strip().split(",")
+            process, count = temp[0], int(temp[1])
+            if process in result:
+                result[process] += count
+            else:
+                result[process] = count
 
-    with open(data_file_name, "r") as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            operation_type, amount = row
-            if operation_type in operation_totals:
-                operation_totals[operation_type] += int(amount)
-
-    result = operation_totals["supply"] - operation_totals["buy"]
-
-    with open(report_file_name, "w") as reportfile:
-        reportfile.write(
-            f"supply,{operation_totals['supply']}\n"
-            f"buy,{operation_totals['buy']}\n"
-            f"result,{result}\n"
-        )
+    with open(report_file_name, "w") as result_file:
+        result_count = 0
+        for process, count in result.items():
+            result_file.write(f"{process},{count}\n")
+            if process == "supply":
+                result_count += count
+            elif process == "buy":
+                result_count -= count
+        result_file.write(f"result,{result_count}\n")
