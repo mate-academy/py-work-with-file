4cbcfdcbd1bb886cd1ef5be0d8a07d792fcb9a33
KovbasinskyV <kovbasinskyvv@gmail.com>
2024-06-23 19:52:41 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    result = {}
+
+    with (open(data_file_name) as data_file):
+        for line in data_file:
+            type_info, amount = line.split(",")
+            result[type_info] = result.get(type_info, 0) + int(amount)
+
+    with open(report_file_name, "a") as report_file:
+        report_file.write(f"supply,{result["supply"]}\n")
+        report_file.write(f"buy,{result["buy"]}\n")
+        report_file.write(f"result,{result["supply"] - result["buy"]}\n")
