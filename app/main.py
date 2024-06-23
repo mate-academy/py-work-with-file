3529547b53cca302d849395d8edf6964d73ab1e2
KovbasinskyV <kovbasinskyvv@gmail.com>
2024-06-23 20:20:07 +0300

    solution1_fix2

--- app/main.py
+++ app/main.py
@@ -5,8 +5,9 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
         for line in data_file:
             type_info, amount = line.split(",")
             result[type_info] = result.get(type_info, 0) + int(amount)
+    difference = result["supply"] - result["buy"]
 
     with open(report_file_name, "a") as report_file:
-        report_file.write("supply," + f"{result["supply"]}\n")
-        report_file.write("buy," + f"{result["buy"]}\n")
-        report_file.write("result," + f"{result["supply"] - result["buy"]}\n")
+        report_file.write("supply," + str(result["supply"]) + "\n")
+        report_file.write("buy," + str(result["buy"]) + "\n")
+        report_file.write("result," + str(difference) + "\n")
