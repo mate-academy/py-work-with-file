acf019acaaea7424f51672a26f7b6e55f234d711
maksym_korniev <maksymkorniev88@gmail.com>
2024-07-16 14:35:51 +0300

    refactor for linux

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     supply = 0
     buy = 0
-    with open("../" + data_file_name, "r") as read_file:
+    with open(data_file_name, "r") as read_file:
         for line in read_file:
             line_data = line.split(",")
             if line_data[0] == "buy":
