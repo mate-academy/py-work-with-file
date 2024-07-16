2ed034fa46bd046138e40e5577aea1037ddf96ff
viktor-77 <vumanskiy0664041341@gmail.com>
2024-07-17 01:35:30 +0300

    Resolved

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    data_file = open("../" + data_file_name)
+    data_file = open(data_file_name)
     supply = 0
     buy = 0
     while file_line := data_file.readline().strip():
