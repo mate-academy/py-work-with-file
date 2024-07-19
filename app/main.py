a0903c967df4da627ba1c340e4f20034430cb651
PyCore23 <143321914+PyCore23@users.noreply.github.com>
2024-07-19 19:35:53 +0300

    flake8 bugs

--- app/main.py
+++ app/main.py
@@ -1,18 +1,15 @@
 def create_report(data_file_name: str, report_file_name: str):
     from csv import reader
-    
+
     sup, buy = 0, 0
-    with open(data_file_name, 'r') as file:
+
+    with open(data_file_name, "r") as file:
         for op, val in reader(file):
             match op:
                 case "supply":
                     sup += int(val)
                 case "buy":
                     buy += int(val)
-    
-    with open(report_file_name, '+a') as report:
-        report.write(
-           f"supply,{sup}\n"
-           f"buy,{buy}\n"
-           f"result,{sup - buy}\n"
-        )
+
+    with open(report_file_name, "+a") as report:
+        report.write(f"supply,{sup}\nbuy,{buy}\nresult,{sup - buy}\n")
