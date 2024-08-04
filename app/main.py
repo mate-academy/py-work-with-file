ba66dc4cd5b2f206849e714b637057a2ae7cb188
Yakiv Lobach <lobachyakiv@gmail.com>
2024-08-04 18:31:22 +0300

    first try

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> str:
+    account = {"supply": 0, "buy": 0}
+    with open(data_file_name, "r") as file:
+        for line in file:
+            line = line.strip()
+            if not line:
+                continue
+            parts = line.split(",")
+            if len(parts) != 2:
+                continue
+            key, value = parts
+            account[key] += int(value)
+    account["result"] = account["supply"] - account["buy"]
+
+    with open(report_file_name, "w") as file:
+        for key, value in account.items():
+            file.write(f"{key},{value}\n")
+    return f"Report created: {report_file_name}"
