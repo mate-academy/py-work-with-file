6136b656cbd414fc06897d4a10915b10d6c0a3da
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-05 09:43:33 +0300

    add method create_report

--- app/main.py
+++ app/main.py
@@ -18,8 +18,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
                     buy += amount
 
         result = (supply - buy)
-        with open(report_file_name, 'w') as report_file:
-            report_file.write(f'supply,{supply}\n')
-            report_file.write(f'buy,{buy}\n')
-            report_file.write(f'result,{result}\n')
-
+        with open(report_file_name, "w") as report_file:
+            report_file.write(f"supply,{supply}\n")
+            report_file.write(f"buy,{buy}\n")
+            report_file.write(f"result,{result}\n")
