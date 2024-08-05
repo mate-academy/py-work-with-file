35118e67e483a384e9b8864cfe68d685f06fecbf
achalaidiuk <sasha.treyser90@gmail.com>
2024-08-05 15:57:10 +0300

    Task Solution

--- app/main.py
+++ app/main.py
@@ -1,20 +1,34 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    data = {"supply": 0, "buy": 0}
-
-    file_to_read = open(data_file_name)
-
-    for string in file_to_read:
-        if string:
-            operation_type, amount = string.split(",")
-            if operation_type == "supply":
-                data["supply"] += int(amount)
-            else:
-                data["buy"] += int(amount)
-
-    file_to_read.close()
-
-    with open(report_file_name, "a") as report:
-
-        report.write(f"supply,{data["supply"]}\n")
-        report.write(f"buy,{data["buy"]}\n")
-        report.write(f"result,{data["supply"] - data["buy"]}\n")
+    try:
+        file_to_read = open(data_file_name)
+    except FileNotFoundError:
+        print("No such file in the directory")
+    except PermissionError:
+        print("You do not have an access to this file")
+    else:
+        supply = 0
+        buy = 0
+        for line in file_to_read:
+            if line:
+                splitted = line.split(",")
+                if splitted[0] == "supply":
+                    supply += int(splitted[1])
+                else:
+                    buy += int(splitted[1])
+        result = supply - buy
+    finally:
+        file_to_read.close()
+    try:
+        file_to_write = open(report_file_name, "w")
+    except FileNotFoundError:
+        print("No such file in the directory")
+    except PermissionError:
+        print("You do not have an access to this file")
+    else:
+        file_to_write.write(
+            "supply" + "," + str(supply) + "\n"
+            + "buy" + "," + str(buy) + "\n"
+            + "result" + "," + str(result) + "\n"
+        )
+    finally:
+        file_to_write.close()
