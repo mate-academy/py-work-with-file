de9db4e678b534e04617cf5a33047d01237e431d
achalaidiuk <sasha.treyser90@gmail.com>
2024-08-05 15:50:04 +0300

    Task Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data = {"supply": 0, "buy": 0}
+
+    file_to_read = open(data_file_name)
+
+    for string in file_to_read:
+        if string:
+            operation_type, amount = string.split(",")
+            if operation_type == "supply":
+                data["supply"] += int(amount)
+            else:
+                data["buy"] += int(amount)
+
+    file_to_read.close()
+
+    with open(report_file_name, "a") as report:
+
+        report.write(f"supply,{data["supply"]}\n")
+        report.write(f"buy,{data["buy"]}\n")
+        report.write(f"result,{data["supply"] - data["buy"]}\n")
