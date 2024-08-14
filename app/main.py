d8b444e94024f1cd148362a6908010bde127ac3a
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-08-14 17:24:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_file = open(data_file_name, "r")
+
+    data = [line.replace("\n", "").split(",") for line in data_file]
+
+    result_dict = {}
+
+    for el in data:
+        if el[0] in result_dict:
+            result_dict[el[0]] += int(el[1])
+        else:
+            result_dict[el[0]] = int(el[1])
+
+    data_file.close()
+    o_file = open(report_file_name, "x")
+
+    o_file.write("supply," + f"{result_dict["supply"]}\n")
+    o_file.write("buy," + f"{result_dict["buy"]}\n")
+    o_file.write("result," + f"{result_dict["supply"] - result_dict["buy"]}\n")
+
+    o_file.close()
