3859daf1c61ab2dad2db8bdb3d736ce9cbf71b5f
user <graban2253@gmail.com>
2024-08-15 14:27:47 +0300

    Finished

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
         for row in reader:
             result[row[0]] = row[1]
 
-    with open(report_file_name, "w", newline='') as file2:
+    with open(report_file_name, "w", newline="") as file2:
         writer = csv.writer(file2)
         for name, value in result.items():
             writer.writerow([name, value])
