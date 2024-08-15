c34366616d9838cc44cf8813896323d555b5b8d9
user <graban2253@gmail.com>
2024-08-15 14:26:38 +0300

    Finished

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import csv
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+
+    result = {}
+    with open(data_file_name, "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            result[row[0]] = row[1]
+
+    with open(report_file_name, "w", newline='') as file2:
+        writer = csv.writer(file2)
+        for name, value in result.items():
+            writer.writerow([name, value])
