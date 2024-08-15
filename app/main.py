989fd11024bff8823e525dcea6d8ec2d2b2325b8
user <graban2253@gmail.com>
2024-08-15 14:33:03 +0300

    Finished

--- app/main.py
+++ app/main.py
@@ -2,14 +2,17 @@ import csv
 
 
 def create_report(data_file_name: str, report_file_name: str) -> None:
-
     result = {}
+
     with open(data_file_name, "r") as file:
         reader = csv.reader(file)
         for row in reader:
-            result[row[0]] = row[1]
+            action, value = row[0], int(row[1])
+            result[action] = value
+
+    result["result"] = result.get("supply", 0) - result.get("buy", 0)
 
     with open(report_file_name, "w", newline="") as file2:
         writer = csv.writer(file2)
-        for name, value in result.items():
-            writer.writerow([name, value])
+        for key in ["supply", "buy", "result"]:
+            writer.writerow([key, result.get(key, 0)])
