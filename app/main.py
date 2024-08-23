69dad2078941de8a4ab6ae821f395f34c8d4a05a
Andrii Uvarov <aaanyyy@gmail.com>
2024-08-23 20:19:29 +0200

    complete code

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as in_f:
+        data_dict = {}
+        for line in in_f.readlines():
+            if line == "" or line == "\n":
+                break
+            key, value = line.split(",")
+            if key in data_dict.keys():
+                data_dict[key] = int(data_dict[key]) + int(value)
+            else:
+                data_dict[key] = value
+
+    with open(report_file_name, "w") as out_f:
+        out_f.write(f"supply, {data_dict["supply"]}\n")
+        out_f.write(f"buy, {data_dict["buy"]}\n")
+        result = int(data_dict["supply"]) - int(data_dict["buy"])
+        out_f.write(f"result, {result}\n")
+
+
+create_report("apples.csv",
+              "apples_report.csv")
