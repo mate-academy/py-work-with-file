820a59ae2644318fcc4c97a6311efe08b1bbf216
Andrii Uvarov <aaanyyy@gmail.com>
2024-08-23 21:24:19 +0200

    complete code and error correction2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open(data_file_name, "r") as in_f:
+    with open("../" + data_file_name, "r") as in_f:
         data_dict = {}
         for line in in_f.readlines():
             if line == "" or line == "\n":
@@ -10,12 +10,11 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             else:
                 data_dict[key] = value
 
-    with open(report_file_name, "w") as out_f:
+    with open("../" + report_file_name, "w") as out_f:
         out_f.write("supply," + str(data_dict["supply"]) + "\n")
         out_f.write("buy," + str(data_dict["buy"]) + "\n")
         result = int(data_dict["supply"]) - int(data_dict["buy"])
         out_f.write(f"result,{result}\n")
 
 
-create_report("../tests/apples.csv",
-              "../tests/apples_report.csv")
+
