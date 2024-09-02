92d44b243e9e8e26f3817b48a4da402b86fb41c4
Illia Dubina <armahanz.music@gmail.com>
2024-09-02 12:23:20 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    report = {
+        "supply": 0,
+        "buy": 0,
+    }
+
+    with open(data_file_name, "r") as data_file:
+        for line in data_file:
+            key, value = line.split(",")
+            report[key] += int(value)
+
+    with open(report_file_name, "w") as report_file:
+        report_file.write(f"supply, {report["supply"]}\n".replace(" ", ""))
+        report_file.write(f"buy, {report["buy"]}\n".replace(" ", ""))
+        report_file.write(
+            f"result, {report["supply"] - report["buy"]}\n".replace(" ", "")
+        )
