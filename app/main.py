f1d9cd5b32b2341a3fa97e55c6b9139b64ffe9c9
Tetiana Sobko <sobkotiana@gmail.com>
2024-09-06 23:55:02 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    data_file = open("../" + data_file_name)
+    data_file = open(data_file_name)
 
     supply = 0
     buy = 0
