2d71be1dfa076db462b06cf55ee19b9fa6fdc943
Ruslan Toister <rtoister.cv@gmail.com>
2024-09-11 13:35:01 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import csv
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+
+    total_supply = 0
+    total_buy = 0
+
+    with open(data_file_name, "r") as data_file:
+        csv_reader = csv.reader(data_file)
+        for line in csv_reader:
+            if not line:
+                continue
+            elif line[0] == "supply":
+                total_supply += int(line[1])
+            else:
+                total_buy += int(line[1])
+
+    with open(report_file_name, "w") as report:
+        report.write(f"supply,{total_supply}\n")
+        report.write(f"buy,{total_buy}\n")
+        report.write(f"result,{total_supply - total_buy}\n")
