700bc4a5613342fb06f966c3b8e05e3cf1096c5b
Andrii Levchenko <levchenkoandrii11@gmail.com>
2024-09-13 19:17:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -16,6 +16,6 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     result = supply_total - buy_total
 
     with open(report_file_name, "w") as report_file:
-        report_file.write(f'supply,{supply_total}\n')
-        report_file.write(f'buy,{buy_total}\n')
-        report_file.write(f'result,{result}\n')
+        report_file.write(f"supply,{supply_total}\n")
+        report_file.write(f"buy,{buy_total}\n")
+        report_file.write(f"result,{result}\n")
