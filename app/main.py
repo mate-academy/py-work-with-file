d6332fc67a3e5ed202bee46a0930f3deb71e2dd9
Ivan Bilobrov <ivan.b@team.voodoo-digital.com>
2024-09-23 19:16:06 +0300

    done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data = {}
+
+    with open(data_file_name, "r") as file_in, \
+         open(report_file_name, "w") as file_out:
+        for line in file_in.readlines():
+            operation, amount = line.split(",")
+
+            if data.get(operation):
+                data[operation] += int(amount)
+            else:
+                data[operation] = int(amount)
+
+        out_content = [f"{key},{value}" for key, value in data.items()]
+
+        file_out.write("\n".join(out_content))
