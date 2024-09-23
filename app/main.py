294c581b67184f3a33b6340cbc6f6b3f349718c2
Ivan Bilobrov <ivan.b@team.voodoo-digital.com>
2024-09-23 19:29:22 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,16 +1,27 @@
+SUPPLY_OPERATION = "supply"
+BUY_OPERATION = "buy"
+
+
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    data = {}
+    supply = 0
+    buy = 0
 
     with open(data_file_name, "r") as file_in, \
          open(report_file_name, "w") as file_out:
         for line in file_in.readlines():
             operation, amount = line.split(",")
 
-            if data.get(operation):
-                data[operation] += int(amount)
-            else:
-                data[operation] = int(amount)
+            if operation == SUPPLY_OPERATION:
+                supply += int(amount)
+            elif operation == BUY_OPERATION:
+                buy += int(amount)
 
-        out_content = [f"{key},{value}" for key, value in data.items()]
+        result = supply - buy
 
-        file_out.write("\n".join(out_content))
+        out_content = (
+            f"{SUPPLY_OPERATION},{supply}\n"
+            f"{BUY_OPERATION},{buy}\n"
+            f"result,{result}\n"
+        )
+
+        file_out.write(out_content)
