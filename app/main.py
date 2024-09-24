535851914102e1a25c4f03787e96e54a4305b5eb
yura <pollo7964132@gmail.com>
2024-09-24 21:18:45 +0300

    fixed flake8 error

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     with open(data_file_name) as file:
-        d = {}
+        dict_result = {}
         for info in file:
             values = info.rstrip("\n").split(",")
-            d.setdefault(values[0], []).append(int(values[1]))
+            dict_result.setdefault(values[0], []).append(int(values[1]))
 
-    supply, buy = sum(d["supply"]), sum(d["buy"])
+    supply, buy = sum(dict_result["supply"]), sum(dict_result["buy"])
 
     with open(report_file_name, "w") as info_file:
         info_file.write(f"supply,{supply}" + "\n")
