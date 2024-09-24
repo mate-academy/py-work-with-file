6953502c677d6995ef2f6a836e01289982c8aa9d
Andrii Uvarov <aaanyyy@gmail.com>
2024-09-24 18:58:18 +0200

    minor changes2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open("../" + data_file_name, "r") as in_f:
+    with open(data_file_name, "r") as in_f:
         data_dict = {}
         for line in in_f.readlines():
             if line == "" or line == "\n":
@@ -10,7 +10,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             else:
                 data_dict[key] = value
 
-    with open("../" + report_file_name, "w") as out_f:
+    with open(report_file_name, "w") as out_f:
         out_f.write("supply," + str(data_dict["supply"]) + "\n")
         out_f.write("buy," + str(data_dict["buy"]) + "\n")
         result = int(data_dict["supply"]) - int(data_dict["buy"])
