d273fab515fb9d5c9412444a844cdfc1c2cc9cbc
Dima Havrylenko <94nj111@gmail.com>
2024-10-09 10:40:24 +0300

    py-work-with-file solution

--- app/main.py
+++ app/main.py
@@ -1,26 +1,15 @@
-import os
-
-
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    report_data = collect_data_from_csv(data_file_name)
-    write_data_to_csv(report_file_name, report_data)
-
-
-def collect_data_from_csv(file_name: str) -> dict:
-    data = {}
-    with open(file_name, "r") as data_file:
-        for line in data_file:
-            line_list = line.split(",")
-            if data.get(line_list[0]):
-                data[line_list[0]] += int(line_list[1])
-            else:
-                data[line_list[0]] = int(line_list[1])
-        data["result"] = data.get("supply") - data.get("buy")
-    return data
-
-
-def write_data_to_csv(file_name: str, data: dict) -> None:
-    with open(file_name, "w") as report_file:
-        report_file.write(f"supply,{data.get("supply")}\n" # noqa E231
-                          f"buy,{data.get("buy")}\n" # noqa E231
-                          f"result,{data.get("result")}\n") # noqa E231
+    supply = 0
+    buy = 0
+    with open(data_file_name, "r") as f:
+        for line in f:
+            values = line.split(",")
+            if values[0] == "supply":
+                supply += int(values[1])
+            if values[0] == "buy":
+                buy += int(values[1])
+        result = supply - buy
+    with open(report_file_name, "w") as f:
+        f.write(f"supply,{supply}\n" # noqa E231
+                f"buy,{buy}\n" # noqa E231
+                f"result,{result}\n") # noqa E231
