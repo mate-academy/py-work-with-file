c3ad4adfc5dbb822ec2ff9108e988d30c5cb78fd
Dima Havrylenko <94nj111@gmail.com>
2024-10-09 08:20:07 +0300

    py-work-with-file solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+import os
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    report_data = collect_data_from_csv(data_file_name)
+    write_data_to_csv(report_file_name, report_data)
+
+
+def collect_data_from_csv(file_name: str) -> dict:
+    data = {}
+    path = os.path.abspath("..")
+    with open(f"{path}\\{file_name}", "r") as data_file:
+        for line in data_file:
+            line_list = line.split(",")
+            if data.get(line_list[0]):
+                data[line_list[0]] += int(line_list[1])
+            else:
+                data[line_list[0]] = int(line_list[1])
+        data["result"] = data.get("supply") - data.get("buy")
+    return data
+
+
+def write_data_to_csv(file_name: str, data: dict) -> None:
+    with open(file_name, "w") as report_file:
+        report_file.write(f"supply,{data.get("supply")}\n"
+                          f"buy,{data.get("buy")}\n"
+                          f"result,{data.get("result")}\n")
