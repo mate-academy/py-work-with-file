2df68b1ecc0f93bd592ca5adcb68bc6d2840b1dd
vavarvariuk <vasya.varvariuk.27@gmail.com>
2024-10-10 14:39:03 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def create_report(data_file_name: str,
+                  report_file_name: str) -> None:
+    data_file_name = open(data_file_name, "r")
+    report_file_name = open(report_file_name, "w")
+    res_dict = {}
+    for line in data_file_name.readlines():
+        mn = line.split(",")
+        if mn[0] not in res_dict:
+            res_dict[mn[0]] = int(mn[1])
+        else:
+            res_dict[mn[0]] += int(mn[1])
+
+    report_file_name.write(f"supply,{res_dict["supply"]}\n"
+                           f"buy, {res_dict["buy"]}\n"
+                           f"result, {(res_dict["supply"] - res_dict["buy"])}")
+    data_file_name.close()
+    report_file_name.close()
+
+
