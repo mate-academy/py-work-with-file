db792a1a8c0a4083ea9332f04a35493def6d2b11
vavarvariuk <vasya.varvariuk.27@gmail.com>
2024-10-11 11:41:40 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 def create_report(data_file_name: str,
                   report_file_name: str) -> None:
     data_file_name = open(data_file_name, "r")
-    report_file_name = open(report_file_name, "w")
     res_dict = {}
     for line in data_file_name.readlines():
         mn = line.split(",")
@@ -11,8 +10,9 @@ def create_report(data_file_name: str,
             res_dict[mn[0]] += int(mn[1])
     supply = res_dict["supply"]
     buy = res_dict["buy"]
+    report_file_name = open(report_file_name, "w")
     report_file_name.write(f"supply,{supply}\n"
-                           f"buy, {buy}\n"
-                           f"result, {supply - buy}")
+                           f"buy,{buy}\n"
+                           f"result,{supply - buy}\n")
     data_file_name.close()
     report_file_name.close()
