fe04c9d78dd0364403ef5b3f199c8fbb2c6c4ef3
Volodymyr Pankiv <ivovan4ik@gmail.com>
2024-10-12 20:52:55 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply = 0
+    buy = 0
+    with open(data_file_name, "r") as file:
+        for line in file:
+            line = line.strip()
+            keyword, value = line.split(",")
+            value = int(value)
+
+            if keyword == "supply":
+                supply += value
+            elif keyword == "buy":
+                buy += value
+
+    with open(report_file_name, "w") as report:
+        report.write(f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n")
