934bc5f5bf4291b4a3ec980fa7314fba522dc1e0
Alex <delete.rap.stupid@gmail.com>
2024-10-14 20:45:20 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    dict_of_data = {}
+
+    with open(data_file_name) as f:
+        for line in f:
+            value, amount = line.rstrip("\n").split(",")
+            if value not in dict_of_data:
+                dict_of_data[value] = int(amount)
+            else:
+                dict_of_data[value] += int(amount)
+
+    result = dict_of_data["supply"] - dict_of_data["buy"]
+
+    with open(report_file_name, "w") as f:
+        f.write(f"{"supply"},{dict_of_data["supply"]}\n")  # noqa: E231
+        f.write(f"{"buy"},{dict_of_data["buy"]}\n")  # noqa: E231
+        f.write(f"result,{result}\n")  # noqa: E231
