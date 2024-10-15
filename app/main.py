72ea6fdfcc3483e956b83c12a7da786bfc7fb99a
mregorlite <egor.infinity@gmail.com>
2024-10-15 18:31:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_dict = {}
+    with open(data_file_name, "r") as file:
+        data = file.read().split()
+        for element in data:
+            name, count = element.split(",")
+            data_dict.update({name: data_dict.get(name, 0) + int(count)})
+
+    with open(report_file_name, "w") as file:
+        file.write(f"supply,{data_dict.get("supply")}\n") # noqa
+        file.write(f"buy,{data_dict.get("buy")}\n") # noqa
+        result = data_dict.get("supply") - data_dict.get("buy")
+        file.write(f"result,{result}\n") # noqa
