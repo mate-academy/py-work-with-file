0b02f6fed8a2f2f19af63c783d208aa63be879a6
Aleksandr <sasha.syrota15@gmail.com>
2024-10-19 10:00:21 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_file = open(data_file_name)
+    buy = 0
+    supply = 0
+    for line in data_file.readlines():
+        data = line.split(",")
+        if data[0] == "buy":
+            buy += int(data[1])
+        else:
+            supply += int(data[1])
+    report_file = open(report_file_name, "w")
+    report_file.writelines(
+        [
+            "supply," + str(supply),
+            "\nbuy," + str(buy),
+            "\nresult," + str(supply - buy),
+            "\n"
+        ]
+    )
