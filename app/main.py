85a48f7e7934cd95c15d7520a123337dd9882780
vadym <vadym@ohmanalytics.com>
2024-10-20 21:32:42 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_report = {"supply": 0, "buy": 0, "result": 0}
+    with open(f"./../{data_file_name}") as file_to_read:
+        for line in file_to_read:
+            transaction_amount = line.split(",")
+            data_report[transaction_amount[0]] += int(transaction_amount[1])
+
+    data_report["result"] = data_report["supply"] - data_report["buy"]
+
+    with open(report_file_name, "w") as file_to_write:
+        for key, value in data_report.items():
+            file_to_write.write(f"{key},{value}\n")
