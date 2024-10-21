4a2fe6778870a88ef889cd4e6681f5969491e94d
Andrii Ovod <ovodenok@mail.com>
2024-10-21 20:07:20 +0300

    Solution without some errors

--- app/main.py
+++ app/main.py
@@ -2,18 +2,20 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
 def create_report(data_file_name: str, report_file_name: str) -> None:
     data_file_path = os.path.join(BASE_DIR, "..", data_file_name)
-    report_file_path = report_file_name  #os.path.join(BASE_DIR, "..", report_file_name)
+    report_file_path = report_file_name
 
     if not os.path.exists(data_file_path):
-        raise FileNotFoundError(f"The data file '{data_file_path}' does not exist.")
+        raise FileNotFoundError(f"The data file '{data_file_path}' "
+                                f"does not exist.")
 
     total_supply = 0
     total_buy = 0
 
-    file = open(data_file_path, "r")
-    for line in file:
+    file_ = open(data_file_path, "r")
+    for line in file_:
         if not line.strip():
             continue
 
@@ -25,7 +27,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
         elif operation == "buy":
             total_buy += amount
 
-    file.close()
+    file_.close()
 
     report_file = open(report_file_path, "w")
     report_file.write(f"supply,{total_supply}\n")
