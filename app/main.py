2581a8d8d5127196b977203c75e50a906f0c8fda
Piotr Zegarek <pzegarek@intel471.com>
2024-10-31 12:59:04 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_file = open(data_file_name, "r")
+    supply = 0
+    buy = 0
+
+    for line in data_file.readlines():
+        operation, amount = line.split(",")
+        if operation == "supply":
+            supply += int(amount)
+        elif operation == "buy":
+            buy += int(amount)
+        else:
+            continue
+    data_file.close()
+
+    result = supply - buy
+    report_file = open(report_file_name, "w")
+    report_file.write(f"supply,{supply}\n")
+    report_file.write(f"buy,{buy}\n")
+    report_file.write(f"result,{result}\n")
+    report_file.close()
