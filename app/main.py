e7d6acfff06e4a5178b8189b03be73316ae4b82f
88victory88 <victory.otrok@gmail.com>
2024-11-10 13:29:30 +0800

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    report_dict = {}
+    with (open(data_file_name, "r") as file_in,
+          open(report_file_name, "a") as file_out):
+        data_file = file_in.read()
+        data_file_list = data_file.split()
+        for it in data_file_list:
+            item = it.split(",")
+            try:
+                report_dict[item[0]]
+            except KeyError:
+                report_dict[item[0]] = int(item[1])
+            else:
+                report_dict[item[0]] += int(item[1])
+        result = report_dict["supply"] - report_dict["buy"]
+        file_out.write(f"supply,{report_dict["supply"]}\n")
+        file_out.write(f"buy,{report_dict["buy"]}\n")
+        file_out.write(f"result,{result}\n")
