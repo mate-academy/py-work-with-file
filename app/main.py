845fdd8bf7680674a9a545288fbd9243cbaeea0d
bashyrov <bashyroov@gmail.com>
2024-11-13 19:57:32 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -5,11 +5,11 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             data_type = line.split(",")[0]
             amount = int(line.split(",")[1].replace("\n", ""))
             if data_type not in dictionary_spreadsheet:
-                dictionary_spreadsheet[type] = amount
+                dictionary_spreadsheet[data_type] = amount
             else:
-                dictionary_spreadsheet[type] += amount
+                dictionary_spreadsheet[data_type] += amount
     result = dictionary_spreadsheet["supply"] - dictionary_spreadsheet["buy"]
     dictionary_spreadsheet["result"] = result
-    with open(f"{report_file_name}", "a") as report_file:
+    with open(report_file_name, "w") as report_file:
         for key, value in dictionary_spreadsheet.items():
             report_file.write(f"{key},{value}\n")
