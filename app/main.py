011c6686b1d609c428fb51ccba1c73dc50718f97
bashyrov <bashyroov@gmail.com>
2024-11-13 19:59:19 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    dictionary_spreadsheet = {}
-    with open(data_file_name, "r") as data_file:
-        for line in data_file:
-            data_type = line.split(",")[0]
-            amount = int(line.split(",")[1].replace("\n", ""))
-            if data_type not in dictionary_spreadsheet:
-                dictionary_spreadsheet[data_type] = amount
-            else:
-                dictionary_spreadsheet[data_type] += amount
-    result = dictionary_spreadsheet["supply"] - dictionary_spreadsheet["buy"]
-    dictionary_spreadsheet["result"] = result
-    with open(report_file_name, "w") as report_file:
-        for key, value in dictionary_spreadsheet.items():
-            report_file.write(f"{key},{value}\n")
+
+    with open(data_file_name, "r") as data_file_name:
+
+        count_supplies = 0
+        count_buys = 0
+        list_of_supplies_and_buys = data_file_name.readlines()
+
+        for line in list_of_supplies_and_buys:
+            row = line.split(",")[1]
+            if "supply" in line:
+                count_supplies += int(row)
+            if "buy" in line:
+                count_buys += int(row)
+
+    amount = count_supplies - count_buys
+
+    with open(report_file_name, "w") as report_file_name:
+
+        report_file_name.write(f"supply,{count_supplies}\n")
+        report_file_name.write(f"buy,{count_buys}\n")
+        report_file_name.write(f"result,{amount}\n")
