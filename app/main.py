16616a451132d835165774aaa1d7351c45878b54
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2024-11-18 22:07:11 +0100

    solution3

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def create_report(data_file_name: str, report_file_name: str) -> str:
+def create_report(data_file_name: str, report_file_name: str) -> None:
     with open(data_file_name, "r") as data_file:
         report = open(report_file_name, "a")
         data_list = [element for element in data_file.readlines()]
@@ -12,6 +12,7 @@ def create_report(data_file_name: str, report_file_name: str) -> str:
                        for element in buy_list])
         result = supply_sum - buy_sum
         report.write((f"supply, {supply_sum}\n"
-                     f"buy, {buy_sum}\n"
-                     f"result, {result}\n").replace(" ", ""))
+                      f"buy, {buy_sum}\n"
+                      f"result, {result}\n"
+                      ).replace(" ", ""))
         report.close()
