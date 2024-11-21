57f036a4e27861bbb5fe16f19a8a58487baf7183
fredisson11 <fredisson11@protonmail.com>
2024-11-21 14:14:56 +0100

    fixed quotation marks in the f-string.

--- app/main.py
+++ app/main.py
@@ -7,6 +7,6 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             result[current_line[0]] += int(current_line[1])
 
     with open(report_file_name, "w") as report_file:
-        report_file.write(f"supply,{result["supply"]}\n"
-                          f"buy,{result["buy"]}\n"
-                          f"result,{result["supply"] - result["buy"]}\n")
+        report_file.write(f"supply,{result['supply']}\n"
+                          f"buy,{result['buy']}\n"
+                          f"result,{result['supply'] - result['buy']}\n")
