93ac1f17312e4f1631587ecc36fede2c51d56bb0
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2024-12-01 18:50:29 +0100

    adding solution py-work-with-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply = 0
+    buy = 0
+
+    file1 = open(data_file_name, "r")
+    for line in file1:
+        line = line.strip()
+        if not line:
+            continue
+
+        operation, amount = line.split(",")
+        amount = int(amount)
+
+        if operation == "supply":
+            supply += amount
+        elif operation == "buy":
+            buy += amount
+
+    file1.close()
+
+    result = supply - buy
+
+    file2 = open(report_file_name, "w")
+    file2.write(f"supply, {supply}\n")
+    file2.write(f"buy, {buy}\n")
+    file2.write(f"result, {result}\n")
+    file2.close()
