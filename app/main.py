9c8556cdf2b6ffeaafa37fef26b22ab97fb9f394
shafran <shafran.andriy@gmail.com>
2024-12-10 22:49:02 +0200

    Created all

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(f"{data_file_name}.csv", "r") as csv:
+        csv_list = csv.readlines()
+        supply = 0
+        buy = 0
+        for cs in range(len(csv_list)):
+            csv_list[cs] = csv_list[cs][:-1]
+            if "supply" in csv_list[cs]:
+                for chair in csv_list[cs]:
+                    if chair == ",":
+                        supply += int(csv_list[cs][csv_list[cs].index(chair) + 1:])
+            if "buy" in csv_list[cs]:
+                for chair in csv_list[cs]:
+                    if chair == ",":
+                        buy += int(csv_list[cs][csv_list[cs].index(chair) + 1:])
+        result = supply - buy
+        with open(f"{report_file_name}.csv", "w+") as new_csv:
+            new_csv.write(f"supply,{supply}\n")
+            new_csv.write(f"buy,{buy}\n")
+            new_csv.write(f"result,{result}\n")
