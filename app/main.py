583f8cf8e8b5b8c45f9a2dccb467990a78f35396
Julia Alexeeva <julia4406@gmail.com>
2024-12-13 17:19:37 +0200

    May be solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     source = open(data_file_name, "r")
-    report = open(report_file_name, "w")
     source_dict = {}
     for line in source.readlines():
         line = line.strip().split(",")
         source_dict[line[0]] = source_dict.setdefault(line[0], 0) + int(line[1])
+    source.close()
+
+    report = open(report_file_name, "w")
     report.write(f"supply,{source_dict.get("supply")}\n")
     report.write(f"buy,{source_dict.get("buy")}\n")
     report.write(
         f"result,{source_dict.get("supply") - source_dict.get("buy")}\n"
     )
+    report.close()
