98f1d4034517bbe6396bdb8b4fe5311f89c0f05f
AntonKrinichnyi <a.krinichnyi@gmail.com>
2024-12-14 21:16:16 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def craeate_report(date_file_name: str, report_file_name: str) -> None:
+    date_file = open(date_file_name, "r")
+    supply = 0
+    buy = 0
+    for line in date_file:
+        name = line.split(",")
+        if "supply" in name:
+            supply += name[1]
+        elif "buy" in name:
+            buy += name[1]
+
+    with open(report_file_name, "w"):
+        report_file_name.write(f"supply, {supply}\n"
+                               f"buy, {buy}\n"
+                               f"result, {supply - buy}")
