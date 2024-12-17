e9f11f50f994cd96e5b68e24ac3d2e2319c7fcb7
Kateryna <katerina.a.biruk@gmail.com>
2024-12-17 20:22:24 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    operations = {}
+    with open(data_file_name, "r") as file:
+        for line in file.readlines():
+            split = line.split(",")
+            operation_type = split[0]
+            amount = int(split[1].strip())
+            if operation_type not in operations.keys():
+                operations[operation_type] = amount
+            else:
+                operations[operation_type] += amount
+
+    operations["result"] = operations["supply"] - operations["buy"]
+
+    with open(report_file_name, "w") as report_file:
+        report_file.write(f'supply,{operations["supply"] }\n')
+        report_file.write(f'buy,{operations["buy"]}\n')
+        report_file.write(f'result,{operations["result"]}\n')
