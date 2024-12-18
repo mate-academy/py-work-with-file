09e81709f97c1a910f852aed3878606b047e257a
Mariana <mariana.antoniak.babish@gmail.com>
2024-12-18 22:51:23 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,17 +1,16 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     with open(data_file_name, "r") as input:
-        input_data = input.read()
         total_supply = 0
         total_buy = 0
-        for line in input_data:
+        for line in input:
             if line == "":
                 continue
-            if line[0] == "supply":
-                total_supply += line[1]
-            elif line[0] == "buy":
-                total_buy += line[1]
+            if "supply" in line:
+                total_supply += int(line[7:])
+            elif "buy" in line:
+                total_buy += int(line[4:])
         result = total_supply - total_buy
     with open(report_file_name, "w") as output:
-        output.write(f"supply, {total_supply}")
-        output.write(f"buy, {total_buy}")
-        output.write(f"result, {result}")
+        output.write(f"supply,{total_supply}\n")
+        output.write(f"buy,{total_buy}\n")
+        output.write(f"result,{result}\n")
