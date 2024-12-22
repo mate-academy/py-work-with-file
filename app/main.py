5e54af7abb6cfd6ac327c914f223f79450ae7191
maksymtrokhymchuk <maximuschampion2002@gmail.com>
2024-12-22 21:00:48 +0200

    work-with-fire

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import csv
 
-def create_report(data_file_name: str, report_file_name: str):
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
     total_supply = 0
     total_buy = 0
 
     try:
-        data_file = open(data_file_name, 'r')
+        data_file = open(data_file_name, "r")
         reader = csv.reader(data_file)
 
         for row in reader:
@@ -15,20 +16,20 @@ def create_report(data_file_name: str, report_file_name: str):
                     amount = int(amount)
                 except ValueError:
                     print(f"Skipping invalid data: {row}")
-                if operation == 'supply':
+                if operation == "supply":
                     total_supply += amount
-                elif operation == 'buy':
+                elif operation == "buy":
                     total_buy += amount
 
         data_file.close()
 
         result = total_supply - total_buy
 
-        report_file = open(report_file_name, 'w', newline='')
+        report_file = open(report_file_name, "w", newline="")
         writer = csv.writer(report_file)
-        writer.writerow(['supply', total_supply])
-        writer.writerow(['buy', total_buy])
-        writer.writerow(['result', result])
+        writer.writerow(["supply", total_supply])
+        writer.writerow(["buy", total_buy])
+        writer.writerow(["result", result])
 
         report_file.close()
 
@@ -37,4 +38,4 @@ def create_report(data_file_name: str, report_file_name: str):
     except FileNotFoundError:
         print(f"Error: The file {data_file_name} was not found.")
     except IOError:
-        print(f"Error: There was an issue with reading or writing the files.")
+        print("Error: There was an issue with reading or writing the files.")
