afc12e309b4a2137d926cce550bdf2bfacb429a3
Dmytro.Poberezhnyi <foxymood@gmail.com>
2024-12-22 15:05:44 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    file = open(data_file_name, "r")
+    input_file = open(data_file_name, "r")
     supply = 0
     buy = 0
-    for line in file.readlines():
+    for line in input_file.readlines():
         if line == "":
             break
         splitted_line = line.split(",")
@@ -11,9 +11,9 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
         elif splitted_line[0] == "buy":
             buy += int(splitted_line[1])
     result = supply - buy
-    file.close()
-    file = open(report_file_name, mode="w")
-    file.write(f"supply,{supply}\n")
-    file.write(f"buy,{buy}\n")
-    file.write(f"result,{result}\n")
-    file.close()
+    input_file.close()
+    report_file = open(report_file_name, mode="w")
+    report_file.write(f"supply,{supply}\n")
+    report_file.write(f"buy,{buy}\n")
+    report_file.write(f"result,{result}\n")
+    report_file.close()
