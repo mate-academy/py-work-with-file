8b23890d6e46b63e6b84278f455ffcd2a3f4696f
Taras <t.kudinov90@gmail.com>
2024-12-24 11:45:21 +0200

    Flake8 mark corrected after merging tests fail

--- app/main.py
+++ app/main.py
@@ -14,9 +14,11 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
                     count_cont[item_list[0]] = int(item_list[1])
             else:
                 break
-        supply = f"supply,{count_cont["supply"]}"
-        buy = f"buy,{count_cont["buy"]}"
-        calc = int(count_cont["supply"]) - int(count_cont["buy"])
+        calc_supply = int(count_cont["supply"])
+        calc_buy = int(count_cont["buy"])
+        supply = f"supply,{calc_supply}"
+        buy = f"buy,{calc_buy}"
+        calc = calc_supply - calc_buy
         result = f"result,{calc}"
         content_out = f"{supply}\n{buy}\n{result}\n"
         file_out.write(content_out)
