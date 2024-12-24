056d91e80ef6a8315b92c4214db7d8809400ebe2
Liza Shulika <liza.shulika2009@gmail.com>
2024-12-24 14:56:47 +0200

    work_with1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     supply_num = 0
     buy_num = 0
-    with open("data_file_name", "r") as f:
+    with open(data_file_name, "r") as f:
         for line in f:
             line_list = line.strip().split(",")
             if line_list[0] == "supply":
@@ -9,5 +9,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             elif line_list[0] == "buy":
                 buy_num += int(line_list[1])
         result = supply_num - buy_num
-    with open("report_file_name", "w") as f:
-        f.write(f"supply,{supply_num}\n buy,{buy_num}\n result,{result}")
+    with open(report_file_name, "w") as f:
+        f.write(f"supply,{supply_num}\n")
+        f.write(f"buy,{buy_num}\n")
+        f.write(f"result,{result}\n")
