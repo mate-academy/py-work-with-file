a1a1bfde2464c39d293aa0079efc8888c098378a
JulianskyMe <juliakamynskaya@gmail.com>
2024-12-28 18:42:30 +0200

    upd flake

--- app/main.py
+++ app/main.py
@@ -1,33 +1,25 @@
 import csv
 
+
 def create_report(data_file_name: str, report_file_name: str):
     supply_total = 0
     buy_total = 0
-    
-    file = open(data_file_name, mode='r')
-    reader = csv.reader(file)
-    
+    new_file = open(data_file_name, mode='r')
+    reader = csv.reader(new_file)
     next(reader, None)
-    
     for row in reader:
         if row:
             operation, amount = row
-            amount = int(amount) 
-            
+            amount = int(amount)
             if operation == 'supply':
                 supply_total += amount
             elif operation == 'buy':
                 buy_total += amount
-    
     result = supply_total - buy_total
-    
-    file.close()
-    
-    file = open(report_file_name, mode='w', newline='')
-    writer = csv.writer(file)
-    
+    new_file.close()
+    new_file = open(report_file_name, mode='w', newline='')
+    writer = csv.writer(new_file)
     writer.writerow(['supply', supply_total])
     writer.writerow(['buy', buy_total])
     writer.writerow(['result', result])
-    
-    file.close()
+    new_file.close()
