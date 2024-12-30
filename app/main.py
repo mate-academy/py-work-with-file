9ef773787136274909fb7d1b518a1ef7c6d6924d
AlArt99 <AlArt1991@gmail.com>
2024-12-30 22:36:33 +0100

    created file report writer

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> int:
+    read_file = open(data_file_name, mode="r")
+    write_file = open(report_file_name, mode="w")
+    output = {}
+
+    line_number = 1
+    for line in read_file:
+        typ, amount = line.split(",")
+        if typ not in output.keys():
+            output[typ] = int(amount)
+        else:
+            output[typ] += int(amount)
+        line_number += 1
+
+    output["result"] = output["supply"] - output["buy"]
+
+    return write_file.write(f"supply,{output["supply"]}\n"
+                            f"buy,{output["buy"]}\n"
+                            f"result,{output["result"]}\n")
