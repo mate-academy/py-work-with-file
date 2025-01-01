da769c4921dc27b29c7c93780eb3f8cb24f78511
JulianskyMe <juliakamynskaya@gmail.com>
2025-01-01 20:45:37 +0200

    upd

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     supply_total = 0
     buy_total = 0
 
-    with open(data_file_name, 'r') as data_file:
+    with open(data_file_name, "r") as data_file:
         reader = csv.reader(data_file)
         for row in reader:
             if not row:
@@ -18,7 +18,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
 
     result = supply_total - buy_total
 
-    with open(report_file_name, 'w') as report_file:
+    with open(report_file_name, "w") as report_file:
         writer = csv.writer(report_file)
         writer.writerow(["supply", supply_total])
         writer.writerow(["buy", buy_total])
