6d8cf7a3e1843cd0a955bbda1db65b9ee278297f
JulianskyMe <juliakamynskaya@gmail.com>
2025-01-01 20:44:45 +0200

    upd

--- app/main.py
+++ app/main.py
@@ -4,22 +4,22 @@ import csv
 def create_report(data_file_name: str, report_file_name: str) -> None:
     supply_total = 0
     buy_total = 0
-    new_file = open(data_file_name, mode="r")
-    reader = csv.reader(new_file)
-    next(reader, None)
-    for row in reader:
-        if row:
+
+    with open(data_file_name, 'r') as data_file:
+        reader = csv.reader(data_file)
+        for row in reader:
+            if not row:
+                continue
             operation, amount = row
-            amount = int(amount)
             if operation == "supply":
-                supply_total += amount
+                supply_total += int(amount)
             elif operation == "buy":
-                buy_total += amount
+                buy_total += int(amount)
+
     result = supply_total - buy_total
-    new_file.close()
-    new_file = open(report_file_name, mode="w", newline="")
-    writer = csv.writer(new_file)
-    writer.writerow(["supply", supply_total])
-    writer.writerow(["buy", buy_total])
-    writer.writerow(["result", result])
-    new_file.close()
+
+    with open(report_file_name, 'w') as report_file:
+        writer = csv.writer(report_file)
+        writer.writerow(["supply", supply_total])
+        writer.writerow(["buy", buy_total])
+        writer.writerow(["result", result])
