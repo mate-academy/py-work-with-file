e63d39f65dcaa2f2bcab9da39a47de1aa0e31e62
Dmytro Kornitskyi <d.kornitskyi@ukr.net>
2025-01-15 17:37:37 +0200

    Change flake 8

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-   supply = 0
-   buy = 0
+    supply = 0
+    buy = 0
 
-   with open(data_file_name, 'r') as file:
-       for line in file:
-           if line.strip():
-               operation, amount = line.strip().split(',')
-               if operation == "supply":
-                   supply += int(amount)
-               elif operation == "buy":
-                   buy += int(amount)
+    with open(data_file_name, "r") as file:
+        for line in file:
+            if line.strip():
+                operation, amount = line.strip().split(',')
+                if operation == "supply":
+                    supply += int(amount)
+                elif operation == "buy":
+                    buy += int(amount)
 
-   result = supply - buy
+    result = supply - buy
 
-   with open(report_file_name, "a") as file:
-       file.write(f"supply,{supply}\n")
-       file.write(f"buy,{buy}\n")
-       file.write(f"result,{result}\n")
+    with open(report_file_name, "a") as file:
+        file.write(f"supply,{supply}\n")
+        file.write(f"buy,{buy}\n")
+        file.write(f"result,{result}\n")
