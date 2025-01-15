c6bb94a16290aa7f6e1819362a4a9844aa1e26bc
Dmytro Kornitskyi <d.kornitskyi@ukr.net>
2025-01-15 17:39:10 +0200

    Change flake8

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     with open(data_file_name, "r") as file:
         for line in file:
             if line.strip():
-                operation, amount = line.strip().split(',')
+                operation, amount = line.strip().split(",")
                 if operation == "supply":
                     supply += int(amount)
                 elif operation == "buy":
