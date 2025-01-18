90843ce9148052730b9b13cc501f7e8535078f1e
Yehor Yurchenko <yegoryurtchenko05@gmail.com>
2025-01-19 00:46:19 +0200

    Solution3

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     data = {"supply": 0, "buy": 0}
-    f = open(data_file_name)
-    for line in f.readlines():
+    file1 = open(data_file_name)
+    for line in file1.readlines():
         data[line[0: line.index(",")]] += int(line[line.index(",") + 1:])
     data["result"] = data["supply"] - data["buy"]
-    files = open(report_file_name, "w")
+    file2 = open(report_file_name, "w")
     for key in data:
-        files.write(key + "," + str(data[key]) + "\n")
+        file2.write(key + "," + str(data[key]) + "\n")
