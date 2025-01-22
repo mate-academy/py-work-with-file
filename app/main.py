c562e588164aaa982383904fb914e478b8305eab
Oksana <oksana.zhornyk@gmail.com>
2025-01-22 23:41:04 +0100

    Solution py-work-with-file v.2

--- app/main.py
+++ app/main.py
@@ -1,17 +1,16 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    data_dict = {}
-    data_file = open(data_file_name)
-    for row in data_file:
-        key, value = row.split(",")
-        if key not in data_dict:
-            data_dict[key] = int(value)
-        else:
-            data_dict[key] += int(value)
-        print(data_dict)
-    data_file.close()
-    report_file = open(report_file_name, mode="a")
-    report_file.write(f"supply,{data_dict["supply"]}\n")
-    report_file.write(f"buy,{data_dict["buy"]}\n")
-    result = data_dict["supply"] - data_dict["buy"]
-    report_file.write(f"result,{result}\n")
-    report_file.close()
+    supply = 0
+    buy = 0
+
+    with open(data_file_name) as data_file:
+        for line in data_file.readlines():
+            separated_line = line.split(",")
+            if separated_line[0] == "supply":
+                supply += int(separated_line[1])
+            elif separated_line[0] == "buy":
+                buy += int(separated_line[1])
+
+    result = supply - buy
+
+    with open(report_file_name, "w") as report_file:
+        report_file.write(f"supply,{supply}\nbuy,{buy}\nresult,{result}\n")
