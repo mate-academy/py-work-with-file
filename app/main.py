e25b58c382e8195f26a5746cd9b1dc240198404e
Oksana <oksana.zhornyk@gmail.com>
2025-01-22 23:30:20 +0100

    Solution py-work-with-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_dict = {}
+    data_file = open(data_file_name)
+    for row in data_file:
+        key, value = row.split(",")
+        if key not in data_dict:
+            data_dict[key] = int(value)
+        else:
+            data_dict[key] += int(value)
+        print(data_dict)
+    data_file.close()
+    report_file = open(report_file_name, mode="a")
+    report_file.write(f"supply,{data_dict["supply"]}\n")
+    report_file.write(f"buy,{data_dict["buy"]}\n")
+    result = data_dict["supply"] - data_dict["buy"]
+    report_file.write(f"result,{result}\n")
+    report_file.close()
