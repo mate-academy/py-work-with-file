6794080abf35e57ea34cd794212ddefa91065416
Evgeniya <didovichevgeniya@gmail.com>
2025-01-23 15:25:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import csv
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as file:
+        reader = csv.DictReader(file)
+        data = list(reader)
+    operations ={}
+    for row in data:
+        operation_type  = row[0]
+        amount = int(row[1])
+        if operation_type in operations:
+            operations[operation_type] += amount
+        else:
+            operations[operation_type] = amount
+
+    with open(report_file_name, "w", newline="") as report_file:
+        csv_writer = csv.writer(report_file)
+        for operations, total_amount in operations.items():
+            csv_writer.writerow([operations, total_amount])
