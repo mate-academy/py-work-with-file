460fb8f9f622d838577e9b354b3ed8b7d76b8946
Ron Phillips <mcspidy@gmail.com>
2025-01-26 09:55:29 -0600

    Solution - Flake8 ' to "

--- app/main.py
+++ app/main.py
@@ -9,23 +9,23 @@ def convert_string(string: str) -> int | float:
 
 
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    file_data = {"supply": '0', "buy": '0', "result": '0'}
-    with open(data_file_name, 'r') as data_file:
+    file_data = {"supply": "0", "buy": "0", "result": "0"}
+    with open(data_file_name, "r") as data_file:
         for line in data_file:
-            key, value = line.strip().split(',')
+            key, value = line.strip().split(",")
             if key in file_data:
                 dict_value = file_data[key]
                 file_data[key] = str(
                     convert_string(dict_value) + convert_string(value)
                 )
-                file_data['result'] = str(
-                    convert_string(file_data['supply'])
-                    - convert_string(file_data['buy'])
+                file_data["result"] = str(
+                    convert_string(file_data["supply"])
+                    - convert_string(file_data["buy"])
                 )
             else:
                 file_data[key] = value
 
-    with open(report_file_name, 'w') as report_file:
+    with open(report_file_name, "w") as report_file:
         for key, value in file_data.items():
             report = f'{key},{value}\n'
             report_file.write(report)
