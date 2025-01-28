094470ac6792a159fdadf2412243e5505644e008
Mykyta <mitina1900@gmail.com>
2025-01-28 12:36:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import csv
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply_total = 0
+    buy_total = 0
+
+    with open(data_file_name, mode="r") as data_file:
+        reader = csv.reader(data_file)
+        for row in reader:
+            if not row:
+                continue
+            data_name , quantity = row
+            quantity = int(quantity)
+            if data_name == "buy":
+                buy_total += quantity
+            elif data_name == "supply":
+                supply_total += quantity
+
+    with open(report_file_name, mode='w', newline='') as report_file:
+        writer = csv.writer(report_file)
+        writer.writerow(["supply", supply_total])
+        writer.writerow(["buy", buy_total])
+        writer.writerow(["result", supply_total - buy_total])
