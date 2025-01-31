fbed7853df5558465133d728fbdb8fad3f27189b
bc2024-4 <bzabrodskij20@gmail.com>
2025-01-31 17:36:34 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     supply_total = 0
     buy_total = 0
 
-    with open(data_file_name, "r", encoding="utf-8") as file:
+    with open(data_file_name, "r") as file:
         for line in file:
             line = line.strip()
             if not line:
@@ -17,7 +17,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
 
     result = supply_total - buy_total
 
-    with open(report_file_name, "w", encoding="utf-8") as file:
+    with open(report_file_name, "w") as file:
         file.write(f"supply, {supply_total}\n")
         file.write(f"buy, {buy_total}\n")
         file.write(f"result, {result}\n")
