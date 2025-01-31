1cb884bc07d09b6259891dc659e76f8168991dcf
Yaroslav <yaroslav.liubarets@icloud.com>
2025-01-31 19:43:01 +0000

    Solution 3.0

--- app/main.py
+++ app/main.py
@@ -1,24 +1,23 @@
-import csv
-
-
 def create_report(data_file_name: str, report_file_name: str) -> None:
     supply_total = 0
     buy_total = 0
 
-    with open(data_file_name, mode="r") as data_file:
-        reader = csv.reader(data_file)
-        for row in reader:
-            if not row:
-                continue
-            data_name , quantity = row
-            quantity = int(quantity)
-            if data_name == "buy":
-                buy_total += quantity
-            elif data_name == "supply":
-                supply_total += quantity
+    with open(data_file_name, "r") as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                parts = line.split(",")
+                if len(parts) == 2:
+                    operation, amount = parts
+                    amount = int(amount)
+                    if operation == "supply":
+                        supply_total += amount
+                    elif operation == "buy":
+                        buy_total += amount
 
-    with open(report_file_name, mode="w", newline="") as report_file:
-        writer = csv.writer(report_file)
-        writer.writerow(["supply", supply_total])
-        writer.writerow(["buy", buy_total])
-        writer.writerow(["result", supply_total - buy_total])
+    result = supply_total - buy_total
+
+    with open(report_file_name, "w") as report:
+        report.write("supply," + str(supply_total) + "\n")
+        report.write("buy," + str(buy_total) + "\n")
+        report.write("result," + str(result) + "\n")
