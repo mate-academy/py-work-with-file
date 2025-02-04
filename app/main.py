3d622d9488102003e60c675da162d569492819a6
Odin-max <meh84741@gmail.com>
2025-02-04 19:34:27 +0200

    some changes

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from typing import Any
 def create_report(data_file_name: str, report_file_name: str) -> Any:
     supply_total = 0
     buy_total = 0
-    
+
     with open(data_file_name, mode="r", newline="") as file:
         reader = csv.reader(file)
         for row in reader:
@@ -16,9 +16,9 @@ def create_report(data_file_name: str, report_file_name: str) -> Any:
                 supply_total += amount
             elif operation == "buy":
                 buy_total += amount
-    
+ 
     result = supply_total - buy_total
-    
+
     with open(report_file_name, mode="w", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(["supply", supply_total])
