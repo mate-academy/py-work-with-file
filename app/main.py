a650dd32db7e938eebcb4b76f65b32b1399d2f32
Andriy Hrudnyy <andgrudniy@gmail.com>
2025-02-07 14:29:23 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,17 +1,12 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     f = open(data_file_name)
     datas = f.read().split("\n")
-    result_dict = {}
+    result_dict = {"supply": 0, "buy": 0}
     for data in datas:
         splited_data = data.split(",")
-        if splited_data[0] in result_dict:
-            if splited_data[0] == "":
-                continue
-            result_dict[splited_data[0]] += int(splited_data[-1])
-        else:
-            if splited_data[0] == "":
-                continue
-            result_dict[splited_data[0]] = int(splited_data[-1])
+        if splited_data[0] == "":
+            continue
+        result_dict[splited_data[0]] += int(splited_data[-1])
     f.close()
 
     f = open(report_file_name, "w")
