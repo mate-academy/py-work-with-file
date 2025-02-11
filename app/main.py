9ed19cd59cbb52b6aca0a5fbd4f669a11a5cbc4f
nikita-plotnytskyi <nikita.plotnitckiy@gmail.com>
2025-02-11 23:09:54 +0200

    fix path

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
 
     result = {"supply": 0, "buy": 0}
-    with open(f"../{data_file_name}") as file:
+    with open(data_file_name, "r") as file:
         for line in file:
             list_items = line.split(",")
             result[list_items[0]] += int(list_items[1].strip("\n"))
