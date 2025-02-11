95d0c2ecaf7e3eca7a791df5999c81ee18e8bc08
nikita-plotnytskyi <nikita.plotnitckiy@gmail.com>
2025-02-11 21:49:21 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+
+    result = {"supply": 0, "buy": 0}
+    with open(f"../{data_file_name}") as file:
+        for line in file:
+            list_items = line.split(",")
+            result[list_items[0]] += int(list_items[1].strip("\n"))
+
+    with open(report_file_name, "w") as new_file:
+        new_file.write(f"supply,{result["supply"]}\n"
+                       f"buy,{result["buy"]}\n"
+                       f"result,{result["supply"] - result["buy"]}\n")
