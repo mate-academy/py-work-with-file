13b7f3135b9be4bc43ebb34f67390f5f4a8c1384
nikita-plotnytskyi <nikita.plotnitckiy@gmail.com>
2025-02-11 22:15:24 +0200

    fix E231

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             result[list_items[0]] += int(list_items[1].strip("\n"))
 
     with open(report_file_name, "w") as new_file:
-        new_file.write(f"supply,{result["supply"]}\n"
-                       f"buy,{result["buy"]}\n"
-                       f"result,{result["supply"] - result["buy"]}\n")
+        new_file.write("supply," + str(result["supply"]) + "\n"
+                       + "buy," + str(result["buy"]) + "\n"
+                       + "result," + str(result["supply"] - result["buy"])
+                       + "\n")
