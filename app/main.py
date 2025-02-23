cbc71d97c3b3843ffd11d1002423b2f529a6c6b1
Евгений Наволоков <goustg62@gmail.com>
2025-02-23 13:44:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     res = {"supply": 0, "buy": 0}
         
     for i in lines:
-        i = i.split(",")
+        i = i.strip().split(",")
         res[i[0]] += int(i[1])
         
-    rir = f"supply,{res['supply']}\nbuy,{res['buy']}\nresult,{res['supply'] - res['buy']}"
+    rir = f"supply,{res['supply']}\nbuy,{res['buy']}\nresult,{res['supply'] - res['buy']}\n"
         
-    with open(report_file_name) as fn:
+    with open(report_file_name, 'w') as fn:
         fn.write(rir)
