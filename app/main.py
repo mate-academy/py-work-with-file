f78900221d022fa3690478cbd9093f831bf5d063
Andrii <yurkivandriy02@gmail.com>
2025-02-26 16:24:22 +0200

    fix flake8 problem

--- app/main.py
+++ app/main.py
@@ -1,27 +1,27 @@
-def create_report(data_file_name: str, report_file_name: str):
+def create_report(data_file_name: str, report_file_name: str) -> None:
     total_supply = 0
     total_buy = 0
 
-    file = open(data_file_name, 'r')
+    data_file = open(data_file_name, "r")
 
-    for line in file:
+    for line in data_file:
         line = line.strip()
 
         if line:
-            line = line.split(',')
+            line = line.split(",")
 
-            if line[0] == 'supply':
+            if line[0] == "supply":
                 total_supply += int(line[1])
-            elif line[0] == 'buy':
+            elif line[0] == "buy":
                 total_buy += int(line[1])
 
-    file.close()
+    data_file.close()
 
     result = total_supply - total_buy
 
-    file = open(report_file_name, "w")
-    file.write(f"supply,{total_supply}\n")
-    file.write(f"buy,{total_buy}\n")
-    file.write(f"result,{result}\n")
+    report_file = open(report_file_name, "w")
+    report_file.write(f"supply,{total_supply}\n")
+    report_file.write(f"buy,{total_buy}\n")
+    report_file.write(f"result,{result}\n")
 
-    file.close()
+    report_file.close()
