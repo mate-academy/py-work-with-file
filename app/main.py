c7511747baf9161edaee5250269a78c815183a26
AndyStar <andy_star@ukr.net>
2025-03-01 16:53:45 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_file = open(data_file_name, "r")
+    data_storage = data_file.readlines()
+    data_file.close()
+    supply = 0
+    buy = 0
+    for one_line in data_storage:
+        one_line_list = one_line.split(",")
+        if one_line_list[0] == "supply":
+            supply += int(one_line_list[1][: - 1])
+        elif one_line_list[0] == "buy":
+            buy += int(one_line_list[1][: - 1])
+    data_file.close()
+    report_file = open(report_file_name, "w")
+    prefix = "supply,"
+    report_file.write(f"{prefix}{supply}\n")
+    prefix = "buy,"
+    report_file.write(f"{prefix}{buy}\n")
+    prefix = "result,"
+    report_file.write(f"{prefix}{supply - buy}\n")
+    report_file.close()
+
+
+create_report("apples.csv", "report.csv")
