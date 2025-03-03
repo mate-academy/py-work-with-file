4c38e96a03bbb80b532f387f721485366f826394
Roman Kostiv <rkostiv@gmail.com>
2025-03-03 21:15:49 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
 
-    file = open(data_file_name, "r")
+    file1 = open(data_file_name, "r")
     new_supply = 0
     new_buy = 0
 
-    for row in file:
+    for row in file1:
         row = row.strip().split(",")
         if row[0] == "supply":
             new_supply += int(row[1])
         elif row[0] == "buy":
             new_buy += int(row[1])
 
-    file.close()
+    file1.close()
 
     file2 = open(report_file_name,"w")
     file2.write(f"supply,{new_supply}\n")
