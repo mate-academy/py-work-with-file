19230cb7a43c72f01225cd116c9f4a282ea43c14
Mike Far <mikefarenyuk@gmail.com>
2025-03-05 11:44:52 +0200

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -9,6 +9,6 @@ def create_report(data_file_name: str, report_file_name: str) -> str:
                 operation_type, amount = map(str.strip, string.split(","))
                 data[operation_type] += int(amount)
 
-        report.write(f'supply, {data["supply"]}\n')
-        report.write(f'buy, {data["buy"]}\n')
-        report.write(f'result, {data["supply"] - data["buy"]}\n')
+        report.write(f'supply,{data["supply"]}\n')
+        report.write(f'buy,{data["buy"]}\n')
+        report.write(f'result,{data["supply"] - data["buy"]}\n')
