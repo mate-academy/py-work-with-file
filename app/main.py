828b5d9e76d4d86d9206e4d062734101303b8bf3
Artur Drzazga <artur.drzazga03@gmail.com>
2025-03-06 09:24:59 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+def create_report(data_file_name: str,report_file_name: str) -> None:
+
+    with open(data_file_name, "r") as fruits:
+        supply = 0
+        buy = 0
+        for fruit in fruits:
+            parts = fruit.strip().split(",")
+            operation = parts[0]
+            amount = int(parts[1])
+            if operation == "supply":
+                supply += amount
+            elif operation == "buy":
+                buy += amount
+        result = supply - buy
+        str_result = str(result)
+
+        with open(report_file_name, "a") as report:
+            report.write(f"supply,{supply}\n")
+            report.write(f"buy,{buy}\n")
+            report.write(f"result,{str_result}\n")
