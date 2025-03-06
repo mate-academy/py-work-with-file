7861970f4f073b7e4f18dfe51974077834eb7eab
Yurii <yakymenko025@gmail.com>
2025-03-06 15:42:17 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+import csv
+
+
+def create_report(data_file_name: str, report_file_name: str):
+    data_file = open(data_file_name, "r")
+    supply = 0
+    buy = 0
+
+    for line in data_file:
+        line_value = line.split(",")
+        if line_value[0] == "supply":
+            supply += int(line_value[1])
+        if line_value[0] == "buy":
+            buy += int(line_value[1])
+    result = supply - buy
+    report_data = [
+        ['supply', str(supply)],
+        ['buy', str(buy)],
+        ['result', str(result)]
+    ]
+    with open(report_file_name, 'w', newline='') as outfile:
+        csv_writer = csv.writer(outfile)
+        csv_writer.writerows(report_data)
