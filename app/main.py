84b56527b0465a3a9d47adcd4bd1c91da05052b5
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-03-08 15:07:11 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    report_file = open(report_file_name, "w")
+    data_file = open(data_file_name, "r")
+
+    report_dict = {"supply": 0, "buy": 0}
+
+    for line in data_file.readlines():
+        if line.split(",")[0] == "supply":
+            report_dict["supply"] += int(line.split(",")[1])
+        else:
+            report_dict["buy"] += int(line.split(",")[1])
+    data_file.close()
+
+    report_file.write(f"supply,{report_dict.get("supply")}\n")
+    report_file.write(f"buy,{report_dict.get("buy")}\n")
+    report_file.write(f"result,"
+                      f"{report_dict.get("supply") - report_dict.get("buy")}\n")
+
+    report_file.close()
