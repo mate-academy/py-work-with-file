a10e4eff0d6dad301ca6ba661feeede5a6360b71
Volodymyr Vitsenko <your@mail.com>
2025-03-08 12:31:39 +0100

    report-maker

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    report = {"supply": 0, "buy": 0}
+
+    data_file = open(data_file_name)
+    for line in data_file:
+        report[line.split(",")[0]] = report[line.split(",")[0]] + int(line.split(",")[1])
+    data_file.close()
+
+    report_file = open(report_file_name, "w")
+    for key in report:
+        report_file.write(f"{key},{report[key]}\n")
+    report_file.write(f"result,{report["supply"] - report["buy"]}\n")
+    report_file.close()
