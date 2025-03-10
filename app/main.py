0561532b58ab00849b185c19b4ae5aca4eead0fc
Anton Bliznuk <antonbliznuk71@gmail.com>
2025-03-10 11:07:54 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -20,7 +20,3 @@ def create_report(
         file.write(f"buy,{dict_of_operation.get("buy")}\n")
         file.write(f"result,{dict_of_operation.get("supply") - dict_of_operation.get("buy")}\n")  # noqa: E501
         file.close()
-
-
-if __name__ == "__main__":
-    create_report("bananas.csv", "test.csv")
