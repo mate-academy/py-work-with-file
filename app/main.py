0e21ae770bffa6d4402a38bec4252b163597adc1
ExampleE <example@example.example>
2025-03-13 10:48:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    input = open(data_file_name, "r")
+    input_file = open(data_file_name, "r")
     supply, buy = 0, 0
-    for line in input.readlines():
+    for line in input_file.readlines():
         if "supply" in line:
             supply += int(line.rsplit(",")[1])
         elif "buy" in line:
             buy += int(line.rsplit(",")[1])
         elif not line.strip():
             print("Empty line")
-    input.close()
-    output = open(report_file_name, "a")
-    output.write(f"supply,{supply}\n")
-    output.write(f"buy,{buy}\n")
+    input_file.close()
+    output_file = open(report_file_name, "a")
+    output_file.write(f"supply,{supply}\n")
+    output_file.write(f"buy,{buy}\n")
     result = supply - buy
-    output.write(f"result,{result}\n")
-    input.close()
-    output.close()
+    output_file.write(f"result,{result}\n")
+    input_file.close()
+    output_file.close()
