210d7f3a2d9f4ce46ff52083958bb5abd4fab61a
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2025-03-16 21:20:12 +0200

    implement py-work-with-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply_total = 0
+    buy_total = 0
+
+    with open(data_file_name) as data_file:
+        for line in data_file:
+            operation, amount = line.split(",")
+            amount_value = int(amount)
+
+            if operation == "supply":
+                supply_total += amount_value
+            elif operation == "buy":
+                buy_total += amount_value
+
+    result_total = supply_total - buy_total
+
+    with open(report_file_name, "w") as report_file:
+        report_file.write(f"supply,{supply_total}\n")
+        report_file.write(f"buy,{buy_total}\n")
+        report_file.write(f"result,{result_total}\n")
