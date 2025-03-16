abb0bdbe8ac2a13b82c4600212b8950255de0494
Max <smolinskijmaksim1@gmail.com>
2025-03-16 13:45:40 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import csv
 def create_report(data_file_name: str, report_file_name: str) -> None:
     total_supply = 0
     total_buy = 0
+
     try:
         with open(data_file_name, "r", newline="") as csv_file:
             reader = csv.reader(csv_file)
