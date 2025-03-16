fe9e5358897bb2dbf2f0cc96ee5b143ee2a0d251
Max <smolinskijmaksim1@gmail.com>
2025-03-16 13:49:39 +0100

    Fix flake

--- app/main.py
+++ app/main.py
@@ -17,7 +17,6 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
 
         result = total_supply - total_buy
 
-
         with open(report_file_name, "w", newline="") as report_file:
             writer = csv.writer(report_file)
             writer.writerow(["supply", total_supply])
@@ -30,7 +29,11 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
         print(f"Сталася помилка: {e}")
 
 
-create_report("C:/Users/OEM/py-work-with-file/apples.csv", "apples_report.csv")
-create_report("C:/Users/OEM/py-work-with-file/bananas.csv", "bananas_report.csv")
-create_report("C:/Users/OEM/py-work-with-file/grapes.csv", "grapes_report.csv")
-create_report("C:/Users/OEM/py-work-with-file/oranges.csv", "oranges_report.csv")
+create_report("C:/Users/OEM/py-work-with-file/apples.csv",
+              "apples_report.csv")
+create_report("C:/Users/OEM/py-work-with-file/bananas.csv",
+              "bananas_report.csv")
+create_report("C:/Users/OEM/py-work-with-file/grapes.csv",
+              "grapes_report.csv")
+create_report("C:/Users/OEM/py-work-with-file/oranges.csv",
+              "oranges_report.csv")
