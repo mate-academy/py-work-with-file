6a355bf4c2ab169792fb96dc8d8adc3ce9370b96
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-03-25 16:30:50 +0100

    feat: create create_report for file function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    obj = {
+        "supply": 0,
+        "buy": 0,
+    }
+
+    with open(data_file_name) as file:
+        for line in file:
+            str_line = line.split(",")
+            type_str = str_line[0]
+            number = int(str_line[1])
+
+            obj[type_str] = obj.get(type_str, 0) + number
+
+        obj["result"] = obj["supply"] - obj["buy"]
+        result = "\n".join([
+            ",".join((key, str(value)))
+            for (key, value) in obj.items()
+        ])
+
+        with open(report_file_name, "w") as write_file:
+            write_file.write(f"{result}\n")
