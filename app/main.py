dd8972e9764c4816fb426bcb58148927a2c6b5b6
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-03-26 22:56:00 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,33 @@
-# write your code here
+import os
+
+
+working_directory = os.getcwd()
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_file_path = os.path.join(working_directory, data_file_name)
+    data_file_path_report = os.path.join(working_directory, report_file_name)
+
+    report_source = open(data_file_path)
+    report_formatted = {}
+
+    for line in report_source:
+        if "," in line:
+            key, value = line.split(",")
+            report_formatted[key] = int(value) + report_formatted.get(key, 0)
+    report_source.close()
+
+    report_formatted["result"] = (
+        report_formatted["supply"] - report_formatted["buy"]
+    )
+
+    report = open(data_file_path_report, "x")
+
+    report.writelines(
+        [
+            f"supply,{report_formatted["supply"]}\n",
+            f"buy,{report_formatted["buy"]}\n",
+            f"result,{report_formatted["result"]}\n",
+        ]
+    )
+    report.close()
