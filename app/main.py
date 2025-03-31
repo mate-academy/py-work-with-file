030abd825dd05e28c02509510947c89ad9d4236a
Aleksander Rokita <kelor2@o2.pl>
2025-03-31 11:37:39 +0200

    Fix element order in report file

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    result = {}
+    report = {}
     with open(data_file_name, "r") as file:
         for line in file:
             key, value = line.split(",")
             key = str(key)
             value = int(value)
-            result.update({key: result.get(key, 0) + value})
-        result.update({"result": result.get("supply") - result.get("buy")})
+            report.update({key: report.get(key, 0) + value})
+        report.update({"result": report.get("supply") - report.get("buy")})
 
     with open(report_file_name, "w") as file:
-        for key, value in result.items():
-            file.write(f"{key},{value}\n")
+        file.write(f"supply,{report.get("supply")}\n")
+        file.write(f"buy,{report.get("buy")}\n")
+        file.write(f"result,{report.get("result")}\n")
