008ce64e3d9c9a5801f11d1c7be5f6a69aab8157
andrey <serdyukov963@gmail.com>
2025-03-31 06:26:54 +0300

    solution""

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    report = {'supply': 0, 'buy': 0, 'result': 0}
+    report = {"supply": 0, "buy": 0, "result": 0}
     with open(data_file_name, 'r') as f:
         for line in f:
             formated_line = line.split(',')
             report[formated_line[0]] += int(formated_line[1])
 
-    report['result'] = report['supply'] - report['buy']
+    report["result"] = report["supply"] - report["buy"]
 
     with open(report_file_name, 'w') as rep:
-        rep.write(f"supply,{report['supply']}\n")
-        rep.write(f"buy,{report['buy']}\n")
-        rep.write(f"result,{report['result']}\n")
+        rep.write(f"supply,{report["supply"]}\n")
+        rep.write(f"buy,{report["buy"]}\n")
+        rep.write(f"result,{report["result"]}\n")
 
     rep.close()
     f.close()
