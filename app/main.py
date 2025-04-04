0b90156d816240ee9da431579560db3adeec9020
nastya <hociy2015@gmail.com>
2025-04-04 19:35:18 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,29 +1,14 @@
-import csv
-
-
-def create_report(data_file_name: str, report_file_name: str):
-    total_supply = 0
-    total_buy = 0
-
-    with open(data_file_name, mode='r', newline='') as infile:
-        reader = csv.reader(infile)
-        for row in reader:
-            if len(row) == 2:
-                operation_type = row[0].strip()
-                try:
-                    amount = int(row[1].strip())
-                except ValueError:
-                    continue
-
-                if operation_type == 'supply':
-                    total_supply += amount
-                elif operation_type == 'buy':
-                    total_buy += amount
-
-    result = total_supply - total_buy
-
-    with open(report_file_name, mode='w', newline='') as outfile:
-        writer = csv.writer(outfile)
-        writer.writerow(['supply', total_supply])
-        writer.writerow(['buy', total_buy])
-        writer.writerow(['result', result])
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply_total = 0
+    buy_total = 0
+    data_file = open(data_file_name)
+    for line in data_file:
+        operation, amount = line.split(",")
+        amount = int(amount)
+        if operation == "supply":
+            supply_total += amount
+        elif operation == "buy":
+            buy_total += amount
+    result = supply_total - buy_total
+    report = open(report_file_name, "w")
+    report.write(f"supply,{supply_total}\nbuy,{buy_total}\nresult,{result}\n")
