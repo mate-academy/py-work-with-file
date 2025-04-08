d2ec8cd4baa1669420eebb75af51f4f83e78ceaa
Vitalik <lg.mobile0808@gmail.com>
2025-04-08 14:31:38 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     supply = 0
     buy = 0
-    file = open(data_file_name.csv, "r")
+    data_file = open(data_file_name.csv, "r")
 
-    for line in file:
+    for line in data_file:
         if line.strip():
             parts = line.strip().split(",")
             operation = parts[0]
@@ -14,7 +14,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             elif operation == "buy":
                 buy += amount
 
-    file.close()
+    data_file.close()
 
     report = open(report_file_name, "w")
     report.write(f"supply,{supply}\n")
