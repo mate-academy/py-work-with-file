bb5958ccf7a4124a12676a5086cdd9515a3c8ccb
Vitalik <lg.mobile0808@gmail.com>
2025-04-08 14:28:55 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-def create_report(data_file_name: str, report_file_name: str):
+def create_report(data_file_name: str, report_file_name: str) -> None:
     supply = 0
     buy = 0
-    file = open(data_file_name, "r")
+    file = open(data_file_name.csv, "r")
 
     for line in file:
         if line.strip():
-            parts = line.strip().split(',')
+            parts = line.strip().split(",")
             operation = parts[0]
             amount = int(parts[1])
 
