fd349d0b425a44332cbb5b93a269dda010384353
yuraf <twinkreferal22847228@gmail.com>
2025-04-25 16:00:27 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -3,22 +3,20 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     buy_total = 0
 
     try:
-        data_file = open(data_file_name, 'r')
+        data_file = open(data_file_name, "r")
         for line in data_file:
             if line.strip():
-                operation, amount = line.strip().split(',')
-                if operation == 'supply':
+                operation, amount = line.strip().split(",")
+                if operation == "supply":
                     supply_total += int(amount)
-                elif operation == 'buy':
+                elif operation == "buy":
                     buy_total += int(amount)
     except FileNotFoundError:
-        print('File not found')
-
-
+        print("File not found")
 
     result = supply_total - buy_total
 
-    report_file = open(report_file_name, 'w')
+    report_file = open(report_file_name, "w")
     report_file.write(f"supply,{supply_total}\n")
     report_file.write(f"buy,{buy_total}\n")
     report_file.write(f"result,{result}\n")
