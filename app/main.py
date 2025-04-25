8b7215b7d0a257a7b368290ad75b7654dd48c247
Andriy125 <yaruta.andriy@gmail.com>
2025-04-26 00:02:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    input_file = open(data_file_name, "r")
+    data = input_file.read()
+    words = {}
+    for line in data.strip().split("\n"):
+        parts = line.split(",")
+        words[parts[0]] = words.get(parts[0], 0) + int(parts[-1])
+
+    words["result"] = words["supply"] - words["buy"]
+    order = ["supply", "buy", "result"]
+    result = "".join(f"{key},{words[key]}\n" for key in order if key in words)
+    print(result)
+    with open(report_file_name, "w") as output_file :
+        output_file.write(result)
