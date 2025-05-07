b4b79275860f7c409eb33bcea1eed28d42ec32bd
D.Spyrydonov <dakurlzz1543@gmail.com>
2025-05-07 19:47:34 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    operations = {}
+
+    try:
+        with open("../" + data_file_name, "r") as data_file:
+            for line in data_file:
+                print(line)
+                operation, amount = line.split(",")
+                if operation not in operations:
+                    operations[operation] = int(amount)
+                else:
+                    operations[operation] += int(amount)
+
+        with open(report_file_name, "w") as report_file:
+            result = operations["supply"] - operations["buy"]
+
+            report_file.write(f"supply,{operations["supply"]}\n"
+                              f"buy,{operations["buy"]}\n"
+                              f"result,{result}\n")
+    except ValueError as error:
+        print(error)
+
+
