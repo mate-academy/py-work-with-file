9402c41ca565390bcb804c1af31643e2d59527da
Alex Shap <aleks.shaparenko@gmail.com>
2025-05-17 16:59:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    calculated_results = {}
+
+    with (open(data_file_name, "r") as data,
+          open(report_file_name, "w") as report):
+        for line in data:
+            line = line.strip()
+            if not line:
+                continue
+
+            product, quantity = line.split(",")
+
+            if product not in calculated_results:
+                calculated_results[product] = int(quantity)
+            else:
+                calculated_results[product] += int(quantity)
+
+        calculated_results["result"] = (
+            calculated_results.get("supply", 0)
+            - calculated_results.get("buy", 0)
+        )
+
+        for key in ["supply", "buy", "result"]:
+            value = calculated_results.get(key, 0)
+            report.write(f"{key},{value}\n")
