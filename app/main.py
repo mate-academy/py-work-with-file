988cd882d4f7492f783ed51188903f2ff28c2c27
m3 <sergey.radik@gmail.com>
2025-05-18 17:04:50 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -20,4 +20,4 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     with open(report_file_name, "w") as report_file:
         report_file.write(f"supply,{supply}\n")
         report_file.write(f"buy,{buy}\n")
-        report_file.write(f"result,{new_data["result"]}\n")
+        report_file.write(f'result,{new_data["result"]}\n')
