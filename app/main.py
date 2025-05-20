09c774ec7bf4dcd81b17c81c8a483dda3a9adb70
MiKPo4eLiK <stassojcuk@gmail.com>
2025-05-20 12:20:41 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+import csv
+
+def process_csv(data_file_name: str, report_file_name: str):
+    supply_total = 0
+    buy_total = 0
+
+    with open(data_file_name, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if row:
+                operation, amount = row[0], int(row[1])
+                if operation == "supply":
+                    supply_total += amount
+                elif operation == "buy":
+                    buy_total += amount
+
+    result = supply_total - buy_total
+
+    with open(report_file_name, mode='w', newline='') as reportfile:
+        writer = csv.writer(reportfile)
+        writer.writerow(["supply", supply_total])
+        writer.writerow(["buy", buy_total])
+        writer.writerow(["result", result])
+
+
+process_csv("input.csv", "report.csv")
