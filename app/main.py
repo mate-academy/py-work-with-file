89f208fcdedb24c08d92c8d0e58f8bf89f87a961
MiKPo4eLiK <stassojcuk@gmail.com>
2025-05-20 12:38:01 +0300

    well

--- app/main.py
+++ app/main.py
@@ -1,21 +1,35 @@
 import csv
+import os
 
 
 def process_csv(data_file_name: str, report_file_name: str) -> None:
     supply_total = 0
     buy_total = 0
 
+    if not os.path.exists(data_file_name):
+        raise FileNotFoundError(f"File {data_file_name} not found!")
+
     with open(data_file_name, newline="") as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-            if row:
+            if len(row) < 2:
+                continue
+
+            try:
                 operation, amount = row[0], int(row[1])
-                if operation == "supply":
-                    supply_total += amount
-                elif operation == "buy":
-                    buy_total += amount
+            except ValueError:
+                continue
+
+            if operation == "supply":
+                supply_total += amount
+            elif operation == "buy":
+                buy_total += amount
 
     result = supply_total - buy_total
+
+    report_dir = os.path.dirname(report_file_name)
+    if report_dir and not os.path.exists(report_dir):
+        os.makedirs(report_dir)
 
     with open(report_file_name, mode="w", newline="") as reportfile:
         writer = csv.writer(reportfile)
