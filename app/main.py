82e7d848cbfc9b0835f3370e39c905a1b866ae87
Mariia Çevik <ilchyshyn.marichka@gmail.com>
2025-05-23 17:46:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    res = {}
+    with open(data_file_name, "r") as data:
+        content = data.readlines()
+        for line in content:
+            line = line.strip().split(",")
+            if line[0] and line[0] not in res:
+                res[line[0]] = int(line[1].strip())
+            elif line[0]:
+                res[line[0]] += int(line[1].strip())
+            else:
+                continue
+
+    result = res["supply"] - res["buy"]
+
+    with open(report_file_name, "w") as report:
+        report.write(f"supply,{res["supply"]}\n")
+        report.write(f"buy,{res["buy"]}\n")
+        report.write(f"result,{result}\n")
