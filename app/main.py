5b844e58f68912f3017f9a11cf6bf782c8302df0
Serhii Kermas <serhii.kermas@gmail.com>
2025-05-28 16:08:49 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
-from ctypes import HRESULT
+# from ctypes import HRESULT
 import csv
 
 
 def create_report(data_file_name: str, report_file_name: str) -> None:
     result_dict = {}
-    with (open(data_file_name, newline='') as data_file,
-          open(report_file_name, "w", newline='') as report_file):
+    with (open(data_file_name, newline="") as data_file,
+          open(report_file_name, "w", newline="") as report_file):
 
         reader = csv.DictReader(data_file)
         fieldnames = reader.fieldnames
 
-        writer = csv.DictWriter(report_file, fieldnames = fieldnames)
+        writer = csv.DictWriter(report_file, fieldnames=fieldnames)
         writer.writeheader()
 
         for row in reader:
