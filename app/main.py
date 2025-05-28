b419297e10be3e86305b19bbf739f9c43e71207f
Serhii Kermas <serhii.kermas@gmail.com>
2025-05-28 16:03:45 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+from ctypes import HRESULT
+import csv
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    result_dict = {}
+    with (open(data_file_name, newline='') as data_file,
+          open(report_file_name, "w", newline='') as report_file):
+
+        reader = csv.DictReader(data_file)
+        fieldnames = reader.fieldnames
+
+        writer = csv.DictWriter(report_file, fieldnames = fieldnames)
+        writer.writeheader()
+
+        for row in reader:
+            for key, value in row.items():
+                if key not in result_dict:
+                    result_dict[key] = value
+                else:
+                    result_dict[key] += value
+
+            writer.writerow(result_dict)
