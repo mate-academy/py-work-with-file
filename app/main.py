747214c1f64fc8af4a866938324b32f5b97b5358
Serhii Kermas <serhii.kermas@gmail.com>
2025-05-29 10:23:28 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,23 +1,21 @@
 # from ctypes import HRESULT
-import csv
 
 
 def create_report(data_file_name: str, report_file_name: str) -> None:
     result_dict = {}
-    with (open(data_file_name, newline="") as data_file,
-          open(report_file_name, "w", newline="") as report_file):
 
-        reader = csv.DictReader(data_file)
-        fieldnames = reader.fieldnames
+    with (open(data_file_name, "r") as data_file,
+          open(report_file_name, "a") as report_file):
 
-        writer = csv.DictWriter(report_file, fieldnames=fieldnames)
-        writer.writeheader()
+        for line in data_file:
+            if line:
+                operation_type = line.split(",")[0]
+                amount = int(line.split(",")[1])
 
-        for row in reader:
-            for key, value in row.items():
-                if key not in result_dict:
-                    result_dict[key] = value
-                else:
-                    result_dict[key] += value
+            if operation_type in result_dict:
+                result_dict[operation_type] += amount
+            else:
+                result_dict.update({operation_type : amount})
 
-            writer.writerow(result_dict)
+        for key, value in result_dict.items():
+            report_file.write(f"{key},{value}\n")
