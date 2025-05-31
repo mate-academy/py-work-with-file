11bb6d1c2688a1ace6a250029a43aeec108b2657
Denis <godlike.kipish@gmail.com>
2025-05-31 16:19:20 +0300

    RESULT

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    file1 = open(data_file_name, "rt")
+    supply = 0
+    buy = 0
+    for file in file1:
+        if "supply" in file:
+            supply += int(file.split(",")[-1])
+        if "buy" in file:
+            buy += int(file.split(",")[-1])
+    result = supply - buy
+    file1.close()
+    file2 = open(report_file_name, "w")
+    file2.write("supply" + "," + str(supply) + "\n")
+    file2.write("buy," + str(buy) + "\n")
+    file2.write("result," + str(result) + "\n")
+    file2.close()
