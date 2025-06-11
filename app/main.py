8270e18b55371bbd8a198d6b24f5d8893d3553ed
Maksym Subchak <donjkee2@gmail.com>
2025-06-11 18:09:05 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     result = {"supply": 0, "buy": 0}
-    file = open(data_file_name, "r")
-    for i in file:
+    file_name = open(data_file_name, "r")
+    for i in file_name:
         line = i.strip()
         if not line:
             continue
@@ -10,8 +10,12 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             result["supply"] += int(file_split[1])
         if file_split[0] == "buy":
             result["buy"] += int(file_split[1])
-    file.close()
+    file_name.close()
     result_minus = result["supply"] - result["buy"]
     file_report = open(report_file_name, "w")
-    file_report.write(f"supply,{result['supply']}\nbuy,{result['buy']}\nresult,{result_minus}\n")
+    file_report.write(
+        f"supply,{result['supply']}\n"
+        f"buy,{result['buy']}\n"
+        f"result,{result_minus}\n"
+    )
     file_report.close()
