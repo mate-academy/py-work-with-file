046b7f14c7d1be33669e10c54e61887402f75baa
turulko-oleksandr <SashaTurulko@i.ua>
2025-06-19 14:27:00 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def create_report(data_file_name: str,
                   report_file_name: str) -> None:
-    data_file = open(data_file_name, "r")
+    data_file = open(f'../{data_file_name}', "r")
     data = dict()
     line_number = 1
     for line in data_file:
@@ -22,5 +22,4 @@ def create_report(data_file_name: str,
     with open(report_file_name, "w") as f:
         f.write(f"supply,{data["supply"]}\n")
         f.write(f"buy,{data["buy"]}\n")
-
         f.write(f"result,{data["supply"] - data["buy"]}\n")
