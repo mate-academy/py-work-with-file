2f45f1c60268bd044e7dbef29c5af9287deb964e
Yuliia Trofimova <julia1golubenko@gmail.com>
2025-06-29 11:40:30 +0300

    Fix quote style issues

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
 from app.report import create_report
 
 if __name__ == "__main__":
-    create_report('data.csv', 'result.csv')
+    create_report("data.csv", "result.csv")
--- app/report.py
+++ app/report.py
@@ -2,20 +2,20 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     supply_total: int = 0
     buy_total: int = 0
 
-    with open(data_file_name, 'r') as data_file:
+    with open(data_file_name, "r") as data_file:
         for line in data_file:
             line = line.strip()
             if not line:
                 continue
-            operation, amount = line.split(',')
-            if operation == 'supply':
+            operation, amount = line.split(",")
+            if operation == "supply":
                 supply_total += int(amount)
-            elif operation == 'buy':
+            elif operation == "buy":
                 buy_total += int(amount)
 
     result: int = supply_total - buy_total
 
-    with open(report_file_name, 'w') as report_file:
+    with open(report_file_name, "w") as report_file:
         report_file.write(f"supply,{supply_total}\n")
         report_file.write(f"buy,{buy_total}\n")
         report_file.write(f"result,{result}\n")
