272de9e62a46da6362c7a6a47a75607d1432bfe5
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-07-06 22:30:39 +0300

    fixed files path

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     report = {}
 
-    with open(f"../{data_file_name}", "r") as file:
+    with open(data_file_name, "r") as file:
         for line in file:
             current_line = line.rstrip().split(",")
 
