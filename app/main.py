e00c627e33363f8a846c530d7dfa782711da50e1
Anastasiia Skorodied <anastasia.skoroded@gmail.com>
2025-07-08 13:40:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_to_write = {"supply": 0, "buy": 0, "result": 0}
+
+    data_file = open(data_file_name, "r")
+    for line in data_file:
+        data = line.split(",")
+        data_to_write[data[0]] += int(data[1])
+    data_file.close()
+
+    data_to_write["result"] = data_to_write["supply"] - data_to_write["buy"]
+
+    report_file = open(report_file_name, "w")
+    for key, value in data_to_write.items():
+        report_file.write(f"{key},{value}\n")
+    report_file.close()
