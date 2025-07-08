98d48dfe00dc4782aecd32e30c904776736aea9b
Pavlo Semenikhin <pashock1337@gmail.com>
2025-07-08 15:17:32 +0300

    changed solution py work with file

--- app/main.py
+++ app/main.py
@@ -2,19 +2,18 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     supply_total = 0
     buy_total = 0
 
-    with open(data_file_name, "r") as f:
-        for line in f:
-            row = line.strip().split(",")
-            if row[0] == "supply":
-                value = int(row[1])
+    with open(data_file_name, "r") as input_file:
+        for row in input_file:
+            parts = row.strip().split(",")
+            if parts[0] == "supply":
+                value = int(parts[1])
                 supply_total += value
-            elif row[0] == "buy":
-                value = int(row[1])
+            elif parts[0] == "buy":
+                value = int(parts[1])
                 buy_total += value
-
         result = supply_total - buy_total
 
-    with open(report_file_name, "w") as w:
-        w.write(f"supply,{str(supply_total)}\n")
-        w.write(f"buy,{str(buy_total)}\n")
-        w.write(f"result,{str(result)}\n")
+    with open(report_file_name, "w") as output_file:
+        output_file.write(f"supply,{str(supply_total)}\n")
+        output_file.write(f"buy,{str(buy_total)}\n")
+        output_file.write(f"result,{str(result)}\n")
