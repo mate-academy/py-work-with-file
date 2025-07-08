efa1be6c252b280b441a7caa0e45fb068d7be526
Dmytro Shevchuk <shevchukkdmytro@gmail.com>
2025-07-08 21:05:44 +0300

    single quotes fix

--- app/main.py
+++ app/main.py
@@ -7,6 +7,6 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             report[operation] = report.get(operation, 0) + int(amount)
 
     with open(report_file_name, "w") as f:
-        f.write(f"supply,{report.get("supply", 0)}\n")
-        f.write(f"buy,{report.get("buy", 0)}\n")
-        f.write(f"result,{report.get("supply", 0) - report.get("buy", 0)}\n")
+        f.write(f"supply,{report.get('supply', 0)}\n")
+        f.write(f"buy,{report.get('buy', 0)}\n")
+        f.write(f"result,{report.get('supply', 0) - report.get('buy', 0)}\n")
