00393acc0fd498e05cb0000a2f69e8c3b366d117
Oleksii Kostenikov <akotenikov@gmail.com>
2025-07-08 20:21:56 +0300

    Fix flake8 quotes error

--- app/main.py
+++ app/main.py
@@ -7,6 +7,6 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
         for line in lines:
             data_dict[line[0]] = data_dict.get(line[0], 0) + int(line[1])
     with open(report_file_name, "w") as report:
-        report.write(f"supply,{data_dict["supply"]}\n")
-        report.write(f"buy,{data_dict["buy"]}\n")
-        report.write(f"result,{data_dict["supply"] - data_dict["buy"]}\n")
+        report.write(f'supply,{data_dict["supply"]}\n')
+        report.write(f'buy,{data_dict["buy"]}\n')
+        report.write(f'result,{data_dict["supply"] - data_dict["buy"]}\n')
