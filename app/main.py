121b6b89628f55cd730e88f7d81f518180d00636
Dmytro Shevchuk <shevchukkdmytro@gmail.com>
2025-07-08 20:55:42 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    report: dict = {}
+
+    with open(data_file_name, "r") as f:
+        for line in f:
+            operation, amount = line.strip().split(",")
+            report[operation] = report.get(operation, 0) + int(amount)
+
+    with open(report_file_name, "w") as f:
+        f.write(f"supply,{report.get("supply", 0)}\n")
+        f.write(f"buy,{report.get("buy", 0)}\n")
+        f.write(f"result,{report.get("supply", 0) - report.get("buy", 0)}\n")
