6fd0604ca62db14451cddbd85c03cbd4a1f8883b
SensibleN00B <sensible.noob@gmail.com>
2025-07-09 15:21:14 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    operations = {
+        "supply": 0,
+        "buy": 0,
+        "result": 0
+    }
+    try:
+        with open(data_file_name, "r") as file:
+            for line in file:
+                line = line.split(",")
+                if line[0] in operations:
+                    operations[line[0]] += int(line[1])
+    except FileNotFoundError as error:
+        print(error)
+    operations["result"] = operations["supply"] - operations["buy"]
+    try:
+        with open(report_file_name, "a") as file:
+            for key, value in operations.items():
+                file.write(f"{key},{value}\n")
+
+    except Exception as error:
+        print(error)
