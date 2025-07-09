d14fccf9728ba235972cf25a79cc5039ce2d40d0
Vasyl <jomajok7777@gmail.com>
2025-07-09 22:23:32 +0300

    Created function create_report

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_file = open(data_file_name)
+    supply = 0
+    buy = 0
+
+    for line in data_file.readlines():
+        if "supply" in line:
+            supply += int(line.split(",")[1])
+        else:
+            buy += int(line.split(",")[1])
+    data_file.close()
+
+    report = open(report_file_name, "w+")
+
+    report.write(f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n")
+    report.close()
