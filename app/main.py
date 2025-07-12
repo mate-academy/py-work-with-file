3cfa6aee3272ecb5a8314e7d5baac2121ad57377
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2025-07-12 18:55:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    report_data = {
+        "supply": 0,
+        "buy": 0,
+        "result": 0
+    }
+    with open(data_file_name, "r") as read_file:
+        for line in read_file:
+            name, number = line.split(",")
+            if name in report_data:
+                report_data[name] += int(number)
+    report_data["result"] = report_data["supply"] - report_data["buy"]
+    with open(report_file_name, "w") as save_file:
+        for key, value in report_data.items():
+            save_file.write(f"{key},{value}\n")
