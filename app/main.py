00822abc5519a72f756f31ffbd70f29acebbfd9c
yarik <super.racing@ukr.net>
2025-07-17 17:03:43 +0300

    python style

--- app/main.py
+++ app/main.py
@@ -5,10 +5,11 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
         if len(line) == 0:
             continue
         values = line.split(",")
-        if values[0] not in my_dict:
-            my_dict[values[0]] = int(values[1])
+        label, value = values
+        if label not in my_dict:
+            my_dict[label] = int(value)
         else:
-            my_dict[values[0]] += int(values[1])
+            my_dict[label] += int(value)
     my_file.close()
     my_file = open(report_file_name, "w")
     my_file.write(f"supply,{my_dict['supply']}\n")
