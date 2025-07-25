e895f10372ffcc1571cb57b81697f9303fcf7a96
Stanislav Vdovychenko <vdovychenkostas@gmail.com>
2025-07-25 21:38:14 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -14,6 +14,6 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
                     result[elements[0]] += int(elements[1])
 
         with open(report_file_name, "w") as report_file:
-            report_file.write(f"supply,{result["supply"]}\n")
-            report_file.write(f"buy,{result["buy"]}\n")
-            report_file.write(f"result,{result["supply"] - result["buy"]}\n")
+            report_file.write(f"supply,{result['supply']}\n")
+            report_file.write(f"buy,{result['buy']}\n")
+            report_file.write(f"result,{result['supply'] - result['buy']}\n")
