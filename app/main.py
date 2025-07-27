30d16591b2a05967b693ccc7238180f43ada99f1
sSPaiSs <naumenkoroma27@gmail.com>
2025-07-27 17:11:06 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     result = result_dict["supply"] - result_dict["buy"]
 
     output_file = open(report_file_name, "w")
-    output_file.write(f"supply,{result_dict["supply"]}\n")
-    output_file.write(f"buy,{result_dict["buy"]}\n")
+    output_file.write(f"supply,{result_dict['supply']}\n")
+    output_file.write(f"buy,{result_dict['buy']}\n")
     output_file.write(f"result,{result}\n")
     output_file.close()
