2f46806664d58eb1e0ec77262223e8119b23cd02
Oleh Bondarchuk <127667913+olehbond96@users.noreply.github.com>
2025-07-29 18:43:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import os
 
+
 def create_report(data_file_name: str, report_file_name: str) -> str:
     current_script_dir = os.path.dirname(os.path.abspath(__file__))
     base_dir = os.path.dirname(current_script_dir)
