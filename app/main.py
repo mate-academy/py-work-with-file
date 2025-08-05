2532fb54ce6d06184d542bac65e778f1eabd2ee7
Maksym Pakula <gargantadoto@gmail.com>
2025-08-05 16:12:48 +0300

    added solution for work with file task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    working_file = open(data_file_name, "r")
+    result = {"supply": 0, "buy": 0, "result": 0}
+
+    for line in working_file:
+        if "supply" in line:
+            result["supply"] += int(line.split(",")[1])
+        elif "buy" in line:
+            result["buy"] += int(line.split(",")[1])
+    result["result"] = result["supply"] - result["buy"]
+    for key, value in result.items():
+        with open(report_file_name, "a") as report:
+            report.write(f"{key},{value}\n")
+
+    working_file.close()
