32bb659f2925a7cade05932e4470dead8299bcc4
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2025-08-26 12:14:47 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+def create_report(
+    data_file_name: str,
+    report_file_name: str
+) -> None:
+    data_file = open(data_file_name, "r")
+    results = {
+        "buy": 0,
+        "supply": 0,
+        "result": 0
+    }
+    for data_line in data_file:
+        if data_line == "":
+            break
+        service, value = data_line.split(",")
+        value = int(value)
+        results[service] += value
+
+    data_file.close()
+    results["result"] = results["supply"] - results["buy"]
+
+    report_file = open(report_file_name, "w")
+    report_file.write(f"supply,{results["supply"]}\n")
+    report_file.write(f"buy,{results["buy"]}\n")
+    report_file.write(f"result,{results["result"]}\n")
+
+    report_file.close()
