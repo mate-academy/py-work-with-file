72bab54a333e7986fd469c9ea6e40b157b5f4a43
MykytaBuildApps <mykyta.havryliuk@buildapps.pro>
2025-08-31 20:50:03 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     report = {"supply": 0, "buy": 0}
-
     with open(data_file_name, "r") as input_file:
         for line in input_file:
             line = line.strip()
@@ -13,9 +12,9 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
 
     with open(report_file_name, "w") as output_file:
         output_file.write(
-            f"supply,{report['supply']}\n"
-            f"buy,{report['buy']}\n"
-            f"result,{result}\n"
+            f"supply, {report['supply']}\n"
+            f"buy, {report['buy']}\n"
+            f"result, {result}\n"
         )
 
     print(f"Report created: {report_file_name}")
