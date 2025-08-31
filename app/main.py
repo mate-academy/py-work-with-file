d953eaf2b491ead51575991665bf5f25ae4f44b1
MykytaBuildApps <mykyta.havryliuk@buildapps.pro>
2025-08-31 20:50:34 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -7,14 +7,9 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
                 continue
             operation, count = line.split(",")
             report[operation] += int(count)
-
     result = report["supply"] - report["buy"]
-
     with open(report_file_name, "w") as output_file:
         output_file.write(
-            f"supply, {report['supply']}\n"
-            f"buy, {report['buy']}\n"
-            f"result, {result}\n"
+            f"supply, {report["supply"]}\nbuy, {report["buy"]}\nresult, {result}\n"
         )
-
     print(f"Report created: {report_file_name}")
