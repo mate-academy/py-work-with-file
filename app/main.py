855846e3df187228d819a875581fa25186463384
Vladislav <ilichoff88@gmail.com>
2025-09-02 00:35:39 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 def create_report(data_file_name: str, report_file_name: str):
     supply = 0
     buy = 0
+
     with open(data_file_name, "r") as f:
         for line in f:
             line = line.strip()
             if not line:
                 continue
             operation, amount = line.split(",")
+            amount = int(amount)
             if operation == "supply":
-                supply += int(amount)
+                supply += amount
             elif operation == "buy":
-                buy += int(amount)
+                buy += amount
 
     with open(report_file_name, "w") as fw:
         fw.write(f"supply,{supply}\n")
