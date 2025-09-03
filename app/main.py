4fe5b10dd7d360b9035d7dfbb9b57bbc649e02b3
Oleksii-Hrechukha <Oleksii_Hrechukha@outlook.com>
2025-09-03 22:29:33 +0300

    Solution_first_step

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import csv
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    result_dict = {}
+
+    with (open(data_file_name, newline="") as input_file,
+          open(report_file_name, "w", newline="") as output_file):
+        reader = csv.reader(input_file)
+        for line in reader:
+            if line[0] in result_dict:
+                result_dict[line[0]] += int(line[1])
+            else:
+                result_dict[line[0]] = int(line[1])
+        max_value = max(result_dict.values())
+        num_difference = max_value - (sum(result_dict.values()) - max_value)
+        output_file.write(f"supply,{result_dict["supply"]}\n"
+                          f"buy,{result_dict["buy"]}\n"
+                          f"result,{num_difference}\n")
