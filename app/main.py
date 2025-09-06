cc18a4f620379d646f49b1c268b75a409120dba6
Yurii <yurij.kenu@gmail.com>
2025-09-06 18:22:43 +0200

    create-report: implemented

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as f:
+        content = f.read()
+
+    supply = buy = 0
+    lines = content.splitlines()
+
+    for line in lines:
+        key, value = line.split(",")
+        if key == "supply":
+            supply += int(value)
+        if key == "buy":
+            buy += int(value)
+
+    with open(report_file_name, "w") as f:
+        f.write(f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n")
