2b12ad505bc5c6884209784fb348ce6963928ecc
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2025-09-23 23:50:37 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
-def create_report(data_file_name: str, report_file_name: str):
-    file = open(data_file_name, "tr")
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    file_name = open(data_file_name, "r")
     res_dict = {}
-    for line in file:
+    for line in file_name:
         list_of_words = line.split(",")
         if res_dict.get(list_of_words[0]) is None:
             res_dict[list_of_words[0]] = int(list_of_words[1])
         else:
             res_dict[list_of_words[0]] += int(list_of_words[1])
-    file.close()
+    file_name.close()
     res_dict["result"] = res_dict["supply"] - res_dict["buy"]
 
     report = open(report_file_name, "w")
