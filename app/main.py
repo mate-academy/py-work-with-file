a36be534767eff772a137142b3634c7ad27e8999
Anton Bilous <anton.bilous.job@gmail.com>
2025-09-27 16:55:40 +0300

    save

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
 # write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    from_file = open(data_file_name)
+    to_file = open(report_file_name, "w")
+
+    result = {
+        "supply": 0,
+        "buy": 0,
+        "result": 0,
+    }
+
+    for line in from_file.readlines():
+        if len(line) > 0:
+            text = line.split(",")
+            result[text[0]] += int(text[1])
+    from_file.close()
+
+    result["result"] = result["supply"] - result["buy"]
+
+    for key, item in result.items():
+        to_file.write(f"{key},{item}\n")
+    to_file.close()
