821539092de3a1901a86b40e1ecb3decbb23c091
Maksym Dazidov <maksymdazidov@gmail.com>
2025-10-10 16:44:02 +0200

    file 7

--- app/main.py
+++ app/main.py
@@ -2,37 +2,15 @@ def create_report(
         data_file_name: str,
         report_file_name: str
 ) -> None:
-    counter = 0
     supply = 0
     buy = 0
     data_file = open(data_file_name)
-    for line in data_file.readlines():
-        if "buy" in line:
-            data_file.read(4)
-            if data_file.read(1).isnumeric():
-                counter += 1
-                if data_file.read(1).isnumeric():
-                    counter += 1
-                    if data_file.read(1).isnumeric():
-                        counter += 1
-                        if data_file.read(1).isnumeric():
-                            counter += 1
-            data_file.seek(4)
-            buy += int(data_file.read(counter))
-            counter = 0
+    for line in data_file:
+        operator, value = line.split(",")
+        if "buy" == operator:
+            buy += int(value)
         else:
-            data_file.read(7)
-            if data_file.read(1).isnumeric():
-                counter += 1
-                if data_file.read(1).isnumeric():
-                    counter += 1
-                    if data_file.read(1).isnumeric():
-                        counter += 1
-                        if data_file.read(1).isnumeric():
-                            counter += 1
-                data_file.seek(7)
-                supply += int(data_file.read(counter))
-                counter = 0
+            supply += int(value)
     data_file.close()
     file_report = open(report_file_name, "w")
     file_report.write(f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n")
