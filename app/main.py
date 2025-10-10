13e63b48cc5dc8f71e67668fed0a34aa4fdc2576
Ihor Zasimenko <boxletter2011@gmail.com>
2025-10-10 18:55:21 +0300

    update code

--- app/main.py
+++ app/main.py
@@ -1,25 +1,24 @@
+import csv
+
+
 def create_report(data_file_name: str, report_file_name: str) -> None:
     total_supply: int = 0
     total_buy: int = 0
 
     with open(data_file_name, encoding="utf-8") as data_file:
-        for line in data_file:
-            line = line.strip()
-            if not line:
+        reader = csv.reader(data_file)
+        for row in reader:
+            if not row:
                 continue
-            operation, amount_str = line.split(",")
-            amount = int(amount_str)
+            operation, amount = row
             if operation == "supply":
-                total_supply += amount
+                total_supply += int(amount)
             elif operation == "buy":
-                total_buy += amount
+                total_buy += int(amount)
 
     result: int = total_supply - total_buy
-    report_lines: list[str] = [
-        f"supply,{total_supply}",
-        f"buy,{total_buy}",
-        f"result,{result}",
-    ]
 
     with open(report_file_name, "w", encoding="utf-8") as report_file:
-        report_file.write("\n".join(report_lines))
+        report_file.write(f"supply,{total_supply}\n")
+        report_file.write(f"buy,{total_buy}\n")
+        report_file.write(f"result,{result}\n")
