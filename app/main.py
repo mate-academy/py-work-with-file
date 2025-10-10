7b3dc27bcf4cb7a9b6cee9c8d5ed6ec4ec87d39b
Maksym Dazidov <maksymdazidov@gmail.com>
2025-10-10 16:15:27 +0200

    file 2

--- app/main.py
+++ app/main.py
@@ -5,35 +5,35 @@ def create_report(
     counter = 0
     supply = 0
     buy = 0
-    file = open(data_file_name)
-    for line in file.readlines():
+    data_file = open(data_file_name)
+    for line in data_file.readlines():
         if "buy" in line:
-            file.read(4)
-            if file.read(1).isnumeric():
-                counter =+ 1
-                if file.read(1).isnumeric():
+            data_file.read(4)
+            if data_file.read(1).isnumeric():
+                counter = +1
+                if data_file.read(1).isnumeric():
                     counter =+ 1
-                    if file.read(1).isnumeric():
+                    if data_file.read(1).isnumeric():
                         counter =+ 1
-                        if file.read(1).isnumeric():
+                        if data_file.read(1).isnumeric():
                             counter += 1
-            file.seek(4)
-            buy += file.read(counter)
+            data_file.seek(4)
+            buy += data_file.read(counter)
             counter = 0
-        else:
-            file.read(7)
-            if file.read(1).isnumeric():
+    else:
+        data_file.read(7)
+        if data_file.read(1).isnumeric():
+            counter =+ 1
+            if data_file.read(1).isnumeric():
                 counter =+ 1
-                if file.read(1).isnumeric():
+                if data_file.read(1).isnumeric():
                     counter =+ 1
-                    if file.read(1).isnumeric():
-                        counter =+ 1
-                        if file.read(1).isnumeric():
-                            counter += 1
-            file.seek(7)
-            supply += file.read(counter)
+                    if data_file.read(1).isnumeric():
+                        counter += 1
+            data_file.seek(7)
+            supply += data_file.read(counter)
             counter = 0
-    file.close()
+    data_file.close()
     file_report = open(report_file_name, "w")
     file_report.write(f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}")
     file_report.close()
