adcf8bc3324352e66c695fac950d83c07173edf1
Vitalii Stetsenko <stvitalik01062006@gmail.com>
2025-10-20 15:22:10 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    f_1 = open(data_file_name)
+    supply_total = 0
+    buy_total = 0
+    for line in f_1:
+        if not line.strip():
+            continue
+        operation, amount = line.strip().split(",")
+        if operation == "supply":
+            supply_total += int(amount)
+        elif operation == "buy":
+            buy_total += int(amount)
+    result = supply_total - buy_total
+    f_2 = open(report_file_name, "w")
+    f_2.write(f"supply,{supply_total}\n")
+    f_2.write(f"buy,{buy_total}\n")
+    f_2.write(f"result,{result}\n")
