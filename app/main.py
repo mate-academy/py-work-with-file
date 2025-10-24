4ab1af4848fb41ec6f0e1a4bc3b2538e121c1bb8
Veniamin Nekhoda <venimain.nekhoda@gmail.com>
2025-10-24 21:09:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def create_report(data_file_name: str, report_file: str) :
+    file = open(data_file_name, 'r')
+    d = {}
+    for line in file :
+        d[line.split(",")[0]] += d.get(line.split(",")[0], 0)
+    file.close()
+    file = open(report_file, 'w')
+    for key, value in d.items() :
+        file.write(key + "," + str(value) + "\n")
+
+
