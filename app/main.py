ee717997bcd801ba029e162e526f4ab34e87b9ab
Veniamin Nekhoda <venimain.nekhoda@gmail.com>
2025-10-25 22:24:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-def create_report(data_file_name: str, report_file: str) :
-    file = open(data_file_name, 'r')
-    d = {}
-    for line in file :
-        d[line.split(",")[0]] += d.get(line.split(",")[0], 0)
-    file.close()
-    file = open(report_file, 'w')
-    for key, value in d.items() :
-        file.write(key + "," + str(value) + "\n")
-
-
+def create_report(data_file_name: str, report_file: str) -> None:
+    data_dict = {"supply": 0, "buy": 0, }
+    data = open(data_file_name, "r")
+    for line in data :
+        data_dict[line.split(",")[0]] += int(line.split(",")[1])
+    data.close()
+    data_dict["result"] = data_dict["supply"] - data_dict["buy"]
+    report = open(report_file, "w")
+    for key, value in data_dict.items() :
+        report.write(key + "," + str(value) + "\n")
+    report.close()
