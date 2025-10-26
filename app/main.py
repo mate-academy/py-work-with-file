50a758dddb3bf237beda5207b1cef2e846da2a10
Mykhaylo Zhyhan <mz5157e@gre.ac.uk>
2025-10-26 21:23:58 +0000

    created a function that reads a file and creates a report based on data of this file and writes it into different file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+import csv
+import os
+os.chdir("/Users/admin/Documents/"
+         "mate academy learning /"
+         "github/py-work-with-file")
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply = 0
+    buy = 0
+    with open(data_file_name, "r") as data_file:
+        data = csv.reader(data_file)
+        for row in data:
+            operation, amount = row[0], int(row[1])
+            if operation == "supply":
+                supply += amount
+            elif operation == "buy":
+                buy += amount
+
+    with open(report_file_name, "w") as report_file:
+        report_file.write(f"supply,{supply}\n")
+        report_file.write(f"buy,{buy}\n")
+        report_file.write(f"result,{supply - buy}\n")
