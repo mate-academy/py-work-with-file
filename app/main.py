687d18af0f517f733e57e3479f6b3968d94ff166
viloker <yarmolykvadym@gmail.com>
2025-10-30 11:11:45 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_file = open(data_file_name, "r")
+    information = dict()
+
+    for value in data_file.readlines():
+
+        index = value.find(",")
+        key = value[: index]
+        number = int(value[index + 1:])
+        if information.get(key):
+
+            information[key] += number
+        else:
+
+            information[key] = number
+
+    data_file.close()
+
+    result = information["supply"] - information["buy"]
+
+    report_file = open(report_file_name, "a")
+    report_file.write(f"supply,{information["supply"]}\n")
+    report_file.write(f"buy,{information["buy"]}\n")
+    report_file.write(f"result,{result}\n")
+    report_file.close()
