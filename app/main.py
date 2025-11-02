0eaea0b187a2ddb5c667770da11b2fb81b33e60b
Wojciech Witkowski <witus85@gmail.com>
2025-11-02 11:03:53 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    read_file = open(data_file_name)
+    operations = {}
+    for line in read_file.readlines():
+        if line:
+            operation = line.split(",")
+            if operation[0] in operations:
+                operations[operation[0]] += int(operation[1])
+            else:
+                operations.update({operation[0]: int(operation[1])})
+    read_file.close()
+    report_file = open(report_file_name, "w")
+    report_file.write(f"supply,{operations["supply"]}\n")
+    report_file.write(f"buy,{operations["buy"]}\n")
+    report_file.write(f"result,{operations["supply"] - operations["buy"]}\n")
+    report_file.close()
