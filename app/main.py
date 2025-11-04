770bd851697c354eb50d8b9c3691cdcfc0c26291
steven42069 <stevendiaz152@gmail.com>
2025-11-03 20:21:48 -0500

    done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as f_in:
+        totals = {}
+        for line in f_in:
+            parts = line.strip().split(",")
+            if len(parts) != 2:
+                continue
+            if parts[0] not in totals:
+                totals[parts[0]] = int(parts[1])
+            else:
+                totals[parts[0]] += int(parts[1])
+    supply = totals.get("supply", 0)
+    bought = totals.get("buy", 0)
+    result = supply - bought
+    with open(report_file_name, "w") as f_out:
+        f_out.write(f"supply,{supply}\n")
+        f_out.write(f"buy,{bought}\n")
+        f_out.write(f"result,{result}\n")
