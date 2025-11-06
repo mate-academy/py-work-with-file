2fd54e87aa830b9a07a465c95db6984a80c6ff3e
Ostap Shp <stark171705@gmail.com>
2025-11-06 14:47:21 +0200

    changes done

--- app/main.py
+++ app/main.py
@@ -1,18 +1,7 @@
-import os
-
-
 def create_report(data_file_name: str, report_file_name: str) -> None:
-
-    app_dir = os.path.dirname(__file__)
-
-    project_root = os.path.dirname(app_dir)
-
-    data_path = os.path.join(project_root, data_file_name)
-    report_path = os.path.join(project_root, "tests", report_file_name)
-
     dictionary = {"supply": 0, "buy": 0}
 
-    with open(data_path) as file:
+    with open(data_file_name, encoding="utf-8") as file:
         for line in file:
             line = line.strip()
             if not line:
@@ -22,7 +11,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
 
     result = dictionary["supply"] - dictionary["buy"]
 
-    with open(report_path, "w") as report_file:
+    with open(report_file_name, "w", encoding="utf-8") as report_file:
         for key, value in dictionary.items():
             report_file.write(f"{key},{value}\n")
         report_file.write(f"result,{result}\n")
