46d575844c230d8d60104c9b58706bcecae3ccc3
Andrii Chychkan <mywork1728@gmail>
2025-11-06 20:48:19 +0200

    started parsing files

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import csv
+# data_file_name read from this
+# report_file_name write to this
+
+def create_report(data_file_name: str,
+                  report_file_name: str) -> None:
+    # file = open(data_file_name, "r")
+    # a = csv.reader(data_file_name, delimiter=',', quotechar='|')
+    # for row in a:
+    #     print(a)
+    with open(data_file_name, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            print(row)
+
+
+create_report("../apples.csv", "../bananas.csv")
+
+
+
