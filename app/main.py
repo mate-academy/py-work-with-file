e411a97d572d38172a472b18078345e6aecac183
Taras Ivashchuk <tvishy@disroot.org>
2025-11-07 20:53:14 +0200

    Initial commit

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply = 0
+    buy = 0
+
+    fd = open(data_file_name, "r")
+    for line in fd:
+        op_type, amount = line.split(",")
+        if op_type == "supply":
+            supply += int(amount)
+        elif op_type == "buy":
+            buy += int(amount)
+    fd.close()
+
+    fd = open(report_file_name, "w")
+    fd.write(f"supply,{supply}\n")
+    fd.write(f"buy,{buy}\n")
+    fd.write(f"result,{supply - buy}\n")
+    fd.close()
