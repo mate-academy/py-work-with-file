65d836109814cb42e60e36af16a2a2371aa4d118
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2025-11-08 23:30:52 +0200

    implemented changes according to flake8

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
         new_data["result"] = new_data["supply"] - new_data["buy"]
 
     with open(report_file_name, "w") as new_file:
-        new_file.write(f"supply,{new_data["supply"]}\n"
-                       f"buy,{new_data["buy"]}\n"
-                       f"result,{new_data["result"]}\n"
+        new_file.write(f'supply,{new_data["supply"]}\n'
+                       f'buy,{new_data["buy"]}\n'
+                       f'result,{new_data["result"]}\n'
                        )
