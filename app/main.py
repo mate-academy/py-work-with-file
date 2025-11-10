8d456228d8b94848ee4d6bc4b0daa7e4940631a6
HAIOVIY <gaevoywork@gmail.com>
2025-11-10 16:49:52 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    file = open(data_file_name)
+
+    dictionary = {}
+    for line in file:
+        line = line.strip()
+        if not line:
+            continue
+
+        key, value = line.split(",")
+
+        dictionary[key] = dictionary.get(key, 0) + value
+    file.close()
+
+    with open(report_file_name, "w", newline="") as report:
+        for key, value in dictionary.items():
+            report.write(f"{key},{value}\n")
+
+    report.close()
