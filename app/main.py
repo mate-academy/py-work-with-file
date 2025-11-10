18896d5576848e7a6f28a763c85dc8fdb39a5dfe
HAIOVIY <gaevoywork@gmail.com>
2025-11-10 17:07:47 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -12,9 +12,10 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
         dictionary[key] = dictionary.get(key, 0) + value
 
     with open(report_file_name, "w", newline="") as report:
-        for key, value in dictionary.items():
-            report.write(f"{key},{value}\n")
         supply_total = dictionary.get("supply", 0)
         buy_total = dictionary.get("buy", 0)
         result = supply_total - buy_total
+
+        report.write(f"supply,{supply_total}\n")
+        report.write(f"buy,{buy_total}\n")
         report.write(f"result,{result}\n")
