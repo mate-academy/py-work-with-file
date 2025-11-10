d26db636fd81c0c7fd9f476f6b170d229e318479
HAIOVIY <gaevoywork@gmail.com>
2025-11-10 16:51:50 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def create_report(data_file_name: str, report_file_name: str):
+def create_report(data_file_name: str, report_file_name: str) -> None:
     file = open(data_file_name)
 
     dictionary = {}
