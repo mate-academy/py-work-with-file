d0d48b32ac1642aedfba5a9bcf5e8e32ba95da6f
Jakub Paluch <qbusik@gmail.com>
2025-11-15 08:20:45 +0100

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,15 +1,19 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    data_file = open(data_file_name, "r")
     report_dict = {"supply": 0, "buy": 0}
 
-    for row in data_file:
-        key, item = row.rstrip("\n").split(",")
-        report_dict[key] += int(item)
-    report_dict.update({"result": report_dict["supply"] - report_dict["buy"]})
-
+    with open(data_file_name, "r") as data_file:
+        for row in data_file:
+            key, item = row.rstrip("\n").split(",")
+            report_dict[key] += int(item)
+        report_dict.update(
+            {"result": report_dict["supply"] - report_dict["buy"]}
+        )
+    supply = report_dict["supply"]
+    buy = report_dict["buy"]
+    result = report_dict["result"]
     with open(report_file_name, "w") as report_file:
         report_file.write(
-            f"supply,{report_dict["supply"]}\n"
-            f"buy,{report_dict["buy"]}\n"
-            f"result,{report_dict["result"]}\n"
+            f"supply,{supply}\n"
+            f"buy,{buy}\n"
+            f"result,{result}\n"
         )
