0f6290da36ffcda173ec5ada11ff36c410fbc716
tomech <109527747+tomech4@users.noreply.github.com>
2025-11-17 22:55:03 +0100

    add return type annotation as per flake8

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def create_report(data_file_name: str, report_file_name: str):
+def create_report(data_file_name: str, report_file_name: str) -> None:
     input_file = open(data_file_name, "r")
     report_dict = {
         "supply": 0,
