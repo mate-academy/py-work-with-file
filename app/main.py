ab79ab765f96539afba726b602b343c43f71d5d2
tomech <109527747+tomech4@users.noreply.github.com>
2025-11-17 22:53:53 +0100

    complete create_report function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    input_file = open(data_file_name, "r")
+    report_dict = {
+        "supply": 0,
+        "buy": 0
+    }
+
+    for line in input_file:
+        split_line = line.split(",")
+        key = split_line[0]
+        value = int(split_line[1])
+        report_dict[key] += value
+
+    report_dict["result"] = report_dict["supply"] - report_dict["buy"]
+    input_file.close()
+
+    output_file = open(report_file_name, "a")
+    for key, value in report_dict.items():
+        output_file.write(f"{key},{value}\n")
+    output_file.close()
