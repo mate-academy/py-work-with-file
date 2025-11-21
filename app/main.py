512ae0aab368557dca3772604b147243cb968489
Tomasz Szatałowicz <szatalowicztomek@gmail.com>
2025-11-21 18:28:04 +0100

    quest

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def create_report(data_file_name: str, report_file_name: str):
+def create_report(data_file_name: str, report_file_name: str) -> None:
     supply_total = 0
     buy_total = 0
     with open(data_file_name, "r") as file:
