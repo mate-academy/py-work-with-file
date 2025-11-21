dfd8131461101beff71ba8589899d7cdcb6749c4
Tomasz Szatałowicz <szatalowicztomek@gmail.com>
2025-11-21 18:22:10 +0100

    failed

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str ):
+    supply_total = 0
+    buy_total = 0
+    with open(data_file_name, "r") as file:
+        for line in file:
+            operation, value = line.strip().split(",")
+            value = int(value)
+
+            if operation == "supply":
+                supply_total += value
+            elif operation == "buy":
+                buy_total += value
+
+    result = supply_total - buy_total
+
+    with open(report_file_name, "w") as report_file:
+        report_file.write(f"supply,{supply_total}\n")
+        report_file.write(f"buy,{buy_total}\n")
+        report_file.write(f"result,{result}\n")
