abb1ac929884ec73473c6f060a767f7f8e4d3772
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-12-08 21:58:22 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name) as data_file:
+        data = {}
+        for line in data_file:
+            key, value = line.split(",")
+            if key in data:
+                data[key] += int(value.strip())
+            else:
+                data[key] = int(value.strip())
+        result = data["supply"] - data["buy"]
+    with open(report_file_name, "w") as report_file:
+        report_file.write(f"supply,{data['supply']}\n")
+        report_file.write(f"buy,{data['buy']}\n")
+        report_file.write(f"result,{result}\n")
+
+
+if __name__ == "__main__":
+    create_report(data_file_name="data.csv", report_file_name="report.csv")
