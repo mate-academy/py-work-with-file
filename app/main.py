de0e5f0c81a86adbe9aff319203c0ba0987682aa
vladiukdaria <vladiukdaria@gmail.com>
2025-12-12 16:03:29 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
 # write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply_total = 0
+    buy_total = 0
+
+    # Читаємо дані з файлу
+    with open(data_file_name, "r") as file:
+        for line in file:
+            line = line.strip()
