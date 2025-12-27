b14974b790d325a16ca3177eb0e93838a6654b01
ArtemZakhar <artemzakharchuk@gmail.com>
2025-12-27 12:48:37 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    file_date = open(data_file_name, "r").readlines()
+
+    operations = {
+        "supply": 0,
+        "buy": 0,
+    }
+
+    for line in file_date:
+        [operation, amount] = line.split(",")
+        operations[operation] += int(amount)
+
+    result = operations["supply"] - operations["buy"]
+    open(report_file_name, "w").write(
+        f'supply,{operations["supply"]}\nbuy,'
+        f'{operations["buy"]}\nresult,{result}\n'
+    )
