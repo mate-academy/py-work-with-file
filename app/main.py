f269b1c5222d2a96a4a2e0fea8ae37cf85181e97
ArtemZakhar <artemzakharchuk@gmail.com>
2025-12-27 12:51:52 +0200

    fefactoring

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    file_date = open(data_file_name, "r").readlines()
-
     operations = {
         "supply": 0,
         "buy": 0,
     }
 
-    for line in file_date:
-        [operation, amount] = line.split(",")
-        operations[operation] += int(amount)
+    with open(data_file_name, "r") as file:
+        file_date = file.readlines()
+
+        for line in file_date:
+            [operation, amount] = line.split(",")
+            operations[operation] += int(amount)
 
     result = operations["supply"] - operations["buy"]
-    open(report_file_name, "w").write(
-        f'supply,{operations["supply"]}\nbuy,'
-        f'{operations["buy"]}\nresult,{result}\n'
-    )
+    with open(report_file_name, "w") as file:
+        file.write(
+            f'supply,{operations["supply"]}\nbuy,'
+            f'{operations["buy"]}\nresult,{result}\n'
+        )
