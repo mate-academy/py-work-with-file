9e316237c45163ac570494dba7c606addd6903c8
Your Name <allanaumenko@Allas-MacBook-Air.local>
2026-01-03 15:50:08 +0800

    Fix flake8 W391 (remove trailing blank line)

--- app/main.py
+++ app/main.py
@@ -26,4 +26,3 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
         writer.writerow(["supply", supply_total])
         writer.writerow(["buy", buy_total])
         writer.writerow(["result", result])
-
