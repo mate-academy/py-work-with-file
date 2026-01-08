fef9212a9b2c1a68e4933e5f6eaed37dd2d3311f
Johan Rangel <johan.apr99@gmail.com>
2026-01-08 17:44:32 +0000

    commented

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ def create_report(data_file_name: str, report_file_name: str):
             key, value = line.strip().split(',')
             dict_data[key] = dict_data.get(key, 0) + int(value)
 
+        dict_data['result'] = int(dict_data['supply']) - int(dict_data['buy'])
+
     with open(report_file_name, 'w') as file:
         for key, value in dict_data.items():
             file.write(f"{key},{value}\n")
