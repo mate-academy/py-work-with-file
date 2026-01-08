05dd9ae68f62ed04ebd735a78d4cf5bc954161f9
Johan Rangel <johan.apr99@gmail.com>
2026-01-08 17:34:10 +0000

    commented

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    dict_data = {}
+
+    with open(data_file_name, 'r') as file:
+        for line in file:
+            key, value = line.strip().split(',')
+            dict_data[key] = dict_data.get(key, 0) + int(value)
+
+    with open(report_file_name, 'w') as file:
+        for key, value in dict_data.items():
+            file.write(f"{key},{value}\n")
+
+
+
+    
