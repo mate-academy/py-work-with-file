8e578d501397996a9a9adc169262bbf761242665
Vadym Kravchenko <vadymkravchenko.us@gmail.com>
2026-01-08 11:43:20 -0500

    'Solution'

--- app/main.py
+++ app/main.py
@@ -22,4 +22,4 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     with open(report_file_name, "w") as file:
         file.write(f"supply,{supply_total}\n")
         file.write(f"buy,{buy_total}\n")
-        file.write(f"result,{result}")
+        file.write(f"result,{result}\n")   # ← обязательно \n в конце
