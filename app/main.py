ddcd69ad99ebd42b8cdee8232831c192037b23ee
YuriiMykhailenko <urijmihajlenko3@gmail.com>
2026-01-10 17:11:51 +0200

    feat

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    sup = 0
+    bu = 0
+    res = 0
+
+    with open(data_file_name, "r") as file:
+        for line in file:
+            parts = line.split(",")
+            option = parts[0]
+            am = int(parts[1])
+
+            if option == "supply":
+                sup += am
+            elif option == "buy":
+                bu += am
+
+        res = sup - bu
+
+    with open(report_file_name, "w") as report_file:
+        report_file.write(f"supply,{sup}\n")
+        report_file.write(f"buy,{bu}\n")
+        report_file.write(f"result,{res}\n")
