26cfcab941d89ea8717ee9e24bb3fbef50ac4e7b
Pavlus Chaban <pavluschaban@MacBook-Pro-Olena.local>
2026-01-11 16:05:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    with open(data_file_name) as data_file, open(report_file_name, "w") as report_file:
+    with (
+            open(data_file_name) as data_file,
+            open(report_file_name, "w") as report_file
+    ):
         data = dict()
         for line in data_file.readlines():
             key, value = line.replace("\n", "").split(",")
@@ -8,7 +11,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             else:
                 data[key] = int(value)
 
-        report_file.write(f"supply,{data.get("supply")}\n")
-        report_file.write(f"buy,{data.get("buy")}\n")
+        report_file.write(f'supply,{data.get("supply")}\n')
+        report_file.write(f'buy,{data.get("buy")}\n')
         result_line = data.get("supply") - data.get("buy")
         report_file.write(f"result,{result_line}\n")
