142cb8ef9d1b6883d5db88ba399f418978a6a05b
Ambra <trank13f@gmail.com>
2026-01-12 10:12:42 +0100

    PEP8

--- app/main.py
+++ app/main.py
@@ -8,4 +8,5 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             if "buy" in line:
                 buy += int(line.split(",")[1])
     with open(report_file_name, "w") as report_file:
-        report_file.write(f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n")
+        report_file.write(f"supply,{supply}\nbuy"
+                          f",{buy}\nresult,{supply - buy}\n")
