0748e5a8722c6cb5a21743f6bd0208b2b1c0df8b
Ambra <trank13f@gmail.com>
2026-01-12 10:11:06 +0100

    created create_report func

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply = 0
+    buy = 0
+    with open(data_file_name) as data_file:
+        for line in data_file:
+            if "supply" in line:
+                supply += int(line.split(",")[1])
+            if "buy" in line:
+                buy += int(line.split(",")[1])
+    with open(report_file_name, "w") as report_file:
+        report_file.write(f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n")
