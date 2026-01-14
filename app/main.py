46e497c0ec813db276709c167e962946ae81d20a
Yuliia Zahoruulko <yzahorulko@gmail.com>
2026-01-14 06:43:02 +0200

    covered with code

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    temp = {
+        "supply": [],
+        "buy": [],
+    }
+
+    input_file = open(data_file_name, "tr")
+    for index in input_file.readlines():
+        key, value = index.split(",")
+        temp[key].append(int(value))
+    input_file.close()
+
+    output_file = open(report_file_name, "tw")
+    for key, value_list in temp.items():
+        temp[key] = sum(temp[key])
+        output_file.write(f"{key},{temp[key]}\n")
+    output_file.write(f"result,{temp['supply'] - temp['buy']}\n")
+    output_file.close()
