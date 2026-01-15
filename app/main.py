d5dcda9cc11351fd701f51e4d86e15f0468127a7
Andriy <andrijbuluk@gmail.com>
2026-01-15 15:32:27 +0100

    Work with files

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name:str) -> None:
+    data_file = open(data_file_name, "r")
+    supply_sum = 0
+    buy_sum = 0
+
+    for f in data_file:
+        if f == "":
+            break
+        for line in f:
+            operator, number = line.split(",")
+            number = int(number)
+            if operator == "supply":
+                supply_sum += number
+            if operator == "buy":
+                buy_sum += number
+        new_number = open(report_file_name, "w")
+        data_file.close()
+        new_number.close()
+
+
+
+
+
+
+
+
