478476a9828823573ab73e7118aa6b45f6063aab
Andriy <andrijbuluk@gmail.com>
2026-01-15 17:01:01 +0100

    Work with files Errors correct (with)

--- app/main.py
+++ app/main.py
@@ -1,22 +1,23 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    open_data_file = open(data_file_name)
     supply_sum = 0
     buy_sum = 0
 
-    for line in open_data_file:
-        line = line.strip()
-        if not line:
-            continue
-        operator, numbers = line.split(',')
-        numbers = int(numbers)
-        if operator == "supply":
-            supply_sum += numbers
-        elif operator == "buy":
-            buy_sum += numbers
+    with open(data_file_name) as open_data_file:
+        for line in open_data_file:
+            line = line.strip()
+            if not line:
+                continue
+            operator, numbers = line.split(',')
+            numbers = int(numbers)
+            if operator == "supply":
+                supply_sum += numbers
+            elif operator == "buy":
+                buy_sum += numbers
 
-    new_numbers = open(report_file_name, "w")
-    new_numbers.write(f"supply,{supply_sum}\n")
-    new_numbers.write(f"buy,{buy_sum}\n")
-    new_numbers.write(f"result,{supply_sum - buy_sum}\n")
+
+    with open(report_file_name, "w") as new_numbers:
+        new_numbers.write(f"supply,{supply_sum}\n")
+        new_numbers.write(f"buy,{buy_sum}\n")
+        new_numbers.write(f"result,{supply_sum - buy_sum}\n")
 
 
