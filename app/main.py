c8e73428f361c57c043a8dec264586b40d862ca5
StychukMaria <stychuk.maria@gmail.com>
2026-01-28 21:14:02 +0200

    Not all solution2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-def create_report(data_file_name: str, report_file_name: str) -> None:
+def create_report(
+        data_file_name: str,
+        report_file_name: str
+) -> None:
     file_read = open(data_file_name, "r")
+    total = {}
+    for line in file_read.readlines():
+        line.strip().strip("\n")
+        if line != "":
+            operation_amount = line.split(",")
+            if len(operation_amount) == 2:
+                total[operation_amount[0]] += int(operation_amount[1])
+    total["result"] = total["supply"] - total["buy"]
     file_write = open(report_file_name, "w")
-    file_write.write(file_read.read())
+    for key, value in total.items():
+        file_write.write(f"{key},{value}\n")
     file_read.close()
     file_write.close()
