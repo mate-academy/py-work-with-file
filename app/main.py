7e0962b873bf04b106e4670dcd9a7786358931e5
StychukMaria <stychuk.maria@gmail.com>
2026-01-28 20:28:52 +0200

    Not all solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    file_read = open(data_file_name, "r")
+    file_write = open(report_file_name, "w")
+    file_write.write(file_read.read())
+    file_read.close()
+    file_write.close()
