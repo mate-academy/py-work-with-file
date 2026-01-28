f7d3a889c7557e21502f2f1c6cde9626a02aeb24
Mark Loney <markloney@loney.me>
2026-01-28 11:18:25 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+
+    report = {}
+
+    data_file = open(data_file_name)
+
+    for line in data_file:
+        if line:
+            item, qty = line.split(",")
+            report[item] = report.get(item, 0) + int(qty)
+
+    data_file.close()
+
+    report_file = open(report_file_name, "w")
+
+    report_file.write(f'supply,{report.get("supply", 0)}\n')
+    report_file.write(f'buy,{report.get("buy", 0)}\n')
+    report_file.write(f"result,"
+                      f'{(report.get("supply", 0)
+                          - report.get("buy", 0))}\n')
+    report_file.close()
