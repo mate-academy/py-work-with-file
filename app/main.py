f2706781aafbb556e20fdb1bdb449c9fe76ad5fa
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-02-02 13:43:49 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
 # write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_file = open(data_file_name)
+    supply = 0
+    buy = 0
+
+    for line in data_file:
+        elems = line.split(",")
+        if elems[0] == "supply":
+            supply = int(elems[1])
+        else:
+            buy = int(elems[1])
+    data_file.close()
+
+    report = open(report_file_name, "w")
+    report.write("supply," + str(supply))
+    report.write("buy," + str(buy))
+    report.write("result," + str(supply - buy))
+    report.close()
