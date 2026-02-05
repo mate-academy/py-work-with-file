cf0d4a3352f27c29828c762af7a5543cd89e4685
ototskiy <ototskiy@gmail.com>
2026-02-05 23:16:59 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,10 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     dict_result = {}
     file_data = open(data_file_name)
     for line in file.readlines():
-        dic_result.[line.split(",")[0]] += line.split(",")[1]
+        if dic_result.[line.split(",")[0]]:
+            dic_result.[line.split(",")[0]] += line.split(",")[1]
+        else:
+            dic_result.[line.split(",")[0]] = line.split(",")[1]
     file_data.close()  
     file_report = open(report_file_name, "w")
     for key, value in dic_result.items():
