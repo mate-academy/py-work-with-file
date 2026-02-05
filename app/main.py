25c9e4d2f05a525f79fcdccda05e6e49b2753f27
Anastasia Holovach <asia.ostafiychuk@gmail.com>
2026-02-05 21:43:06 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+import csv
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    result = {
+        "supply": 0,
+        "buy": 0,
+        "result": 0
+    }
+
+    with open(data_file_name, "r", newline="") as file_in:
+        reader = csv.reader(file_in)
+
+        for key, value in reader:
+            result[key] += int(value)
+
+        result["result"] = result["supply"] - result["buy"]
+
+    with open(report_file_name, "w", newline="") as file_out:
+        writer = csv.writer(file_out)
+
+        for key, value in result.items():
+            writer.writerow([key, value])
