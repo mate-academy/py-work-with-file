8484ff3b5ba94f5c3afb96d3c7fbd54be09edc03
Danylo <danil.bogomazov1356@gmail.com>
2026-02-11 18:49:25 +0200

    Create create_report func

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    report = open(report_file_name, "w")
+    data = open(data_file_name, "r")
+    new_dict = {}
+    for line in data:
+        if not line:
+            break
+        string = line.split(",")
+        if new_dict.get(string[0]) is None:
+            new_dict[string[0]] = int(string[1])
+            continue
+        new_dict[string[0]] += int(string[1])
+    data.close()
+    new_dict["result"] = new_dict["supply"] - new_dict["buy"]
+    order = ["supply", "buy", "result"]
+    for key in order:
+        report.write(key + "," + str(new_dict[key]) + "\n")
+    report.close()
