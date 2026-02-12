4b3d5193e8e48bc16352b01b84bb693b889a06fb
Illia Savinov <dresar2404@gmail.com>
2026-02-12 14:52:04 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -2,21 +2,20 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     buy = 0
     supply = 0
-    with open(data_file_name, 'r', encoding='utf-8') as f:
+    with open(data_file_name, "r", encoding="utf-8") as f:
         for line in f:
             line = line.strip()
             if not line:
                 continue
-            parts = line.split(',')
+            parts = line.split(",")
             op = parts[0]
             amount = int(parts[1])
-            if op == 'supply':
+            if op == "supply":
                 supply += amount
-            elif op == 'buy':
+            elif op == "buy":
                 buy += amount
     result = supply - buy
-    with open(report_file_name, 'w', encoding='utf-8', newline='') as f:
+    with open(report_file_name, "w", encoding="utf-8", newline="") as f:
         f.write(f"supply,{supply}\n")
         f.write(f"buy,{buy}\n")
         f.write(f"result,{result}\n")
-
