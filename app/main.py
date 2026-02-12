1eb9a620806e678e827fbf5aafd19985d5a8da65
Serhii <sergei.amvarcumov@gmail.com>
2026-02-12 23:09:29 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    totals = {"supply": 0, "buy": 0}
+    with open(data_file_name, "r", encoding="utf-8") as data_file:
+        for line in data_file:
+            clean_line = line.strip()
+            if not clean_line:
+                continue
+            operation, amount = clean_line.split(",")
+            totals[operation] += int(amount)
+
+    result = totals["supply"] - totals["buy"]
+    with open(report_file_name, "w", encoding="utf-8") as report_file:
+        report_file.write(f"supply,{totals['supply']}\n")
+        report_file.write(f"buy,{totals['buy']}\n")
+        report_file.write(f"result,{result}\n")
