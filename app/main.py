656fcbfc7c5b6782f69eee8606fb5a629eaf77d0
Denys Zelenov <hellgarden03@gmail.com>
2026-02-12 15:28:59 +0000

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     supply = 0
     buy = 0
+
     with open(data_file_name, "r") as file:
         for line in file:
-            data = line.strip().split(",")
-            if len(data) < 2:
+            line = line.strip()
+            if not line:
                 continue
-            cmd = data[0].strip()
-            numbers_str = data[1].strip().split()
-            numbers = [int(x) for x in numbers_str if x.isdigit()]
-            total = sum(numbers)
+            cmd, amount_str = line.split(",")
+            amount = int(amount_str)
             if cmd == "supply":
-                supply += total
+                supply += amount
             elif cmd == "buy":
-                buy += total
+                buy += amount
+
     with open(report_file_name, "w") as result:
         result.write(f"supply,{supply}\n")
         result.write(f"buy,{buy}\n")
