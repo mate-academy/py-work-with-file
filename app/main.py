c7d0a31f7f1a425015f8acac2dc9b88e1fb46c2b
Jerzy Joniec <j.joniec5@gmail.com>
2026-02-13 09:32:50 +0100

    „final”

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_file = open(data_file_name, "r")
+    data = data_file.readlines()
+    lista = [item.strip() for item in data]
+    buy = 0
+    supply = 0
+    for data in lista:
+        key = (data.split(",")[0])
+        if key == "buy":
+            buy += int(data.split(",")[1])
+        if key == "supply":
+            supply += int(data.split(",")[1])
+
+    with open(report_file_name, "w") as final:
+        final.write(f"supply,{supply}\n")
+        final.write(f"buy,{buy}\n")
+        final.write(f"result,{supply - buy}\n")
