a8d7faa3a17a0d5aef4248cbe6ab5ec5f46a4f63
Mariana Trinko <marjanavv@gmail.com>
2026-02-19 18:00:47 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str):
+    file = open(data_file_name)
+    data_set = file.readlines();
+    supply = 0
+    buy = 0
+    for data in data_set:
+        array = data.split(",")
+        if array[0] == "supply":
+            supply += int(array[1])
+        if array[0] == "buy":
+            buy += int(array[1])
+
+    result = supply - buy
+    content = f"supply,{str(supply)}\nbuy,{str(buy)}\nresult,{result}\n"
+    report = open(report_file_name, "w")
+    report.write(content)
