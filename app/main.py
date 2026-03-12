1697c5350973ae0dc6342275672fe032f6d5d137
Dmytro Shulha <dimonchik0218@gmail.com>
2026-03-12 12:47:50 +0200

    task work_with_file done

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     total_supply = 0
     total_buy = 0
+
     with open(data_file_name, "r", encoding="utf-8") as file:
         for line in file:
             line = line.strip()
