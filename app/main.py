ecab610d2057702e70a916046af77ad74f3367df
Illia Redka <illaredka8@gmail.com>
2026-03-18 12:59:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    report = {}
+    data_file = open(data_file_name, "r")
+    for line in data_file.readlines():
+        splited_line = line.rstrip("\n").split(",")
+        if splited_line[0] in report:
+            report[splited_line[0]] += int(splited_line[1])
+        else:
+            report[splited_line[0]] = int(splited_line[1])
+    data_file.close()
+
+    report_file = open(report_file_name, "w")
+    report_file.write(
+        f'supply,{report["supply"]}\n'
+        f'buy,{report["buy"]}\n'
+        f'result,{report["supply"] - report["buy"]}\n'
+    )
+    report_file.close()
