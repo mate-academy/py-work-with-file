14193233b30107a5e69a3092f6ddcc32ae17dbf2
Vijay Chand <vijaychand6979@googlemail.com>
2026-03-25 16:34:38 +0000

    Added code

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    input_file = open(data_file_name, "r")
+
+    supply = 0
+    buy = 0
+
+    for line in input_file:
+        method, amount = line.split(sep=",")
+        if method == "supply":
+            supply += int(amount)
+        elif method == "buy":
+            buy += int(amount)
+    input_file.close()
+
+    output_file = open(report_file_name, "w")
+    output_file.write(f"supply,{supply}\n")
+    output_file.write(f"buy,{buy}\n")
+    output_file.write(f"result,{supply - buy}\n")
+    output_file.close()
