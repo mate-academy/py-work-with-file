741e719be6bf6b9f86d35ef455c13d45bbc73589
Pavlo Danylkiv <danylkiv.pavlo@gmail.com>
2026-03-27 00:53:09 +0200

    complete task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def create_report(data_file: str, report_file_name: str) -> None:
+    with open(data_file, "r") as read_file:
+        result = {"supply": 0, "buy": 0}
+        for item in read_file:
+            key, value = item.strip().split(",")
+            result[key] += int(value)
+        result["result"] = result["supply"] - result["buy"]
+        with open(report_file_name, "w") as write_file:
+            for keys, values in result.items():
+                write_file.write(f"{keys},{values}\n")
