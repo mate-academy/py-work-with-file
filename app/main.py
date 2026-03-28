e4dc35f3d79f97ba2d000fe16a595e80ac764958
Alex <alex@example.com>
2026-03-29 00:14:38 +0200

    finish 2

--- app/main.py
+++ app/main.py
@@ -3,12 +3,12 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     supply = 0
     buy = 0
 
-    with open(data_file_name, 'r') as infile:
+    with open(data_file_name, "r") as infile:
         for line in infile:
             parts1 = line.strip()
             if not parts1:
                 continue
-            parts2 = parts1.split(',')
+            parts2 = parts1.split(",")
             if len(parts2) != 2:
                 continue
             op = parts2[0].strip()
@@ -18,13 +18,13 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
             except ValueError:
                 continue
 
-            if op == 'supply':
+            if op == "supply":
                 supply += value
 
-            elif op == 'buy':
+            elif op == "buy":
                 buy += value
 
-    with open(report_file_name, 'w', encoding='utf-8') as f:
-        f.write(f'supply,{supply}\n')
-        f.write(f'buy,{buy}\n')
-        f.write(f'result,{supply - buy}\n')
+    with open(report_file_name, "w", encoding="utf-8") as f:
+        f.write(f"supply,{supply}\n")
+        f.write(f"buy,{buy}\n")
+        f.write(f"result,{supply - buy}\n")
