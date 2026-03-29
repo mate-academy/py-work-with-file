f1fae638f55d15ea1a74d66d612c7b900b7869fb
Agnieszka Barcik <aga.barcik11@gmail.com>
2026-03-29 21:50:44 +0200

    add return type annotation

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def create_report(data_file_name: str, report_file_name: str):
+def create_report(data_file_name: str, report_file_name: str) -> None:
     p1 = open(data_file_name, "r")
 
     sum_buy = 0
