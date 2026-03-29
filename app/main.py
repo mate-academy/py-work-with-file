5a24778a63397c60c87c94de9fd1ea9c7a72421a
Oleh M <oleh.murachov28@gmail.com>
2026-03-29 18:15:33 +0200

    pol

--- app/main.py
+++ app/main.py
@@ -1,42 +1,25 @@
-import csv
+def create_report(data_file_name: str, report_file_name: str):
+    supply_total = 0
+    buy_total = 0
 
+    with open(data_file_name, 'r') as file:
+        for line in file:
+            line = line.strip()
 
-def create_report(
-    data_file_name: str,
-    report_file_name: str
-) -> None:
-    supply = 0
-    buy = 0
-
-    with open(data_file_name, mode='r', encoding='utf-8') as file:
-        reader = csv.reader(file)
-
-        for row in reader:
-            if len(row) != 2:
+            if not line:
                 continue
 
-            operation, amount = row
-
-            try:
-                amount = int(amount)
-            except ValueError:
-                continue
+            operation, value = line.split(',')
+            value = int(value)
 
             if operation == 'supply':
-                supply += amount
+                supply_total += value
             elif operation == 'buy':
-                buy += amount
+                buy_total += value
 
-    result = supply - buy
+    result = supply_total - buy_total
 
-    with (
-        open(report_file_name,
-             mode='w',
-             encoding='utf-8',
-             newline='')
-        as file
-    ):
-        writer = csv.writer(file)
-        writer.writerow(['supply', supply])
-        writer.writerow(['buy', buy])
-        writer.writerow(['result', result])
+    with open(report_file_name, 'w') as file:
+        file.write(f"supply,{supply_total}\n")
+        file.write(f"buy,{buy_total}\n")
+        file.write(f"result,{result}\n")
