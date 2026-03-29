b93dc5d200e432723e434602fdf78485a896e747
Oleh M <oleh.murachov28@gmail.com>
2026-03-29 15:27:23 +0200

    ok

--- app/main.py
+++ app/main.py
@@ -1,1 +1,42 @@
-# write your code here
+import csv
+
+
+def create_report(
+    data_file_name: str,
+    report_file_name: str
+) -> None:
+    supply = 0
+    buy = 0
+
+    with open(data_file_name, mode='r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+
+        for row in reader:
+            if len(row) != 2:
+                continue
+
+            operation, amount = row
+
+            try:
+                amount = int(amount)
+            except ValueError:
+                continue
+
+            if operation == 'supply':
+                supply += amount
+            elif operation == 'buy':
+                buy += amount
+
+    result = supply - buy
+
+    with (
+        open(report_file_name,
+             mode='w',
+             encoding='utf-8',
+             newline='')
+        as file
+    ):
+        writer = csv.writer(file)
+        writer.writerow(['supply', supply])
+        writer.writerow(['buy', buy])
+        writer.writerow(['result', result])
