1bc39394b902e2da8b3168d723e7eda1522ed537
Neyrony <sserebriy@gmail.com>
2026-03-30 16:34:28 +0300

    Implemented solution for py-work-with-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply = {"supply": 0, "buy": 0}
+    with open(data_file_name) as data_file:
+        for line in data_file:
+            key, value = line[:len(line) - 1].split(",")
+            supply[key] += int(value)
+
+    with open(report_file_name, "a") as report_file:
+        supply["result"] = supply["supply"] - supply["buy"]
+        for key, value in supply.items():
+            report_file.write(f"{key},{value}\n")
