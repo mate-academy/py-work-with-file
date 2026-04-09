b04b2749deaf64ba2f56f2ee4e13dc36c7946f54
Дмитро Кедьо <your@mail.com>
2026-04-09 16:48:01 +0300

    fix: Solution

--- app/main.py
+++ app/main.py
@@ -5,11 +5,9 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     supply_total: int = 0
     buy_total: int = 0
 
-
-    with open(data_file_name, "r") as data_file:  # type: TextIO
+    with open(data_file_name, "r", encoding="utf-8") as data_file:  # type: TextIO
         for line in data_file:
             line = line.strip()
-
 
             if not line:
                 continue
@@ -24,8 +22,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
 
     result: int = supply_total - buy_total
 
-
-    with open(report_file_name, "w") as report_file:  # type: TextIO
+    with open(report_file_name, "w", encoding="utf-8") as report_file:  # type: TextIO
         report_file.write(f"supply,{supply_total}\n")
         report_file.write(f"buy,{buy_total}\n")
         report_file.write(f"result,{result}\n")
