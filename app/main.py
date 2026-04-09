ef2ee2e91a688c6dd342ca6736dd2018dc47e449
David Ivan <davidivan6900@gmail.com>
2026-04-09 16:38:19 +0700

    Solution

--- app/main.py
+++ app/main.py
@@ -1,21 +1,20 @@
 def create_report(data_file_name: str, report_file_name: str) -> None:
     data_file = open(data_file_name, "r")
-    operation_dict = {}
+    supply = 0
+    buy = 0
     result = 0
+
     for line in data_file.readlines():
         name, price = line.split(",")
-        if name not in operation_dict:
-            operation_dict[name] = int(price)
-        operation_dict[name] += int(price)
-        if name == "buy":
-            result -= int(price)
-        if name == "supply":
-            result += int(price)
 
+        if name == "buy":
+            buy += int(price)
+        if name == "supply":
+            supply += int(price)
+    result = supply - buy
+    print(supply, buy, result)
     data_file.close()
-    operation_dict["result"] = result
 
     report_file = open(report_file_name, "w")
-    for key, value in operation_dict.items():
-        report_file.write(f"{key},{value}\n")
+    report_file.write(f"supply,{supply}\nbuy,{buy}\nresult,{result}")
     report_file.close()
