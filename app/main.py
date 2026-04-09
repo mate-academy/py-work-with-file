46165081f9f016b25134c5c3aaec368de25d8473
Дмитро Кедьо <your@mail.com>
2026-04-09 16:44:35 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,31 @@
-# write your code here
+from typing import TextIO
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply_total: int = 0
+    buy_total: int = 0
+
+
+    with open(data_file_name, "r") as data_file:  # type: TextIO
+        for line in data_file:
+            line = line.strip()
+
+
+            if not line:
+                continue
+
+            operation, amount_str = line.split(",")
+            amount: int = int(amount_str)
+
+            if operation == "supply":
+                supply_total += amount
+            elif operation == "buy":
+                buy_total += amount
+
+    result: int = supply_total - buy_total
+
+
+    with open(report_file_name, "w") as report_file:  # type: TextIO
+        report_file.write(f"supply,{supply_total}\n")
+        report_file.write(f"buy,{buy_total}\n")
+        report_file.write(f"result,{result}\n")
