759019f6c0a3bd2ab0a0f7101e52281a8e5fe993
David Ivan <davidivan6900@gmail.com>
2026-04-09 15:52:48 +0700

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_file = open(data_file_name, "r")
+    operation_dict = {}
+    for line in data_file.readlines():
+        name, price = line.split(",")
+        if name not in operation_dict:
+            operation_dict[name] = int(price)
+        operation_dict[name] += int(price)
+    data_file.close()
+
+    report_file = open(report_file_name, "w")
+    for key, value in operation_dict.items():
+        report_file.write(f"{key},{value}\n")
+    report_file.close()
+
