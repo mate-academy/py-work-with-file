7ce32e3e2290b77477ca38e7c738faed0c3c1207
Дмитро Кедьо <your@mail.com>
2026-04-09 16:50:50 +0300

    fix: 1Solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
-from typing import TextIO
-
-
 def create_report(data_file_name: str, report_file_name: str) -> None:
-    supply_total: int = 0
-    buy_total: int = 0
+    supply_total = 0
+    buy_total = 0
 
-    with open(data_file_name, "r", encoding="utf-8") as data_file:  # type: TextIO
+    with open(
+        data_file_name,
+        "r",
+        encoding="utf-8",
+    ) as data_file:
         for line in data_file:
             line = line.strip()
 
@@ -13,16 +14,20 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
                 continue
 
             operation, amount_str = line.split(",")
-            amount: int = int(amount_str)
+            amount = int(amount_str)
 
             if operation == "supply":
                 supply_total += amount
             elif operation == "buy":
                 buy_total += amount
 
-    result: int = supply_total - buy_total
+    result = supply_total - buy_total
 
-    with open(report_file_name, "w", encoding="utf-8") as report_file:  # type: TextIO
+    with open(
+        report_file_name,
+        "w",
+        encoding="utf-8",
+    ) as report_file:
         report_file.write(f"supply,{supply_total}\n")
         report_file.write(f"buy,{buy_total}\n")
         report_file.write(f"result,{result}\n")
