f1046132ab0136a7526265bc363778c32cea95de
Андрій Грабовський <yojyk.pl@gmail.com>
2026-04-11 12:16:35 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    results = {"supply": 0, "buy": 0}
+    with (open(data_file_name, "r") as data_file,
+          open(report_file_name, "w") as report_file):
+        for line in data_file:
+            line = line.strip()
+            if not line:
+                continue
+            name, amount = line.split(",")
+            results[name] += int(amount)
+        net_result = results["supply"] - results["buy"]
+        report_file.write(
+            f"supply,{results['supply']}\n"
+            f"buy,{results['buy']}\n"
+            f"result,{net_result}\n"
+        )
