b3e2a6c0f94d8967b4d3e221d44af9822461b0b7
Alisa Hoys <hoys@protonmail.com>
2026-04-11 12:18:27 +0200

    Created report file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply = 0
+    buy = 0
+    with open(data_file_name, "r") as data_file:
+        data_lst = data_file.readlines()
+
+        for line in data_lst:
+            line = line.split(",")
+            if line[0] == "supply":
+                supply += int(line[1])
+            elif line[0] == "buy":
+                buy += int(line[1])
+
+    with open(report_file_name, "w") as report_file:
+        report_file.write(f"supply,{supply}\n")
+        report_file.write(f"buy,{buy}\n")
+        report_file.write(f"result,{supply - buy}\n")
