6744cc2dd838ba367ed5c8cb04850a002195a94b
Roman Smyk <smyk_roman@ukr.net>
2026-04-13 13:53:39 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     supply = 0
     buy = 0
 
-    with open(data_file_name) as file:
+    with open(data_file_name, "r") as file:
         for line in file:
             line = line.strip()
             if line.split(",")[0] == "supply":
