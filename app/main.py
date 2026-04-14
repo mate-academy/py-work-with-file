08105b6f9644a61506cdf34e3249e2c4a2cebe6f
Prus Oleksandr <cheepaxxa2003@gmail.com>
2026-04-14 17:22:37 +0300

    Implementing a report from a data file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply = 0
+    buy = 0
+    data_file = open(data_file_name, "r")
+    for line in data_file:
+        if not line.strip():
+            continue
+        operation, amount = line.strip().split(",")
+        if operation == "supply":
+            supply += int(amount)
+        if operation == "buy":
+            buy += int(amount)
+    report_file = open(report_file_name, "w")
+    report_file.write(f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n")
+    data_file.close()
+    report_file.close()
