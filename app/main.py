a89eef44b19445f5e42e8d46aec1d92cae7fc675
amesemyta1 <amesemyta@gmail.com>
2026-04-14 17:14:15 +0300

    first commit create_report

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data_file = open(data_file_name, "r")
+    report_file = open(report_file_name, "w")
+
+    data = {}
+
+    for line in data_file:
+        line = line.split(",")
+        if line[0] not in data:
+            data[line[0]] = int(line[1].strip("\n"))
+        else:
+            data[line[0]] += int(line[1].strip("\n"))
+
+    report_file.write(f"supply,{data['supply']}\n")
+    report_file.write(f"buy,{data['buy']}\n")
+    report_file.write(f'result,{data["supply"] - data["buy"]}\n')
+
+    data_file.close()
+    report_file.close()
