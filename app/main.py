dc1ee2d09e1f8f11d0c275a059bbb66e5e579883
andriihrechko <andriihrechko250703@gmail.com>
2026-04-14 13:02:22 +0300

    feat: complete create_report() feature

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    report_dict = {"supply": 0, "buy": 0}
+
+    with open(data_file_name, "r") as input_file:
+        for line in input_file:
+            operation, amount = line.strip().split(",")
+            if operation in report_dict:
+                report_dict[operation] += int(amount)
+    report_dict["result"] = report_dict["supply"] - report_dict["buy"]
+
+    with open(report_file_name, "w") as output_file:
+        for key, value in report_dict.items():
+            output_file.write(f"{key},{value}\n")
