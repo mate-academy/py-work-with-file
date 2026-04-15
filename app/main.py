08198804bd7581e727b8f3e4ab3c0248ce3b90ce
Igor <zalugin.iv2@gmail.com>
2026-04-15 14:01:21 +0200

    now tests passing by console

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import csv
 
 def create_report(data_file_name: str, report_file_name: str) -> None:
     result = {"supply": 0, "buy": 0, "result": 0}
-    with open(f"../{data_file_name}", "r") as file:
+    with open(data_file_name, "r") as file:
         reader = csv.reader(file)
         for row in reader:
             key = row[0]
