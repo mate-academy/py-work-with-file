07ec21df37f83f47453bfd6a70fdfee78edf36b7
David <meeddry@gmail.com>
2026-04-15 22:48:53 +0300

    change_path_to_tests

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
-import tests
+import os
+
 
 def create_report(data_file_name: str, report_file_name: str):
-    with open(data_file_name, "r", encoding="utf-8") as data_file, \
-         open(report_file_name, "w", encoding="utf-8") as report_file:
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+    data_path = os.path.join(BASE_DIR, data_file_name)
+    report_path = os.path.join(BASE_DIR, report_file_name)
+
+    with open(data_path, "r", encoding="utf-8") as data_file, \
+         open(report_path, "w", encoding="utf-8") as report_file:
 
         report = {
             "supply": 0,
@@ -12,16 +18,12 @@ def create_report(data_file_name: str, report_file_name: str):
         for line in data_file:
             if not line.strip():
                 continue
-            column1, column2 = line.strip().split(",")
 
-            report[column1] += int(column2)
+            operation, amount = line.strip().split(",")
+            report[operation] += int(amount)
 
         result = report["supply"] - report["buy"]
 
         report_file.write(f"supply,{report['supply']}\n")
         report_file.write(f"buy,{report['buy']}\n")
         report_file.write(f"result,{result}\n")
-
-
-
-
