ecb20f8afd7f20b269d6bfc2dc390606ffe04615
Ilona <ilonanikolaychuk052@gmail.com>
2026-04-15 22:30:48 +0300

    Implement function create_report

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import csv
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    result_data = {"supply": 0, "buy": 0, "result": 0}
+    data_file = open(data_file_name, "r")
+    reader = csv.reader(data_file)
+    for row in reader:
+        result_data[row[0]] += int(row[1])
+    result_data["result"] = result_data["supply"] - result_data["buy"]
+    data_file.close()
+
+    with open(report_file_name, "w") as f:
+        writer = csv.writer(f)
+        for key, value in result_data.items():
+            writer.writerow([key, value])
