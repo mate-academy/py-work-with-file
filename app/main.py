e6971e1fd13cfd48c36bc84596fb7a3ff461f941
Raffaele Maccarrone <raffaele.alves.maccarrone@gmail.com>
2026-05-02 14:06:50 -0300

    Solução

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data = open(data_file_name)
+    report = {
+        "supply": 0,
+        "buy": 0,
+        "result": 0
+    }
+    for line in data:
+        line = line.rstrip("\n")
+        if line == "":
+            continue
+        element = line.split(",")
+        report[element[0]] += int(element[1])
+    report["result"] = report["supply"] - report["buy"]
+    new_file = open(report_file_name, "w")
+    for key, value in report.items():
+        new_file.write(f"{key},{value}\n")
