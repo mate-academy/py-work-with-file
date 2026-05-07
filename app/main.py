bd47b02c092365e5fda689805c585fb14258ae43
ProstoPonchik <vladyslavvi@fornova.net>
2026-05-07 20:32:32 +0200

    Solved!

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import pandas
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    df = pandas.read_csv(data_file_name, sep=",", header=None,
+                         names=["operation", "value"])
+
+    supply_sum = df[df["operation"] == "supply"]["value"].sum()
+    buy_sum = df[df["operation"] == "buy"]["value"].sum()
+    diff = supply_sum - buy_sum
+
+    result_df = pandas.DataFrame({
+        "text": ["supply", "buy", "result"],
+        "value": [supply_sum, buy_sum, diff]
+    })
+
+    result_df.to_csv(report_file_name, index=False, header=False, sep=",")
