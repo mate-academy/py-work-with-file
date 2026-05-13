b43122c0417b958ea0b35a83bdfd08cc3354c888
Shongi <shongi85@gmail.com>
2026-05-13 13:34:30 +0200

    file read-write

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    output_data = {}
+
+    input_file = open(data_file_name)
+    for line in input_file:
+        key, value = line.strip().split(",")
+        output_data[key] = output_data.get(key, 0) + int(value)
+    input_file.close()
+
+    output_file = open(report_file_name, "w")
+    output_file.write(f"supply,{output_data['supply']}\n")
+    output_file.write(f"buy,{output_data['buy']}\n")
+    output_file.write(f"result,{output_data['supply'] - output_data['buy']}\n")
+    output_file.close()
