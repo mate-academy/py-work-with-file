6e332e37ea8c9db32472a5580f247196e469dc8d
Nikita Ivanichenko <importantthings1482@gmail.com>
2026-05-27 19:05:10 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    market_data = {"supply": 0, "buy": 0}
+    with open(data_file_name, "r") as data_file:
+        for line in data_file:
+            key, value = line.strip().split(",")
+            market_data[key] = market_data.get(key, 0) + int(value)
+    market_data["result"] = market_data["supply"] - market_data["buy"]
+    with open(report_file_name, "w") as report_file:
+        for key, value in market_data.items():
+            report_file.write(f"{key},{value}\n")
