c92b15ca52fbf0abbc44aa295043642d80aeb026
alemonnjy222 <71006485+alemonnjy222@users.noreply.github.com>
2026-06-08 10:30:12 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,15 +7,15 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
 
             if not line:
                 continue
-        operation, amount = line.split(',')
+        operation, amount = line.split(",")
         amount = int(amount)
 
-        if operation == 'supply':
+        if operation == "supply":
             supply_total += amount
-        if operation == 'buy':
+        if operation == "buy":
             buy_total += amount
     result = supply_total - buy_total
-    with open(report_file_name, 'w') as f:
+    with open(report_file_name, "w") as f:
         f.write(f"supply, {supply_total}\n")
         f.write(f"buy, {buy_total}\n)")
         f.write(f"result, {result}\n)")
