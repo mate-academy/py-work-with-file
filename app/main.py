f7c305275f12d2d8456bd6e8a420987fb8e7f3c9
Pat9990 <patikuk@onet.pl>
2026-06-08 22:29:35 +0200

    task done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+import csv
+
+
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply = 0
+    buy = 0
+
+    with open(data_file_name, mode="r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+
+        for operation, amount in reader:
+            amount = int(amount)
+
+            if operation == "supply":
+                supply += amount
+            elif operation == "buy":
+                buy += amount
+
+    result = supply - buy
+
+    with open(report_file_name, mode="w", encoding="utf-8") as file:
+        file.write(f"supply,{supply}\n")
+        file.write(f"buy,{buy}\n")
+        file.write(f"result,{result}\n")
