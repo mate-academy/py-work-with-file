23cd52d8613d4c652aaa6e61699349c7e5ce1fc9
Walter_504 <157768009+WalterMatheusgg@users.noreply.github.com>
2026-06-12 15:31:13 -0300

    UPPDATE

--- app/main.py
+++ app/main.py
@@ -9,12 +9,13 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
 
                 if operation == "supply":
                     supply += int(amount)
+
                 elif operation == "buy":
                     buy += int(amount)
 
     result = supply - buy
 
-   with open(report_file_name, "w") as report_file:
+    with open(report_file_name, "w") as report_file:
         report_file.write(f"supply,{supply}\n")
         report_file.write(f"buy,{buy}\n")
         report_file.write(f"result,{result}\n")
