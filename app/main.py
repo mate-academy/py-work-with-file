afa7da31b5ad83f7891d3b4786e087f168aabe42
Vitalii <stalchenkovitaliy@gmail.com>
2026-06-17 12:27:36 +0400

    Solve test work-with-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as file:
+        text = file.read()
+
+    report = {
+        "supply": 0,
+        "buy": 0,
+    }
+
+    for line in text.splitlines():
+        operation, amount = line.split(",")
+        report[operation] += int(amount)
+
+    result = report["supply"] - report["buy"]
+
+    with open(report_file_name, "w") as file:
+        file.write(
+            f"supply,{report['supply']}\n"
+            f"buy,{report['buy']}\n"
+            f"result,{result}\n"
+        )
