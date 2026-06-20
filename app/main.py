c3000c724f97f4d4b3649b0ae6d8ea07579f3189
Alona Samotoi <meit109nerium@gmail.com>
2026-06-20 18:43:27 +0300

    Solution0

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    supply = 0
+    buy = 0
+
+    with open(data_file_name, "r") as f:
+        for line in f:
+            list_of_line = line.split(",")
+            if not line:
+                continue
+            elif list_of_line[0] == "buy":
+                buy += int(list_of_line[1])
+            elif list_of_line[0] == "supply":
+                supply += int(list_of_line[1])
+
+    result = supply - buy
+
+    with open(report_file_name, "w") as r:
+        r.write(f"supply,{supply})")
+        r.write(f"buy,{buy})")
+        r.write(f"result,{result}")
