4b0e5472b4ed536c750384ac8cf68447bfa8a48c
Oleh K <hmkl.public@gmail.com>
2026-06-25 21:17:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as file:
+        content = file.readlines()
+
+    report_content = {"supply": 0, "buy": 0}
+
+    for line in content:
+        if not line.strip():
+            continue
+
+        action, price = line.strip().split(",")
+
+        if action in report_content:
+            report_content[action] += int(price)
+
+    result = report_content["supply"] - report_content["buy"]
+    report_template = (
+        f"supply,{report_content['supply']}\n"
+        f"buy,{report_content['buy']}\n"
+        f"result,{result}\n"
+    )
+
+    with open(report_file_name, "w") as file:
+        file.write(report_template)
