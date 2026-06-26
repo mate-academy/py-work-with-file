d1179a3135fa30d18e562979d1ebcb0b9c45f1c0
Fetivol <Fetisowova@gmail.com>
2026-06-26 18:41:19 +0200

    created function create_report

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    data = {"supply": 0, "buy": 0}
+
+    with open(data_file_name, "r") as data_file:
+        for line in data_file.readlines():
+            clean_line = line.strip()
+            if clean_line:
+                data[clean_line.split(",")[0]] += int(clean_line.split(",")[1])
+
+    data["result"] = data["supply"] - data["buy"]
+    print(data)
+
+    with open(report_file_name, "w") as report_file:
+        for key, value in data.items():
+            report_file.write(f"{key},{value}\n")
