317b78144e503dacbc4332d330f1b7d532a26fae
Anderson <anderson.medeiros3011@gmail.com>
2026-07-09 17:08:18 -0300

    feat: implementa criacao de relatorio csv 2

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ def create_report(data_file_name: str, report_file_name: str) -> None:
     supply = 0
     buy = 0
 
-
     with open(data_file_name, "r") as file:
         lines = file.readlines()
 
