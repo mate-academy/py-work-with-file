9917d3c73bd28ca0303ba64c0585c81670612b4a
Maksym Murtazin <max.murtazin2006@gmail.com>
2026-07-15 16:08:48 +0300

    Created a function that opens an input file for reading and creates an output file where it writes a report.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name) as data_file:
+        supply = 0
+        buy = 0
+        for line in data_file:
+            if "supply" in line:
+                supply += int(line.split(",")[1])
+            elif "buy" in line:
+                buy += int(line.split(",")[1])
+
+        with open(report_file_name, "w") as report_file:
+            report_file.write(
+                f"supply,{supply}\nbuy,{buy}\nresult,{supply - buy}\n")
